2caf157a78b20f3f680eb69f5110cdcef1de957a
Roman31X <die.haa65@gmail.com>
2024-01-02 21:55:39 -0500

    Solucion simplificada

--- src/main/java/com/Ejercicio/Kyu8/Años_Gatos_Perros/AñosGatosyPerros.java
+++ src/main/java/com/Ejercicio/Kyu8/Años_Gatos_Perros/AñosGatosyPerros.java
@@ -3,13 +3,12 @@ package com.Ejercicio.Kyu8.Años_Gatos_Perros;
 public class AñosGatosyPerros {
     public static int[] yearsHumanosGatosPerros(final int humanoAnios){
         int yearCat = 0,yearDog = 0;
-        if(humanoAnios < 1){
+        if(humanoAnios <= 0){
             System.out.println("Debe ingresar un numero entero mayor a 0");;
-        }else{
-            for (int i = 0; i < humanoAnios; i++) {
-                yearCat+= (i==1)?15:(i==2)?9:(i>2)?4:0;
-                yearDog+= (i==1)?15:(i==2)?9:(i>2)?5:0;
-            }
+        }
+        for (int i = 1; i <= humanoAnios; i++) {
+            yearCat += (i==1)?15:(i==2)?9:(i>2)?4:0;
+            yearDog += (i==1)?15:(i==2)?9:(i>2)?5:0;
         }
         return new int[]{humanoAnios,yearCat,yearDog};
     }
--- src/main/java/com/Ejercicio/Kyu8/Años_Gatos_Perros/testAñosGatosyPerros.java
+++ src/main/java/com/Ejercicio/Kyu8/Años_Gatos_Perros/testAñosGatosyPerros.java
@@ -1,7 +1,26 @@
 package com.Ejercicio.Kyu8.Años_Gatos_Perros;
 
+import static com.Ejercicio.Kyu8.Años_Gatos_Perros.AñosGatosyPerros.yearsHumanosGatosPerros;
+
 public class testAñosGatosyPerros {
     public static void main(String[] args) {
-
+        System.out.println("Solución a calculo de años humanos en años gatos y perros");
+        int[] yearCalculo1 = yearsHumanosGatosPerros(1);
+        System.out.println("1 año humano equivale en Humano - Gato - Perro");
+        for (int edad1 : yearCalculo1) {
+            System.out.print("["+edad1+"]");
+        }
+        System.out.println();
+        int[] yearCalculo2 = yearsHumanosGatosPerros(2);
+        System.out.println("2 año humano equivale en Humano - Gato - Perro");
+        for (int edad2 : yearCalculo2) {
+            System.out.print("["+edad2+"]");
+        }
+        System.out.println();
+        int[] yearCalculo3 = yearsHumanosGatosPerros(10);
+        System.out.println("10 año humano equivale en Humano - Gato - Perro");
+        for (int edad3 : yearCalculo3) {
+            System.out.print("["+edad3+"]");
+        }
     }
 }
