fa7f5b04685d2c537079e911dedfe75f588ce560
Roman31X <die.haa65@gmail.com>
2024-01-05 11:33:15 -0500

    Ejerccio de suma de numeros impares completada

---
+++ src/main/java/com/Ejercicio/Kyu7/SumaNumeroImpares/SumaNumerosImpares.java
@@ -0,0 +1,7 @@
+package com.Ejercicio.Kyu7.SumaNumeroImpares;
+
+public class SumaNumerosImpares {
+    public static int SumarNumerosImpares(int n){
+        return n * n * n;
+    }
+}
---
+++ src/main/java/com/Ejercicio/Kyu7/SumaNumeroImpares/testSumaDeNumerosImpares.java
@@ -0,0 +1,11 @@
+package com.Ejercicio.Kyu7.SumaNumeroImpares;
+
+import static com.Ejercicio.Kyu7.SumaNumeroImpares.SumaNumerosImpares.SumarNumerosImpares;
+
+public class testSumaDeNumerosImpares {
+    public static void main(String[] args) {
+        System.out.println("Test de prueba de suma de los números impares d eun rango");
+        System.out.println("Suma los números impares hasta el 1 :"+ SumarNumerosImpares(1));
+        System.out.println("Suma los números impares hasta el 42 :"+ SumarNumerosImpares(42));
+    }
+}
