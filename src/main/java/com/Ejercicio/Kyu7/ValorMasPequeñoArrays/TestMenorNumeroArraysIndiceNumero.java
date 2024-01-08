0bc7397a8895533c0f9216ab20dcb00dff8a4dd1
Roman31X <die.haa65@gmail.com>
2024-01-07 20:59:09 -0500

    Correciones de ejerccio

--- src/main/java/com/Ejercicio/Kyu7/ValorMasPequeñoArrays/TestMenorNumeroArraysIndiceNumero.java
+++ src/main/java/com/Ejercicio/Kyu7/ValorMasPequeñoArrays/TestMenorNumeroArraysIndiceNumero.java
@@ -5,8 +5,8 @@ import static com.Ejercicio.Kyu7.ValorMasPequeñoArrays.ValorPequeñoArrays.Pequ
 public class TestMenorNumeroArraysIndiceNumero {
     public static void main(String[] args) {
         System.out.println("Test de prueba de menor Numero de un Arrays por indice y Número");
-        System.out.println("Test de array [1,2,3] INDICE : "+PequellodelArreglo(new int[]{1,2,3},"index") );
-        System.out.println("Test de array [7, 12, 3, 2, 27] VALOR : "+PequellodelArreglo(new int[]{7, 12, 3, 2, 27},"value") );
+        System.out.println("Test de array [1,2,3] INDICE : "+PequellodelArreglo(new int[]{1,2,3,1},"index") );
+        System.out.println("Test de array [7, 12, 3, 2, 27] VALOR : "+PequellodelArreglo(new int[]{7, 12, 3, 2, 27,1},"value") );
         System.out.println("Test de array [7, 12, 3, 2, 27] INDICE : "+PequellodelArreglo(new int[]{7, 12, 3, 2, 27},"index") );
     }
 }
--- src/main/java/com/Ejercicio/Kyu7/ValorMasPequeñoArrays/ValorPequeñoArrays.java
+++ src/main/java/com/Ejercicio/Kyu7/ValorMasPequeñoArrays/ValorPequeñoArrays.java
@@ -2,18 +2,19 @@ package com.Ejercicio.Kyu7.ValorMasPequeñoArrays;
 
 public class ValorPequeñoArrays {
     public static int PequellodelArreglo(final int[] numbers,final String toReturn){
-        int numero = 10;
+        int numero = 0, menor = 10;
         switch(toReturn){
             case "index":
                 for (int i = 0; i < numbers.length; i++) {
-                    if(numero>numbers[i]){
+                    if(menor>numbers[i]){
+                        menor = numbers[i];
                         numero = i;
                     }
                 }
                 break;
             case "value":
                 for (int i = 0; i < numbers.length; i++) {
-                    if(numero>numbers[i]){
+                    if(menor>numbers[i]){
                         numero = numbers[i];
                     }
                 }
