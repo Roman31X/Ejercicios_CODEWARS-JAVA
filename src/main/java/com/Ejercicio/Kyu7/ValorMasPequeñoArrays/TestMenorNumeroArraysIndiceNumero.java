5ae39e8157ef904ca77cd01598999e61ab4a63bf
Roman31X <die.haa65@gmail.com>
2024-01-07 19:08:29 -0500

    Ejercicio en proceso

---
+++ src/main/java/com/Ejercicio/Kyu7/ValorMasPequeñoArrays/TestMenorNumeroArraysIndiceNumero.java
@@ -0,0 +1,12 @@
+package com.Ejercicio.Kyu7.ValorMasPequeñoArrays;
+
+import static com.Ejercicio.Kyu7.ValorMasPequeñoArrays.ValorPequeñoArrays.PequellodelArreglo;
+
+public class TestMenorNumeroArraysIndiceNumero {
+    public static void main(String[] args) {
+        System.out.println("Test de prueba de menor Numero de un Arrays por indice y Número");
+        System.out.println("Test de array [1,2,3] INDICE : "+PequellodelArreglo(new int[]{1,2,3},"index") );
+        System.out.println("Test de array [7, 12, 3, 2, 27] VALOR : "+PequellodelArreglo(new int[]{7, 12, 3, 2, 27},"value") );
+        System.out.println("Test de array [7, 12, 3, 2, 27] INDICE : "+PequellodelArreglo(new int[]{7, 12, 3, 2, 27},"index") );
+    }
+}
