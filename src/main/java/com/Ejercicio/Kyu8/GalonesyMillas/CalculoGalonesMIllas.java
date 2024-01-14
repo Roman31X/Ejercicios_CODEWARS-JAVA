f8bd48e70b7d458c78db405d03694bc921221bf2
Roman31X <die.haa65@gmail.com>
2024-01-13 22:03:16 -0500

    Ejerccios completados

--- src/main/java/com/Ejercicio/Kyu8/GalonesyMillas/CalculoGalonesMIllas.java
+++ src/main/java/com/Ejercicio/Kyu8/GalonesyMillas/CalculoGalonesMIllas.java
@@ -1,4 +1,7 @@
 package com.Ejercicio.Kyu8.GalonesyMillas;
 
 public class CalculoGalonesMIllas {
+    public static boolean RecorridoMillas(double distanceToPump, double mpg, double fuelLeft){
+        return ((mpg * fuelLeft) >= distanceToPump)?true:false;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/GalonesyMillas/TestPruebaRecorridoGalonMillas.java
+++ src/main/java/com/Ejercicio/Kyu8/GalonesyMillas/TestPruebaRecorridoGalonMillas.java
@@ -1,4 +1,12 @@
 package com.Ejercicio.Kyu8.GalonesyMillas;
 
+import static com.Ejercicio.Kyu8.GalonesyMillas.CalculoGalonesMIllas.RecorridoMillas;
+
 public class TestPruebaRecorridoGalonMillas {
+    public static void main(String[] args) {
+        System.out.println("Test de prueba de Millas recorridas por galón");
+        System.out.println("Distancia [50] Millas [25] Galones[2] llegamos :"+RecorridoMillas(50,25,2));
+        System.out.println("Distancia [100] Millas [50] Galones[1] llegamos :"+RecorridoMillas(100,50,1));
+
+    }
 }
