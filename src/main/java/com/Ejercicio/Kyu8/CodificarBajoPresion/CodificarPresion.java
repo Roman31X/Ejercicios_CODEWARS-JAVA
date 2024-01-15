2abb09fce323880a909544928e6a15009cf95f25
Roman31X <die.haa65@gmail.com>
2024-01-14 20:32:04 -0500

    Ejerccio completado

---
+++ src/main/java/com/Ejercicio/Kyu8/CodificarBajoPresion/CodificarPresion.java
@@ -0,0 +1,7 @@
+package com.Ejercicio.Kyu8.CodificarBajoPresion;
+
+public class CodificarPresion {
+    public static int numeroDoble(int i){
+        return i * 2;
+    }
+}
---
+++ src/main/java/com/Ejercicio/Kyu8/CodificarBajoPresion/TestCodificarBajoPresion.java
@@ -0,0 +1,13 @@
+package com.Ejercicio.Kyu8.CodificarBajoPresion;
+
+import static com.Ejercicio.Kyu8.CodificarBajoPresion.CodificarPresion.numeroDoble;
+
+public class TestCodificarBajoPresion {
+    public static void main(String[] args) {
+        System.out.println("Test para Codificar bajo presion el doble del numero ingresado");
+        System.out.println("El doble de 2 es : "+numeroDoble(2));
+        System.out.println("El doble de 100 es : "+numeroDoble(100));
+        System.out.println("El doble de 200 es : "+numeroDoble(200));
+
+    }
+}
