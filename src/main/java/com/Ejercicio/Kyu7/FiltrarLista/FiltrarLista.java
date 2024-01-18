27b5d3b22cae11eff75a741f36192f6f110e0936
Roman31X <die.haa65@gmail.com>
2024-01-17 21:14:05 -0500

    Ejerccio completado

--- src/main/java/com/Ejercicio/Kyu7/FiltrarLista/FiltrarLista.java
+++ src/main/java/com/Ejercicio/Kyu7/FiltrarLista/FiltrarLista.java
@@ -1,14 +1,14 @@
 package com.Ejercicio.Kyu7.FiltrarLista;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FiltrarLista {
     public static List<Object> filtroLista(final List<Object> list){
-        List<Object> nuevaLista = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-
-        }
-        return nuevaLista;
+        List<Object> filteredList = list.stream()
+                .filter(element -> element instanceof Integer)
+                .map(element -> (Integer) element)
+                .collect(Collectors.toList());
+        return filteredList;
     }
 }
--- src/main/java/com/Ejercicio/Kyu7/FiltrarLista/TestFiltrarLista.java
+++ src/main/java/com/Ejercicio/Kyu7/FiltrarLista/TestFiltrarLista.java
@@ -1,7 +1,28 @@
 package com.Ejercicio.Kyu7.FiltrarLista;
 
+import java.util.List;
+
+import static com.Ejercicio.Kyu7.FiltrarLista.FiltrarLista.filtroLista;
+
 public class TestFiltrarLista {
     public static void main(String[] args) {
         System.out.println("Prueba de Filtrado de lista");
+        List<Object> Lista1 = filtroLista(List.of(1, 2, "a", "b"));
+        System.out.println("List [1, 2, \"a\", \"b\"] : Filtrar : ");
+        for (int i = 0; i < Lista1.size(); i++) {
+            System.out.println(Lista1.get(i));
+        }
+        System.out.println("|*******************************|");
+        List<Object> Lista2 = filtroLista(List.of(1, "a", "b", 0, 15));
+        System.out.println("List [1, \"a\", \"b\", 0, 15] : Filtrar : ");
+        for (int i = 0; i < Lista2.size(); i++) {
+            System.out.println(Lista2.get(i));
+        }
+        System.out.println("|*******************************|");
+        List<Object> Lista3 = filtroLista(List.of(1, 2, "aasf", "1", "123", 123));
+        System.out.println("List [1, 2, \"aasf\", \"1\", \"123\", 123] : Filtrar : ");
+        for (int i = 0; i < Lista3.size(); i++) {
+            System.out.println(Lista3.get(i));
+        }
     }
 }
