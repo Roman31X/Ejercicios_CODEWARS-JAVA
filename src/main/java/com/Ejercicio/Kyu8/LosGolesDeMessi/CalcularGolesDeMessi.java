590ce42e36cf2b9250eb9ca6cb0119ec27f34b09
Roman31X <die.haa65@gmail.com>
2024-01-19 15:35:00 -0500

    Test aprovados y datos agregados a README

--- src/main/java/com/Ejercicio/Kyu8/LosGolesDeMessi/CalcularGolesDeMessi.java
+++ src/main/java/com/Ejercicio/Kyu8/LosGolesDeMessi/CalcularGolesDeMessi.java
@@ -1,4 +1,7 @@
 package com.Ejercicio.Kyu8.LosGolesDeMessi;
 
 public class CalcularGolesDeMessi {
+    public static int goals(int laLigaGoals, int copaDelReyGoals, int championsLeagueGoals) {
+        return laLigaGoals+copaDelReyGoals+championsLeagueGoals;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/LosGolesDeMessi/TestGolesMessi.java
+++ src/main/java/com/Ejercicio/Kyu8/LosGolesDeMessi/TestGolesMessi.java
@@ -1,4 +1,20 @@
 package com.Ejercicio.Kyu8.LosGolesDeMessi;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu8.LosGolesDeMessi.CalcularGolesDeMessi.goals;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
 public class TestGolesMessi {
+    public static void main(String[] args) {
+        System.out.println("Grasshopper - Los goles de Messi funcionan");
+    }
+
+    @Test
+    public void basicTests() {
+        assertEquals(0, goals(0, 0, 0));
+        assertEquals(58, goals(43, 10, 5));
+    }
+
 }
