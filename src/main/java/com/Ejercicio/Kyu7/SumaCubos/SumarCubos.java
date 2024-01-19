a6d8f9474a16cf199e6edc320bafbbad399f550b
Roman31X <die.haa65@gmail.com>
2024-01-18 21:01:44 -0500

    Ejerccio agregado Kyu 7

---
+++ src/main/java/com/Ejercicio/Kyu7/SumaCubos/SumarCubos.java
@@ -0,0 +1,4 @@
+package com.Ejercicio.Kyu7.SumaCubos;
+
+public class SumarCubos {
+}
---
+++ src/main/java/com/Ejercicio/Kyu7/SumaCubos/TestSumarCubos.java
@@ -0,0 +1,8 @@
+package com.Ejercicio.Kyu7.SumaCubos;
+
+public class TestSumarCubos {
+    public static void main(String[] args) {
+        System.out.println("Test para suma de cubos indicando por números enteros");
+
+    }
+}
