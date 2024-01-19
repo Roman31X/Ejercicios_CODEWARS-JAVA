87c5c1c3b3450d859a589473915b71c3d4c03791
Roman31X <die.haa65@gmail.com>
2024-01-18 21:15:32 -0500

    Ejerccio completado

--- src/main/java/com/Ejercicio/Kyu7/SumaCubos/SumarCubos.java
+++ src/main/java/com/Ejercicio/Kyu7/SumaCubos/SumarCubos.java
@@ -1,4 +1,13 @@
 package com.Ejercicio.Kyu7.SumaCubos;
 
 public class SumarCubos {
+    public static long sumaCubos(long n){
+        long cuboSuma = 0;
+        for (int i = 1; i <= n; i++) {
+            cuboSuma += (long)Math.pow(i,3);
+            //cuboSuma += (long) i*i*i;
+
+        }
+        return cuboSuma;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/SumaCubos/TestSumarCubos.java
+++ src/main/java/com/Ejercicio/Kyu7/SumaCubos/TestSumarCubos.java
@@ -1,8 +1,18 @@
 package com.Ejercicio.Kyu7.SumaCubos;
 
+import static com.Ejercicio.Kyu7.SumaCubos.SumarCubos.sumaCubos;
+
 public class TestSumarCubos {
     public static void main(String[] args) {
         System.out.println("Test para suma de cubos indicando por números enteros");
+        System.out.println("La suma al buco de [1] es : "+sumaCubos(1));
+        System.out.println("La suma al buco de [2] es : "+sumaCubos(2));
+        System.out.println("La suma al buco de [3] es : "+sumaCubos(3));
+        System.out.println("La suma al buco de [4] es : "+sumaCubos(4));
+        System.out.println("La suma al buco de [10] es : "+sumaCubos(10));
+        System.out.println("La suma al buco de [123] es : "+sumaCubos(123));
+
+
 
     }
 }
