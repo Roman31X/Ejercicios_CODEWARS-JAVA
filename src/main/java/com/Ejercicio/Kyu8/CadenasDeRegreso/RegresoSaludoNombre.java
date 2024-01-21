13324e67ac8868b97f213ba08d7e60771f1f7799
Roman31X <die.haa65@gmail.com>
2024-01-20 21:03:36 -0500

    Ejerccio completado

---
+++ src/main/java/com/Ejercicio/Kyu8/CadenasDeRegreso/RegresoSaludoNombre.java
@@ -0,0 +1,7 @@
+package com.Ejercicio.Kyu8.CadenasDeRegreso;
+
+public class RegresoSaludoNombre {
+    public static String saludoCadena(String name){
+        return "Hello, "+name+" how are you doing today?";
+    }
+}
---
+++ src/main/java/com/Ejercicio/Kyu8/CadenasDeRegreso/TestSalusoCadenaNombre.java
@@ -0,0 +1,10 @@
+package com.Ejercicio.Kyu8.CadenasDeRegreso;
+
+import static com.Ejercicio.Kyu8.CadenasDeRegreso.RegresoSaludoNombre.saludoCadena;
+
+public class TestSalusoCadenaNombre {
+    public static void main(String[] args) {
+        System.out.println("Test de prueba de cadena con saludo de NOMBRE:");
+        System.out.println("Usuario [Ryan] : "+saludoCadena("Ryan"));
+    }
+}
