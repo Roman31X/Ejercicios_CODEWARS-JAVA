1026a886f78bb877099c637d911faa9652646989
Roman31X <die.haa65@gmail.com>
2024-01-20 20:54:05 -0500

    Ejerccio completado Kyu 7

--- src/main/java/com/Ejercicio/Kyu7/MenorMayorArray/ObtenerMenorMayor.java
+++ src/main/java/com/Ejercicio/Kyu7/MenorMayorArray/ObtenerMenorMayor.java
@@ -7,21 +7,20 @@ public class ObtenerMenorMayor {
     public static String MayorMenorCadena(String numbers){
         String numero = "";
         List<Integer> numeros = new ArrayList<>();
-        int menor = 100, mayor = 0;
+        int menor = 1000, mayor = -1;
         for (int i = 0; i < numbers.length(); i++) {
             if(numbers.charAt(i) != ' '){
                 numero += numbers.charAt(i);
-
             }else{
-                System.out.println(numero);
                 numeros.add(Integer.parseInt(numero));
                 numero = "";
             }
         }
+        numeros.add(Integer.parseInt(numero));
         for (Integer digitos: numeros) {
             mayor = (digitos > mayor)?digitos:mayor;
             menor = (digitos < menor)?digitos:menor;
         }
-        return "{"+menor+" "+mayor+"}";
+        return mayor+" "+menor;
     }
 }
--- src/main/java/com/Ejercicio/Kyu7/MenorMayorArray/TestMayorMenorCadenaCaracter.java
+++ src/main/java/com/Ejercicio/Kyu7/MenorMayorArray/TestMayorMenorCadenaCaracter.java
@@ -5,7 +5,12 @@ import static com.Ejercicio.Kyu7.MenorMayorArray.ObtenerMenorMayor.MayorMenorCad
 public class TestMayorMenorCadenaCaracter {
     public static void main(String[] args) {
         System.out.println("Tes de prueba para capturar el Menor y el Mayor de una Cadena de Caracteres");
-        System.out.println("Menor y Mayor de [1 25 -6] : "+MayorMenorCadena("1 -5 -26 8 9 -1"));
+        System.out.println("Menor y Mayor de [1 -5 -26 8 9 -1] : "+MayorMenorCadena("1 -5 -26 8 9 -1"));
+        System.out.println("Menor y Mayor de [8 3 -5 42 -1 0 0 -9 4 7 4 -4] : "+MayorMenorCadena("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+        System.out.println("Menor y Mayor de [1 2 3] : "+MayorMenorCadena("1 2 3"));
+        System.out.println("Menor y Mayor de [1 -5 -6 4 50 -50] : "+MayorMenorCadena("1 -5 -6 4 50 -50"));
+        System.out.println("Menor y Mayor de [5 4 8 9 15 20 -1] : "+MayorMenorCadena("5 4 8 9 15 20 -1"));
+
 
     }
 }
