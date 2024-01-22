1474781912842818fc06cbf232a27b5f4d9f3be4
Roman31X <die.haa65@gmail.com>
2024-01-22 16:51:02 -0500

    Ejerccio completado Kyu 8

---
+++ src/main/java/com/Ejercicio/Kyu8/SigueConElAro/SigueJugandoAro.java
@@ -0,0 +1,7 @@
+package com.Ejercicio.Kyu8.SigueConElAro;
+
+public class SigueJugandoAro {
+    public static String hoopCount(int n){
+        return(n>=10)?"Great, now move on to tricks":"Keep at it until you get it";
+    }
+}
---
+++ src/main/java/com/Ejercicio/Kyu8/SigueConElAro/TestSigueJugandoConAro.java
@@ -0,0 +1,11 @@
+package com.Ejercicio.Kyu8.SigueConElAro;
+
+import static com.Ejercicio.Kyu8.SigueConElAro.SigueJugandoAro.hoopCount;
+
+public class TestSigueJugandoConAro {
+    public static void main(String[] args) {
+        System.out.println("Test de Prueba va saber si seguiré jugando con el Aro");
+        System.out.println("Ahora : "+hoopCount(11));
+        System.out.println("Ahora : "+hoopCount(7));
+    }
+}
