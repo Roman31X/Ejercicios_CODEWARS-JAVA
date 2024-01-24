f2cd029532eaa86a5eca8042cbf32ea950c58f34
Roman31X <die.haa65@gmail.com>
2024-01-23 22:53:24 -0500

    Ejerccio completado

--- src/main/java/com/Ejercicio/Kyu6/NumeroNarcisista/TestNumeroNarcisista.java
+++ src/main/java/com/Ejercicio/Kyu6/NumeroNarcisista/TestNumeroNarcisista.java
@@ -1,7 +1,13 @@
 package com.Ejercicio.Kyu6.NumeroNarcisista;
 
+import static com.Ejercicio.Kyu6.NumeroNarcisista.VerificarNumeroNarcisista.esNarcisista;
+
 public class TestNumeroNarcisista {
     public static void main(String[] args) {
+        System.out.println("Test de prueba de número Narcisista");
+        System.out.println("Numero [153] es : "+esNarcisista(153));
+        System.out.println("Numero [1634] es : "+esNarcisista(1634));
+        System.out.println("Numero [112] es : "+esNarcisista(112));
 
     }
 }
--- src/main/java/com/Ejercicio/Kyu6/NumeroNarcisista/VerificarNumeroNarcisista.java
+++ src/main/java/com/Ejercicio/Kyu6/NumeroNarcisista/VerificarNumeroNarcisista.java
@@ -2,6 +2,19 @@ package com.Ejercicio.Kyu6.NumeroNarcisista;
 
 public class VerificarNumeroNarcisista {
     public static boolean esNarcisista(int number){
-        return true;
+        String numero = String.valueOf(number);
+        int[] digitos = new int[numero.length()];
+        int total = 0;
+        for (int i = 0; i < numero.length(); i++) {
+            digitos[i] = (int) Math.pow(Integer.parseInt(String.valueOf(numero.charAt(i))), numero.length());
+        }
+        for (int digito:digitos) {
+            total+=digito;
+        }
+        if(number == total){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
