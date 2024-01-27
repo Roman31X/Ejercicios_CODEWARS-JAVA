673f47f87d166cbe199b8fe472200a0d2eb50d0b
Roman31X <die.haa65@gmail.com>
2024-01-27 16:44:43 -0500

    Ejerccio terminado Kyu6

--- src/main/java/com/Ejercicio/Kyu6/ComprarUnCarro/CalculoComprarVehiculo.java
+++ src/main/java/com/Ejercicio/Kyu6/ComprarUnCarro/CalculoComprarVehiculo.java
@@ -2,6 +2,20 @@ package com.Ejercicio.Kyu6.ComprarUnCarro;
 
 public class CalculoComprarVehiculo {
     public static int[] nuevoCarro(int startPriceOld, int startPriceNew, int savingperMonth, double percentLossByMonth){
+        double precioViejo = startPriceOld, precioNuevo = startPriceNew, ahorroMes = 0;
+        int mes = 0;
+        while((precioViejo + ahorroMes) < precioNuevo){
+            mes++;
+            if (mes % 2 == 0) {
+                percentLossByMonth = percentLossByMonth +  0.5;
+            }
 
+            precioViejo = (precioViejo - precioViejo * (percentLossByMonth/100));
+            precioNuevo = (precioNuevo - precioNuevo * (percentLossByMonth/100));
+            ahorroMes += savingperMonth;
+        }
+        int restanteDinero = (int) Math.round(precioViejo + ahorroMes - precioNuevo);
+
+        return new int[]{mes, restanteDinero};
     }
 }
--- src/main/java/com/Ejercicio/Kyu6/ComprarUnCarro/TestCompraUnCarro.java
+++ src/main/java/com/Ejercicio/Kyu6/ComprarUnCarro/TestCompraUnCarro.java
@@ -1,8 +1,13 @@
 package com.Ejercicio.Kyu6.ComprarUnCarro;
 
+import java.util.Arrays;
+
+import static com.Ejercicio.Kyu6.ComprarUnCarro.CalculoComprarVehiculo.nuevoCarro;
+
 public class TestCompraUnCarro {
     public static void main(String[] args) {
         System.out.println("Test de meses y ahorros para comprar un carro");
-        System.out.println("DATOS [] : ");
+        int[] datos = nuevoCarro(2000,8000,1000,1.5);
+        System.out.println("DATOS [2000,8000,1000,1.5] : "+ Arrays.toString(datos));
     }
 }
