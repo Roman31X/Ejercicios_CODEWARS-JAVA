d847b544a9a08684fa193de713a830b65674c867
Roman31X <die.haa65@gmail.com>
2024-01-26 21:52:18 -0500

    Ejerccio completado

--- src/main/java/com/Ejercicio/Kyu6/SistemaContraseña/TestVerificacionContraseniaaSistema.java
+++ src/main/java/com/Ejercicio/Kyu6/SistemaContraseña/TestVerificacionContraseniaaSistema.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu6.SistemaContraseña;
 
+import static com.Ejercicio.Kyu6.SistemaContraseña.VerificarContraseñaSistema.verificarContrasenia;
+
 public class TestVerificacionContraseniaaSistema {
+    public static void main(String[] args) {
+        System.out.println("Test de prueba de Verificación de Contraseñas");
+        System.out.println("Contraseña [1,1,0,1,0,0,0,1,1,0,0,0,1,0,1,1] : es ["+verificarContrasenia(new int[]{1,1,0,12,0,0,0,1,1,0,0,0,1,0,1,1})+"]");
+        System.out.println("Contraseña [1,1,0,0,0,0,1,1,0] : es ["+verificarContrasenia(new int[]{1,1,0,0,0,0,1,1,0})+"]");
+    }
 }
--- src/main/java/com/Ejercicio/Kyu6/SistemaContraseña/VerificarContraseñaSistema.java
+++ src/main/java/com/Ejercicio/Kyu6/SistemaContraseña/VerificarContraseñaSistema.java
@@ -1,4 +1,25 @@
 package com.Ejercicio.Kyu6.SistemaContraseña;
 
 public class VerificarContraseñaSistema {
+    public static String verificarContrasenia(final int[] key){
+        int n = (int) Math.sqrt(key.length);
+        // Verificar si la matriz es cuadrada
+        if (n * n != key.length) {
+            return "No";
+        }
+        // Verificar simetría respecto al punto central
+        for (int i = 0; i < n / 2; i++) {
+            for (int j = 0; j < n; j++) {
+                int topLeft = key[i * n + j];
+                int bottomRight = key[(n - 1 - i) * n + (n - 1 - j)];
+
+                // Si no es simétrico, devolver "No"
+                if (topLeft != bottomRight) {
+                    return "No";
+                }
+            }
+        }
+        // Si pasó todas las verificaciones, devolver "Sí"
+        return "Yes";
+    }
 }
