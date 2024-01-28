58f0688e50c9d77d10bee1025cf1088c934f03a0
Roman31X <die.haa65@gmail.com>
2024-01-27 23:52:30 -0500

    Buscando solucion a kyu7 reversa

--- src/main/java/com/Ejercicio/Kyu7/PalabraInversa/InvertirPalabra.java
+++ src/main/java/com/Ejercicio/Kyu7/PalabraInversa/InvertirPalabra.java
@@ -2,23 +2,32 @@ package com.Ejercicio.Kyu7.PalabraInversa;
 
 public class InvertirPalabra {
     public static String reversoPalabra(final String original){
-        // Convert the word to a char array for easier reversal
-        char[] charArray = original.toCharArray();
+        // Dividir la cadena en palabras
+        String[] palabras = original.split("(\\b)");
 
-        // Reverse the char array in-place
-        int izquierda = 0, derecha = charArray.length - 1;
-        while (izquierda < derecha) {
-            char temp = charArray[izquierda];
-            charArray[izquierda] = charArray[derecha];
-            charArray[derecha] = temp;
-            izquierda++;
-            derecha--;
+        // Invertir cada palabra en el arreglo
+        for (int i = 0; i < palabras.length; i++) {
+            // Convertir la palabra a un arreglo de caracteres para invertirla más fácilmente
+            char[] caracteres = palabras[i].toCharArray();
+
+            // Invertir el arreglo de caracteres en su lugar
+            int izquierda = 0, derecha = caracteres.length - 1;
+            while (izquierda < derecha) {
+                char temp = caracteres[izquierda];
+                caracteres[izquierda] = caracteres[derecha];
+                caracteres[derecha] = temp;
+                izquierda++;
+                derecha--;
+            }
+
+            // Convertir el arreglo de caracteres invertido de vuelta a una cadena
+            palabras[i] = new String(caracteres);
         }
-        String invertido = "";
-        for (int i = 0; i < charArray.length; i++) {
-            invertido += charArray[i];
+        for (int i = 0; i < palabras.length; i++) {
+            System.out.print(palabras[i]);
         }
-        // Convert the reversed char array back to a string
-        return invertido;
+
+        // Unir las palabras invertidas en una cadena
+        return String.join("", palabras);
     }
 }
--- src/main/java/com/Ejercicio/Kyu7/PalabraInversa/TestInvertiPalabra.java
+++ src/main/java/com/Ejercicio/Kyu7/PalabraInversa/TestInvertiPalabra.java
@@ -6,5 +6,7 @@ public class TestInvertiPalabra {
     public static void main(String[] args) {
         System.out.println("Test de inversion de palabra");
         System.out.println("\"!Los perros ladran¡\" : "+reversoPalabra("!Los perros ladran¡"));
+        System.out.println("\"[dios.] lazy the over jumps...\" : "+reversoPalabra("[dios.] lazy the over jumps..."));
+
     }
 }
