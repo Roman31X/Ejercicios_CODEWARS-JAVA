188d0015be92b2ea9e96b07fa4ab2bfe00424c45
Roman31X <die.haa65@gmail.com>
2024-01-28 21:34:39 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu8/MatricesDeSuma/SumaMatris.java
+++ src/main/java/com/Ejercicio/Kyu8/MatricesDeSuma/SumaMatris.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu8.MatricesDeSuma;
 
 public class SumaMatris {
+    public static double sumaMatrsi(double[] numbers){
+        double suma = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            suma+=numbers[i];
+        }
+        return suma;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/MatricesDeSuma/TestSumaMatris.java
+++ src/main/java/com/Ejercicio/Kyu8/MatricesDeSuma/TestSumaMatris.java
@@ -1,4 +1,15 @@
 package com.Ejercicio.Kyu8.MatricesDeSuma;
 
+import static com.Ejercicio.Kyu8.MatricesDeSuma.SumaMatris.sumaMatrsi;
+
 public class TestSumaMatris {
+    public static void main(String[] args) {
+        System.out.println("Tes de suma de números de matriz ");
+        System.out.println("Matriz [] : "+sumaMatrsi(new double[]{}));
+        System.out.println("Matriz [-2.398] : "+sumaMatrsi(new double[]{-2.398}));
+        System.out.println("Matriz [1, 2, 3] : "+sumaMatrsi(new double[]{1, 2, 3}));
+        System.out.println("Matriz [1.1, 2.2, 3.3] : "+sumaMatrsi(new double[]{1.1, 2.2, 3.3}));
+        System.out.println("Matriz [1, 5.2, 4, 0, -1] : "+sumaMatrsi(new double[]{1, 5.2, 4, 0, -1}));
+        System.out.println("Matriz [30, 89, 100, 101] : "+sumaMatrsi(new double[]{30, 89, 100, 101}));
+    }
 }
