14831330b3d80c1e5a6c237cd6baf1682d5a6977
Roman31X <die.haa65@gmail.com>
2024-01-30 16:18:22 -0500

    Ejerccioc completado

---
+++ src/main/java/com/Ejercicio/Kyu8/ParoImpar/CalcularParoImpar.java
@@ -0,0 +1,7 @@
+package com.Ejercicio.Kyu8.ParoImpar;
+
+public class CalcularParoImpar {
+    public static String parOImpar(int number){
+        return (number%2==0)?"Even":"Odd";
+    }
+}
---
+++ src/main/java/com/Ejercicio/Kyu8/ParoImpar/TestNumeroParOImpar.java
@@ -0,0 +1,12 @@
+package com.Ejercicio.Kyu8.ParoImpar;
+
+import static com.Ejercicio.Kyu8.ParoImpar.CalcularParoImpar.parOImpar;
+
+public class TestNumeroParOImpar {
+    public static void main(String[] args) {
+        System.out.println("Test para saber que tipo de numero es el ingresado");
+        System.out.println("Número [2] : "+parOImpar(2));
+        System.out.println("Número [5] : "+parOImpar(5));
+
+    }
+}
