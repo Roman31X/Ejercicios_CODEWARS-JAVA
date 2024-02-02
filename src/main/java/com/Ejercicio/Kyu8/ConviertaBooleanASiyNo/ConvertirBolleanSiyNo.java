236082ca7b6c493288656dc25f4e67a39dc14a9a
Roman31X <die.haa65@gmail.com>
2024-02-02 13:47:12 -0500

    Ejercicio completado

---
+++ src/main/java/com/Ejercicio/Kyu8/ConviertaBooleanASiyNo/ConvertirBolleanSiyNo.java
@@ -0,0 +1,7 @@
+package com.Ejercicio.Kyu8.ConviertaBooleanASiyNo;
+
+public class ConvertirBolleanSiyNo {
+    public static String booleanPalabra(boolean b){
+        return (b)?"Yes":"No";
+    }
+}
---
+++ src/main/java/com/Ejercicio/Kyu8/ConviertaBooleanASiyNo/TestConversionBolleanoaSINO.java
@@ -0,0 +1,11 @@
+package com.Ejercicio.Kyu8.ConviertaBooleanASiyNo;
+
+import static com.Ejercicio.Kyu8.ConviertaBooleanASiyNo.ConvertirBolleanSiyNo.booleanPalabra;
+
+public class TestConversionBolleanoaSINO {
+    public static void main(String[] args) {
+        System.out.println("Test convertir boolean a SI Y NO");
+        System.out.println("Boolean [true] : "+booleanPalabra(true));
+        System.out.println("Boolean [false] : "+booleanPalabra(false));
+    }
+}
