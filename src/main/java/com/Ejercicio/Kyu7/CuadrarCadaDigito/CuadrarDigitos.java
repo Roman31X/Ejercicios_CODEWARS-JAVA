dbbfabe0486d67d40294f6416061a350632f2b27
Roman31X <die.haa65@gmail.com>
2024-02-03 12:12:55 -0500

    Ejercicico completado

--- src/main/java/com/Ejercicio/Kyu7/CuadrarCadaDigito/CuadrarDigitos.java
+++ src/main/java/com/Ejercicio/Kyu7/CuadrarCadaDigito/CuadrarDigitos.java
@@ -1,4 +1,16 @@
 package com.Ejercicio.Kyu7.CuadrarCadaDigito;
 
 public class CuadrarDigitos {
+    public static int cuadrarDigito(int n){
+        StringBuilder result = new StringBuilder();
+
+        String numberString = Integer.toString(n);
+
+        for (char digitChar : numberString.toCharArray()) {
+            int digit = Character.getNumericValue(digitChar);
+            result.append(digit * digit);
+        }
+
+        return Integer.parseInt(result.toString());
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/CuadrarCadaDigito/TestCuadrarDigitosEntero.java
+++ src/main/java/com/Ejercicio/Kyu7/CuadrarCadaDigito/TestCuadrarDigitosEntero.java
@@ -1,7 +1,11 @@
 package com.Ejercicio.Kyu7.CuadrarCadaDigito;
 
+import static com.Ejercicio.Kyu7.CuadrarCadaDigito.CuadrarDigitos.cuadrarDigito;
+
 public class TestCuadrarDigitosEntero {
     public static void main(String[] args) {
-
+        System.out.println("Test para cuadrar dígitos de un número entero");
+        System.out.println("Número [9119] : "+cuadrarDigito(9119));
+        System.out.println("Número [0] : "+cuadrarDigito(0));
     }
 }
