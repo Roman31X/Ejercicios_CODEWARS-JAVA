29b85de586bb2ee8c002f26d9f665eddc65502a4
Roman31X <die.haa65@gmail.com>
2024-02-04 17:38:00 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu7/DosCadenaAUno/TestDosaUnaCadena.java
+++ src/main/java/com/Ejercicio/Kyu7/DosCadenaAUno/TestDosaUnaCadena.java
@@ -1,7 +1,12 @@
 package com.Ejercicio.Kyu7.DosCadenaAUno;
 
+import static com.Ejercicio.Kyu7.DosCadenaAUno.SeleccionCadenasDosaUno.largCadena;
+
 public class TestDosaUnaCadena {
     public static void main(String[] args) {
+        System.out.println("Test uniendo cadenas sin repetir caracteres");
+        System.out.println("Cadena [aretheyhere] - [yestheyarehere] : "+largCadena("aretheyhere", "yestheyarehere"));
+        System.out.println("Cadena [loopingisfunbutdangerous] - [lessdangerousthancoding] : "+largCadena("loopingisfunbutdangerous", "lessdangerousthancoding"));
 
     }
 }
