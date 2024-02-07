ba751edaf619d483b2b404e8424fe51679f55156
Roman31X <die.haa65@gmail.com>
2024-02-07 09:49:30 -0500

    Ejerccicio resuelto

--- src/main/java/com/Ejercicio/Kyu7/FuncionAritmetica/CalculeFuncionAritmetica.java
+++ src/main/java/com/Ejercicio/Kyu7/FuncionAritmetica/CalculeFuncionAritmetica.java
@@ -1,4 +1,13 @@
 package com.Ejercicio.Kyu7.FuncionAritmetica;
 
 public class CalculeFuncionAritmetica {
+    public static int aritmetica(int a, int b, String operator){
+        switch (operator){
+            case "add": return a+b;
+            case "subtract": return a-b;
+            case "multiply": return a*b;
+            case "divide": return a/b;
+            default: return 0;
+        }
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/FuncionAritmetica/TestFuncionAritmetrica.java
+++ src/main/java/com/Ejercicio/Kyu7/FuncionAritmetica/TestFuncionAritmetrica.java
@@ -1,7 +1,13 @@
 package com.Ejercicio.Kyu7.FuncionAritmetica;
 
+import static com.Ejercicio.Kyu7.FuncionAritmetica.CalculeFuncionAritmetica.aritmetica;
+
 public class TestFuncionAritmetrica {
     public static void main(String[] args) {
-
+        System.out.println("Test para realizar operaciones aritméticas");
+        System.out.println("Suma [5-2] : "+aritmetica(5, 2,"add"));
+        System.out.println("Resta [5-2] : "+aritmetica(5, 2,"subtract"));
+        System.out.println("Multiplicar [5-2] : "+aritmetica(5, 2,"multiply"));
+        System.out.println("Division [5-2] : "+aritmetica(5, 2,"divide"));
     }
 }
