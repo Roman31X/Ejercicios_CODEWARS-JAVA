6f3e89c9d8a83b4fc5622c921fa1a63e0b7b9112
Roman31X <die.haa65@gmail.com>
2024-02-07 10:27:46 -0500

    Ejercicio suma consecutiva terminado

--- src/main/java/com/Ejercicio/Kyu6/SumaConsecutivas/CalculoSumaConsecutiva.java
+++ src/main/java/com/Ejercicio/Kyu6/SumaConsecutivas/CalculoSumaConsecutiva.java
@@ -1,5 +1,23 @@
 package com.Ejercicio.Kyu6.SumaConsecutivas;
 
-public class CalculoSumaConsecutiva {
+import java.util.*;
 
+public class CalculoSumaConsecutiva {
+    public static List<Integer> sumaConsecutiva(List<Integer> s){
+        List<Integer> resultado = new ArrayList<>();
+
+        int sum = s.get(0);
+        for (int i = 1; i < s.size(); i++) {
+            if (s.get(i - 1).equals(s.get(i))) {
+                sum += s.get(i);
+            } else {
+                resultado.add(sum);
+                sum = s.get(i);
+            }
+        }
+
+        resultado.add(sum);
+
+        return resultado;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu6/SumaConsecutivas/TestSumaConsecutiva.java
+++ src/main/java/com/Ejercicio/Kyu6/SumaConsecutivas/TestSumaConsecutiva.java
@@ -1,7 +1,16 @@
 package com.Ejercicio.Kyu6.SumaConsecutivas;
 
+import static com.Ejercicio.Kyu6.SumaConsecutivas.CalculoSumaConsecutiva.sumaConsecutiva;
+import java.util.*;
+
 public class TestSumaConsecutiva {
     public static void main(String[] args) {
-
+        System.out.println("Test para suma consecutiva de números enteros en un LIST");
+        List<Integer> i = Arrays.asList(1,4,4,4,0,4,3,3,1);
+        List<Integer> nuevo = sumaConsecutiva(i);
+        System.out.println("List[1,4,4,4,0,4,3,3,1] : "+nuevo.toString());
+        List<Integer> o = Arrays.asList(-5,-5,7,7,12,0);
+        List<Integer> nuevo2 = sumaConsecutiva(o);
+        System.out.println("List[-5,-5,7,7,12,0] : "+nuevo2.toString());
     }
 }
