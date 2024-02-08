5dedb3d62fc24f5dc3abd0938838956f29955f05
Roman31X <die.haa65@gmail.com>
2024-02-08 15:06:12 -0500

    Modificacion de ejercicio

--- src/main/java/com/Ejercicio/Kyu7/EscuelaDeManejo/CalcularCostoEscuelaManejo.java
+++ src/main/java/com/Ejercicio/Kyu7/EscuelaDeManejo/CalcularCostoEscuelaManejo.java
@@ -6,27 +6,22 @@ public class CalcularCostoEscuelaManejo {
         boolean estado = true;
         int total = 0, i = 0;
         while (estado){
-            if(i == 0){
-                if(mins < 75){
-                    precio = "una";
-                    estado = false;
-                }
-            }else if(i == 1){
-                if(mins >75){
-                    precio = "una";
-                }else if(mins < 60){
-                    precio = "media";
-                }else{
-                    precio = "salida";
-                }
+            if(mins < 75 && i == 0){
+                precio = "una";
+                estado = false;
+            }else if(mins >75){
+                precio = "una";
+            }else if(mins < 60 && mins > 0){
+                precio = "media";
+            }else{
+                precio = "salida";
             }
             switch (precio){
-                case "una":total+=30;mins-=60;break;
+                case "una":total+=30;mins-=60;i=1;break;
                 case "media":total+=10;mins-=30;break;
                 case "salida":estado = false;
             }
             precio="";
-            i=1;
         }
         return total;
     }
