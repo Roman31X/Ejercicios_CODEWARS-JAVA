19bd3a9e93be2c7a37f06e762db7d60b5583d301
Roman31X <die.haa65@gmail.com>
2024-02-08 12:44:52 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu7/NumerosPersonasAutobus/ContarPersonasAutobus.java
+++ src/main/java/com/Ejercicio/Kyu7/NumerosPersonasAutobus/ContarPersonasAutobus.java
@@ -1,4 +1,13 @@
 package com.Ejercicio.Kyu7.NumerosPersonasAutobus;
 
+import java.util.ArrayList;
+
 public class ContarPersonasAutobus {
+    public static int contarPersonas(ArrayList<int[]> stops){
+        int personasEnBus = 0;
+        for (int[] parada : stops) {
+            personasEnBus += parada[0] - parada[1];
+        }
+        return Math.max(personasEnBus, 0);
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/NumerosPersonasAutobus/TestContadorAutobusStop.java
+++ src/main/java/com/Ejercicio/Kyu7/NumerosPersonasAutobus/TestContadorAutobusStop.java
@@ -1,7 +1,23 @@
 package com.Ejercicio.Kyu7.NumerosPersonasAutobus;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static com.Ejercicio.Kyu7.NumerosPersonasAutobus.ContarPersonasAutobus.contarPersonas;
+import static junit.framework.TestCase.assertEquals;
+
 public class TestContadorAutobusStop {
     public static void main(String[] args) {
+        System.out.println("Test para número de personas en el autobús");
+    }
 
+    @Test
+    public void test1() {
+        ArrayList<int[]> list = new ArrayList<int[]>();
+        list.add(new int[] {10,0});
+        list.add(new int[] {3,5});
+        list.add(new int[] {2,5});
+        assertEquals(5, contarPersonas(list));
     }
 }
