5efede77eb2039e16ba446f0cffe26e77ca72eec
Roman31X <die.haa65@gmail.com>
2024-02-09 13:35:37 -0500

    Esperar respuesta de comentario de tiempo en servidor

--- src/main/java/com/Ejercicio/Kyu6/SumaNumerosSumaSumasHataSuma/CalcularSumaMasSuma.java
+++ src/main/java/com/Ejercicio/Kyu6/SumaNumerosSumaSumasHataSuma/CalcularSumaMasSuma.java
@@ -14,5 +14,11 @@ public class CalcularSumaMasSuma {
 
         result = result.add(BigInteger.ONE).multiply(result).divide(BigInteger.valueOf(2));
         return result;
+        /*
+        *
+        BigInteger N = BigInteger.valueOf(n);
+        BigInteger result = N.multiply(N.add(BigInteger.ONE)).multiply(N.add(BigInteger.valueOf(2))).divide(BigInteger.valueOf(6));
+        return result;
+        * */
     }
 }
--- src/main/java/com/Ejercicio/Kyu6/SumaNumerosSumaSumasHataSuma/TestCaclularSumaDeSuma.java
+++ src/main/java/com/Ejercicio/Kyu6/SumaNumerosSumaSumasHataSuma/TestCaclularSumaDeSuma.java
@@ -6,5 +6,7 @@ public class TestCaclularSumaDeSuma {
     public static void main(String[] args) {
         System.out.println("Test Sumar los números, sumar las sumas y sumar los números hasta esa suma");
         System.out.println("Número [3] : "+sumaSuma(3));
+        System.out.println("Número [5] : "+sumaSuma(5));
+        System.out.println("Número [100] : "+sumaSuma(100));
     }
 }
