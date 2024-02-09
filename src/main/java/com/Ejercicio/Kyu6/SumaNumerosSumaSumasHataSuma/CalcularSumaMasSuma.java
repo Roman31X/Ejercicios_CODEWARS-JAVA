6f1a9d1cb5a57be8cb314c6193208d96ca5157b3
Roman31X <die.haa65@gmail.com>
2024-02-09 13:34:40 -0500

    Evaluar reduccion de codigo

--- src/main/java/com/Ejercicio/Kyu6/SumaNumerosSumaSumasHataSuma/CalcularSumaMasSuma.java
+++ src/main/java/com/Ejercicio/Kyu6/SumaNumerosSumaSumasHataSuma/CalcularSumaMasSuma.java
@@ -6,17 +6,13 @@ public class CalcularSumaMasSuma {
     public static BigInteger sumaSuma(int n){
         BigInteger sum = BigInteger.ZERO;
         BigInteger result = BigInteger.ZERO;
-        BigInteger auxiliar = BigInteger.ZERO;
-
 
         for (int i = 1; i <= n; i++) {
             sum = sum.add(BigInteger.valueOf(i));
             result = result.add(sum);
         }
-        auxiliar = result;
-        result = result.add(BigInteger.valueOf(1));
-        result = result.multiply(auxiliar);
-        result = result.divide(BigInteger.valueOf(2));
+
+        result = result.add(BigInteger.ONE).multiply(result).divide(BigInteger.valueOf(2));
         return result;
     }
 }
