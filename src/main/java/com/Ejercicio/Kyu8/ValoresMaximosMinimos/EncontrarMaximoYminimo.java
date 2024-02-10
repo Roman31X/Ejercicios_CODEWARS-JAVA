19cb10b9da225695e94b437c50dfbbf8b53ca139
Roman31X <die.haa65@gmail.com>
2024-02-10 16:16:52 -0500

    Ejerccico terminado

--- src/main/java/com/Ejercicio/Kyu8/ValoresMaximosMinimos/EncontrarMaximoYminimo.java
+++ src/main/java/com/Ejercicio/Kyu8/ValoresMaximosMinimos/EncontrarMaximoYminimo.java
@@ -1,4 +1,22 @@
 package com.Ejercicio.Kyu8.ValoresMaximosMinimos;
 
 public class EncontrarMaximoYminimo {
+    public static int min(int[] list){
+        int menor = Integer.MAX_VALUE;
+        for (int i = 0; i < list.length; i++) {
+            if(menor > list[i]){
+                menor = list[i];
+            }
+        }
+        return menor;
+    }
+    public static int max(int[] list){
+        int mayor= Integer.MIN_VALUE;
+        for (int i = 0; i < list.length; i++) {
+            if(mayor < list[i]){
+                mayor = list[i];
+            }
+        }
+        return mayor;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/ValoresMaximosMinimos/TestMaximoyMinimo.java
+++ src/main/java/com/Ejercicio/Kyu8/ValoresMaximosMinimos/TestMaximoyMinimo.java
@@ -1,7 +1,21 @@
 package com.Ejercicio.Kyu8.ValoresMaximosMinimos;
 
+import org.testng.annotations.Test;
+
+import static com.Ejercicio.Kyu8.ValoresMaximosMinimos.EncontrarMaximoYminimo.*;
+import static junit.framework.TestCase.assertEquals;
+
 public class TestMaximoyMinimo {
     public static void main(String[] args) {
-
+        System.out.println("Test para encontrar al mayor y menor numero de una Arrays List");
     }
+
+    @Test
+    public void testExamples() {
+        assertEquals(-110, min(new int[]{-52, 56, 30, 29, -54, 0, -110}));
+        assertEquals(0, min(new int[]{42, 54, 65, 87, 0}));
+        assertEquals(566, max(new int[]{4,6,2,1,9,63,-134,566}));
+        assertEquals(5, max(new int[]{5}));
+    }
+
 }
