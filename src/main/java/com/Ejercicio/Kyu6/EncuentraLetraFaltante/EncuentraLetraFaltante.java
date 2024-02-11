872abea4594977105608d5e5b721a71443915fbc
Roman31X <die.haa65@gmail.com>
2024-02-11 13:11:27 -0500

    Ejercicicio completado con su test

--- src/main/java/com/Ejercicio/Kyu6/EncuentraLetraFaltante/EncuentraLetraFaltante.java
+++ src/main/java/com/Ejercicio/Kyu6/EncuentraLetraFaltante/EncuentraLetraFaltante.java
@@ -1,4 +1,12 @@
 package com.Ejercicio.Kyu6.EncuentraLetraFaltante;
 
 public class EncuentraLetraFaltante {
+    public static char letraFaltante(char[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i + 1] - array[i] > 1) {
+                return (char) (array[i] + 1);
+            }
+        }
+        throw new IllegalArgumentException("Input array is invalid or does not contain a missing letter.");
+    }
 }
--- src/main/java/com/Ejercicio/Kyu6/EncuentraLetraFaltante/TestEncontrarLetraFaltante.java
+++ src/main/java/com/Ejercicio/Kyu6/EncuentraLetraFaltante/TestEncontrarLetraFaltante.java
@@ -1,8 +1,17 @@
 package com.Ejercicio.Kyu6.EncuentraLetraFaltante;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu6.EncuentraLetraFaltante.EncuentraLetraFaltante.letraFaltante;
+import static org.junit.Assert.assertEquals;
+
 public class TestEncontrarLetraFaltante {
     public static void main(String[] args) {
         System.out.println("Test para encontrar letra faltante");
     }
-
+    @Test
+    public void exampleTests() {
+        assertEquals('e', letraFaltante(new char[] { 'a','b','c','d','f' }));
+        assertEquals('P', letraFaltante(new char[] { 'O','Q','R','S' }));
+    }
 }
