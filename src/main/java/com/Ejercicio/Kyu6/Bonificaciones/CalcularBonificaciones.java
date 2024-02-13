ca8ba076f3e53dd05682a13feee7a433527fd429
Roman31X <die.haa65@gmail.com>
2024-02-12 23:30:27 -0500

    Linak agregados a Readme

--- src/main/java/com/Ejercicio/Kyu6/Bonificaciones/CalcularBonificaciones.java
+++ src/main/java/com/Ejercicio/Kyu6/Bonificaciones/CalcularBonificaciones.java
@@ -1,19 +1,16 @@
 package com.Ejercicio.Kyu6.Bonificaciones;
 
-import java.util.Arrays;
-
 public class CalcularBonificaciones {
     public static long[] bonus(int[] arr, long s){
         long totalDays = 0;
         for (int days : arr) {
             totalDays += days;
         }
-
         long[] bonuses = new long[arr.length];
         for (int i = 0; i < arr.length; i++) {
             bonuses[i] = Math.round((double) arr[i] * s / totalDays);
         }
-
         return bonuses;
+
     }
 }
