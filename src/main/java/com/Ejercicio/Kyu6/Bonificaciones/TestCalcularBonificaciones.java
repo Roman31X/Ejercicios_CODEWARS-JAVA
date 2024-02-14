4ef53b9a15e95bfdd6e043882a1143cecaa6191d
Roman31X <die.haa65@gmail.com>
2024-02-13 23:14:31 -0500

    Ejercicio analisado en proceso

--- src/main/java/com/Ejercicio/Kyu6/Bonificaciones/TestCalcularBonificaciones.java
+++ src/main/java/com/Ejercicio/Kyu6/Bonificaciones/TestCalcularBonificaciones.java
@@ -1,7 +1,6 @@
 package com.Ejercicio.Kyu6.Bonificaciones;
 
 import org.testng.annotations.Test;
-
 import static com.Ejercicio.Kyu6.Bonificaciones.CalcularBonificaciones.bonus;
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
@@ -34,7 +33,5 @@ public class TestCalcularBonificaciones {
         dotest(new int[] {22, 3, 15}, 18228, new long[] {1860, 13640, 2728});
         dotest(new int[] {8, 14, 11}, 23541, new long[] {10241, 5852, 7448});
         dotest(new int[] {8, 20, 17}, 25281, new long[] {13515, 5406, 6360});
-        dotest(new int[] {6, 15, 23}, 22156, new long[] {13340, 5336, 3480});
-        dotest(new int[] {7, 9, 12}, 10880, new long[] {4608, 3584, 2688});
     }
 }
