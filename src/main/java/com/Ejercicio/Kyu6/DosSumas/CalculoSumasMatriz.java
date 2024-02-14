d6016beca661727362bc0f1d7e6f5ab372838578
Roman31X <die.haa65@gmail.com>
2024-02-14 14:01:26 -0500

    Ejercicioc completado

--- src/main/java/com/Ejercicio/Kyu6/DosSumas/CalculoSumasMatriz.java
+++ src/main/java/com/Ejercicio/Kyu6/DosSumas/CalculoSumasMatriz.java
@@ -1,4 +1,21 @@
 package com.Ejercicio.Kyu6.DosSumas;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CalculoSumasMatriz {
+    public static int[] twoSum(int[] numbers, int target) {
+        Map<Integer, Integer> numIndexMap = new HashMap<>();
+
+        for (int i = 0; i < numbers.length; i++) {
+            int complement = target - numbers[i];
+            if (numIndexMap.containsKey(complement)) {
+                return new int[]{numIndexMap.get(complement), i};
+            }
+            numIndexMap.put(numbers[i], i);
+        }
+
+        // No se espera que llegue a este punto debido a la garantía de entrada válida
+        return null;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu6/DosSumas/TestDosSumasArrays.java
+++ src/main/java/com/Ejercicio/Kyu6/DosSumas/TestDosSumasArrays.java
@@ -1,4 +1,19 @@
 package com.Ejercicio.Kyu6.DosSumas;
 
+import java.util.Arrays;
+
+import static com.Ejercicio.Kyu6.DosSumas.CalculoSumasMatriz.twoSum;
+
 public class TestDosSumasArrays {
+    public static void main(String[] args) {
+        System.out.println("Test ara suma de dos números iguales Dentro de un Arrays");
+        int[] nuevo = twoSum(new int[]{1,2,3},          4);
+        System.out.println("Array [1,2,3] : "+ Arrays.toString(nuevo));
+        int[] nuevo1 = twoSum(new int[]{1234,5678,9012}, 14690);
+        System.out.println("Array [1234,5678,9012] : "+ Arrays.toString(nuevo1));
+        int[] nuevo2 = twoSum(new int[]{2,2,3},          4);
+        System.out.println("Array [2,2,3] : "+ Arrays.toString(nuevo2));
+        int[] nuevo3 = twoSum(new int[]{2,3,1},          4);
+        System.out.println("Array [2,3,1] : "+ Arrays.toString(nuevo3));
+    }
 }
