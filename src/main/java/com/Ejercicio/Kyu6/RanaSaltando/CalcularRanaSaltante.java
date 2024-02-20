a07bfce056015bf63b10da592ecdb3272be8c6f2
Roman31X <die.haa65@gmail.com>
2024-02-19 22:32:48 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu6/RanaSaltando/CalcularRanaSaltante.java
+++ src/main/java/com/Ejercicio/Kyu6/RanaSaltando/CalcularRanaSaltante.java
@@ -1,4 +1,25 @@
 package com.Ejercicio.Kyu6.RanaSaltando;
 
 public class CalcularRanaSaltante {
+    public static int solution(int[] a) {
+        int currentPosition = 0;
+        int jumps = 0;
+
+        while (currentPosition >= 0 && currentPosition < a.length) {
+            int jumpLength = a[currentPosition];
+
+            if (jumpLength == 0) {
+                return -1;
+            }
+
+            currentPosition += jumpLength;
+            jumps++;
+
+            if (jumps > a.length) {
+                return -1;
+            }
+        }
+
+        return jumps;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu6/RanaSaltando/TestRanaSaltante.java
+++ src/main/java/com/Ejercicio/Kyu6/RanaSaltando/TestRanaSaltante.java
@@ -1,4 +1,47 @@
 package com.Ejercicio.Kyu6.RanaSaltando;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static com.Ejercicio.Kyu6.RanaSaltando.CalcularRanaSaltante.solution;
+
+@RunWith(Parameterized.class)
 public class TestRanaSaltante {
+    public static void main(String[] args) {
+        System.out.println("Test de Rana Saltante");
+    }
+
+    private int[] input;
+    private int expected;
+
+    public TestRanaSaltante(int[] a, int expected) {
+        this.input = a;
+        this.expected = expected;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1, 2, 2, -1}, 4},
+                {new int[]{1, 2, 1, 5}, 3},
+                {new int[]{1, 1, 1, 1, 5, 1, 1, 1, 1, 1, 1, 1, 1}, 9},
+                {new int[]{1, 2, 3, 1, -2, 1}, 6},
+                {new int[]{1, 1, 1, 1}, 4},
+                {new int[]{-1, -1, -2}, 1},
+                {new int[]{-3}, 1},
+                {new int[]{1, -1}, -1},
+                {new int[]{1, 2, 1, 2, -3, -4}, -1},
+                {new int[]{1, 0, 2}, -1}
+        });
+    }
+
+    @Test
+    public void should_test_solution() {
+        Assert.assertEquals(Arrays.toString(input), expected, solution(input));
+    }
 }
