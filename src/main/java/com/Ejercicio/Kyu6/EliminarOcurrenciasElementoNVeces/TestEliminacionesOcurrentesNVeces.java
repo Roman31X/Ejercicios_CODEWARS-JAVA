38451ab0b235326761a3ce9c5984155b9fbe3f22
Roman31X <die.haa65@gmail.com>
2024-02-24 15:36:00 -0500

    Test Sencillo

--- src/main/java/com/Ejercicio/Kyu6/EliminarOcurrenciasElementoNVeces/TestEliminacionesOcurrentesNVeces.java
+++ src/main/java/com/Ejercicio/Kyu6/EliminarOcurrenciasElementoNVeces/TestEliminacionesOcurrentesNVeces.java
@@ -1,4 +1,7 @@
 package com.Ejercicio.Kyu6.EliminarOcurrenciasElementoNVeces;
 
 public class TestEliminacionesOcurrentesNVeces {
+    public static void main(String[] args) {
+        System.out.println("Test Eliminar apariciones de un elemento si ocurre más de n veces");
+    }
 }
