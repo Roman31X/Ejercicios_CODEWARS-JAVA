b83d15ab4ca7b16eb1295b3717e5a3948393e7a7
Roman31X <die.haa65@gmail.com>
2024-02-24 15:35:00 -0500

    Codigo desarrollado

--- src/main/java/com/Ejercicio/Kyu6/EliminarOcurrenciasElementoNVeces/EliminarElementosConcurrentesNVeces.java
+++ src/main/java/com/Ejercicio/Kyu6/EliminarOcurrenciasElementoNVeces/EliminarElementosConcurrentesNVeces.java
@@ -1,4 +1,35 @@
 package com.Ejercicio.Kyu6.EliminarOcurrenciasElementoNVeces;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EliminarElementosConcurrentesNVeces {
+    public static int[] deleteNth(int[] elements, int maxOccurrences) {
+        if (maxOccurrences == 0) {
+            return new int[0]; // Devolver un array vacío
+        }
+
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        List<Integer> resultList = new ArrayList<>();
+
+        for (int num : elements) {
+            if (!occurrences.containsKey(num)) {
+                occurrences.put(num, 1);
+                resultList.add(num);
+            } else if (occurrences.get(num) < maxOccurrences) {
+                occurrences.put(num, occurrences.get(num) + 1);
+                resultList.add(num);
+            }
+        }
+
+        // Convertir la lista de resultados a un array
+        int[] resultArray = new int[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            resultArray[i] = resultList.get(i);
+        }
+
+        return resultArray;
+    }
 }
