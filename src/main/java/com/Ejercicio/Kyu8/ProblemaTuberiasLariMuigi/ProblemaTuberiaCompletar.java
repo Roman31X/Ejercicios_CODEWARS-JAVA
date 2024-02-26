204162b9dae34a722ac63f8affc7e7589b3b4248
Roman31X <die.haa65@gmail.com>
2024-02-25 19:41:18 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu8/ProblemaTuberiasLariMuigi/ProblemaTuberiaCompletar.java
+++ src/main/java/com/Ejercicio/Kyu8/ProblemaTuberiasLariMuigi/ProblemaTuberiaCompletar.java
@@ -1,4 +1,29 @@
 package com.Ejercicio.Kyu8.ProblemaTuberiasLariMuigi;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProblemaTuberiaCompletar {
+    public static int[] pipeFix(int[] numbers){
+        List<Integer> result = new ArrayList<>();
+
+        if (numbers.length == 0) {
+            return new int[0];
+        }
+
+        int min = numbers[0];
+        int max = numbers[numbers.length - 1];
+
+        for (int i = min; i <= max; i++) {
+            result.add(i);
+        }
+
+        // Convertir la lista de resultados a un array
+        int[] resultArray = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            resultArray[i] = result.get(i);
+        }
+
+        return resultArray;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/ProblemaTuberiasLariMuigi/TestProblemaTuberiaLariMuigi.java
+++ src/main/java/com/Ejercicio/Kyu8/ProblemaTuberiasLariMuigi/TestProblemaTuberiaLariMuigi.java
@@ -1,4 +1,13 @@
 package com.Ejercicio.Kyu8.ProblemaTuberiasLariMuigi;
 
+import java.util.Arrays;
+
+import static com.Ejercicio.Kyu8.ProblemaTuberiasLariMuigi.ProblemaTuberiaCompletar.pipeFix;
+
 public class TestProblemaTuberiaLariMuigi {
+    public static void main(String[] args) {
+        System.out.println("Test Sencillo de problema con tuberías de Lario y Mugio");
+        int[] prueba = pipeFix(new int[] {6,9});
+        System.out.println("Números "+ Arrays.toString(prueba));
+    }
 }
