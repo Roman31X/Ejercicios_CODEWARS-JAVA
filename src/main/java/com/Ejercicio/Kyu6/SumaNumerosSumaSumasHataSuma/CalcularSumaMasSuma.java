89b36dc090f2d6e4b3a5a1bd81944db31c4a83e4
Roman31X <die.haa65@gmail.com>
2024-02-26 13:26:00 -0500

    Lineas recuperadas

--- src/main/java/com/Ejercicio/Kyu6/SumaNumerosSumaSumasHataSuma/CalcularSumaMasSuma.java
+++ src/main/java/com/Ejercicio/Kyu6/SumaNumerosSumaSumasHataSuma/CalcularSumaMasSuma.java
@@ -4,8 +4,22 @@ import java.math.BigInteger;
 
 public class CalcularSumaMasSuma {
     public static BigInteger sumaSuma(int n){
+        /*BigInteger sum = BigInteger.ZERO;
+        BigInteger result = BigInteger.ZERO;
+
+        for (int i = 1; i <= n; i++) {
+            sum = sum.add(BigInteger.valueOf(i));
+            result = result.add(sum);
+        }
+
+        result = result.add(BigInteger.ONE).multiply(result).divide(BigInteger.valueOf(2));
+        return result;*/
         BigInteger nBigInteger = BigInteger.valueOf(n);
+
+        // Calcular S(n) = n * (n + 1) / 2
         BigInteger sumN = nBigInteger.multiply(nBigInteger.add(BigInteger.ONE)).divide(BigInteger.valueOf(2));
+
+        // Simplificar la expresión final sin necesidad de calcular S(n) dos veces
         BigInteger result = sumN.multiply(sumN.add(BigInteger.ONE)).divide(BigInteger.valueOf(4));
 
         return result;
