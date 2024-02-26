5bd9093150ae25a8496def2e55e761b9f958f74d
Roman31X <die.haa65@gmail.com>
2024-02-25 19:50:43 -0500

    Test de Junit aprobados

--- src/main/java/com/Ejercicio/Kyu6/DameUnDiamante/GenerarDiamanteString.java
+++ src/main/java/com/Ejercicio/Kyu6/DameUnDiamante/GenerarDiamanteString.java
@@ -1,4 +1,33 @@
 package com.Ejercicio.Kyu6.DameUnDiamante;
 
 public class GenerarDiamanteString {
+    public static String print(int n) {
+        if (n % 2 == 0 || n < 0) {
+            return null;
+        }
+
+        StringBuilder diamond = new StringBuilder();
+
+        for (int i = 0; i < n / 2 + 1; i++) {
+            for (int j = 0; j < n / 2 - i; j++) {
+                diamond.append(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                diamond.append("*");
+            }
+            diamond.append("\n");
+        }
+
+        for (int i = n / 2 - 1; i >= 0; i--) {
+            for (int j = 0; j < n / 2 - i; j++) {
+                diamond.append(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                diamond.append("*");
+            }
+            diamond.append("\n");
+        }
+
+        return diamond.toString();
+    }
 }
--- src/main/java/com/Ejercicio/Kyu6/DameUnDiamante/TestDameUnDiamante.java
+++ src/main/java/com/Ejercicio/Kyu6/DameUnDiamante/TestDameUnDiamante.java
@@ -1,4 +1,56 @@
 package com.Ejercicio.Kyu6.DameUnDiamante;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu6.DameUnDiamante.GenerarDiamanteString.print;
+import static org.junit.Assert.assertEquals;
+
 public class TestDameUnDiamante {
+    public static void main(String[] args) {
+        System.out.println("Test Sencillo de Dame un Diamante");
+    }
+
+    @Test
+    public void testDiamond3() {
+        StringBuffer expected = new StringBuffer();
+        expected.append(" *\n");
+        expected.append("***\n");
+        expected.append(" *\n");
+
+        assertEquals(expected.toString(), print(3));
+    }
+
+    @Test
+    public void testDiamond5() {
+        StringBuffer expected = new StringBuffer();
+        expected.append("  *\n");
+        expected.append(" ***\n");
+        expected.append("*****\n");
+        expected.append(" ***\n");
+        expected.append("  *\n");
+
+        assertEquals(expected.toString(), print(5));
+    }
+
+    @Test
+    public void testDiamond1() {
+        StringBuffer expected = new StringBuffer();
+        expected.append("*\n");
+        assertEquals(expected.toString(), print(1));
+    }
+
+    @Test
+    public void testDiamond0() {
+        assertEquals(null, print(0));
+    }
+
+    @Test
+    public void testDiamondMinus2() {
+        assertEquals(null, print(-2));
+    }
+
+    @Test
+    public void testDiamond2() {
+        assertEquals(null, print(2));
+    }
 }
