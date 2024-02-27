34db91e1b233452ca9fc32f703d79abe7d181ceb
Roman31X <die.haa65@gmail.com>
2024-02-27 15:10:00 -0500

    Importaciones de librerias Test

--- src/main/java/com/Ejercicio/Kyu6/DetectarPangrama/TestDetectarPangrama.java
+++ src/main/java/com/Ejercicio/Kyu6/DetectarPangrama/TestDetectarPangrama.java
@@ -1,6 +1,8 @@
 package com.Ejercicio.Kyu6.DetectarPangrama;
 
-
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
 
 public class TestDetectarPangrama {
 
