66536a60fef14bc92b509c7e21a741dfc3abda9f
Roman31X <die.haa65@gmail.com>
2024-02-27 15:05:00 -0500

    Ejercicio desarollado

--- src/main/java/com/Ejercicio/Kyu6/DetectarPangrama/DetectardorPangrama.java
+++ src/main/java/com/Ejercicio/Kyu6/DetectarPangrama/DetectardorPangrama.java
@@ -1,7 +1,21 @@
 package com.Ejercicio.Kyu6.DetectarPangrama;
 
-
+import java.util.HashSet;
 
 public class DetectardorPangrama {
+    public boolean check(String sentence){
+        HashSet<Character> alphabetSet = new HashSet<>();
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            alphabetSet.add(ch);
+        }
 
+        HashSet<Character> sentenceSet = new HashSet<>();
+        for (char c : sentence.toLowerCase().toCharArray()) {
+            if (Character.isLetter(c)) {
+                sentenceSet.add(c);
+            }
+        }
+
+        return sentenceSet.equals(alphabetSet);
+    }
 }
