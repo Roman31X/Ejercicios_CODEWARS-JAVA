02dd695eadc4f2a0db8ed7510e691e240de23327
Roman31X <die.haa65@gmail.com>
2024-02-27 15:15:00 -0500

    Test aprobados

--- src/main/java/com/Ejercicio/Kyu6/DetectarPangrama/TestDetectarPangrama.java
+++ src/main/java/com/Ejercicio/Kyu6/DetectarPangrama/TestDetectarPangrama.java
@@ -5,7 +5,16 @@ import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
 
 public class TestDetectarPangrama {
-
-
+    DetectardorPangrama pc = new DetectardorPangrama();
+    @Test
+    public void test1() {
+        String pangram1 = "The quick brown fox jumps over the lazy dog.";
+        assertEquals(true, pc.check(pangram1));
+    }
+    @Test
+    public void test2() {
+        String pangram2 = "You shall not pass!";
+        assertEquals(false, pc.check(pangram2));
+    }
 
 }
