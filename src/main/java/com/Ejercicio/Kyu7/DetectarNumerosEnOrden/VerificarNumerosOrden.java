8ca2815bdd42eca4d49a0503cfa33f0b739fe092
Roman31X <die.haa65@gmail.com>
2024-02-29 14:04:00 -0500

    Ejercicio concluido

--- src/main/java/com/Ejercicio/Kyu7/DetectarNumerosEnOrden/VerificarNumerosOrden.java
+++ src/main/java/com/Ejercicio/Kyu7/DetectarNumerosEnOrden/VerificarNumerosOrden.java
@@ -1,4 +1,12 @@
 package com.Ejercicio.Kyu7.DetectarNumerosEnOrden;
 
 public class VerificarNumerosOrden {
+    public static boolean isAscOrder(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
