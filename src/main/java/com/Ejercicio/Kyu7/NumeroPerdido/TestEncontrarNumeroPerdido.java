9aa78180df7a752157811e63b6a84e06d8998298
Roman31X <die.haa65@gmail.com>
2024-03-02 15:22:00 -0500

    Test aprobados

--- src/main/java/com/Ejercicio/Kyu7/NumeroPerdido/TestEncontrarNumeroPerdido.java
+++ src/main/java/com/Ejercicio/Kyu7/NumeroPerdido/TestEncontrarNumeroPerdido.java
@@ -1,11 +1,30 @@
 package com.Ejercicio.Kyu7.NumeroPerdido;
 
 import org.junit.Test;
+
+import static com.Ejercicio.Kyu7.NumeroPerdido.EncontrarNumeroPerdido.stray;
 import static org.junit.Assert.assertEquals;
 
 public class TestEncontrarNumeroPerdido {
     public static void main(String[] args) {
         System.out.println("Test Encuentra al número perdido");
     }
+    @Test
+    public void threeElements1() {
+        assertEquals(2, getActualFor(2, 1, 1));
+    }
 
+    @Test
+    public void threeElements2() {
+        assertEquals(2, getActualFor(1, 2, 1));
+    }
+
+    @Test
+    public void threeElements3() {
+        assertEquals(2, getActualFor(1, 1, 2));
+    }
+
+    private int getActualFor(int... numbers) {
+        return stray(numbers);
+    }
 }
