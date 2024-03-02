4f9972674e223ca1a469bf77a4fc40c29aff3982
Roman31X <die.haa65@gmail.com>
2024-03-02 15:28:00 -0500

    Desarrollo completado

--- src/main/java/com/Ejercicio/Kyu8/TablaMultiplicarN/TablaMultiplicarNumeros.java
+++ src/main/java/com/Ejercicio/Kyu8/TablaMultiplicarN/TablaMultiplicarNumeros.java
@@ -1,4 +1,12 @@
 package com.Ejercicio.Kyu8.TablaMultiplicarN;
 
 public class TablaMultiplicarNumeros {
+    public static String multiTable(int num){
+        String cadena = "";
+        for (int i = 1; i <= 10; i++) {
+            int multi = i * num;
+            cadena+=i+" * "+num+" = "+multi+"\n";
+        }
+        return cadena;
+    }
 }
