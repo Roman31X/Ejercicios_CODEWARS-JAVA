4b511a1dad8f39a63ce42d14b7c354aa1455a596
Roman31X <die.haa65@gmail.com>
2024-03-03 12:23:35 -0500

    Test aprobados

--- src/main/java/com/Ejercicio/Kyu8/FraseAplastante/TestOrdenarFrase.java
+++ src/main/java/com/Ejercicio/Kyu8/FraseAplastante/TestOrdenarFrase.java
@@ -8,5 +8,19 @@ public class TestOrdenarFrase {
     public static void main(String[] args) {
         System.out.println("Test Frase Aplastante");
     }
+    @Test
+    public void validate() {
+        assertEquals("Bilal Djaghout", smash(new String[] { "Bilal", "Djaghout" }));
+    }
 
+    @Test
+    public void validateEmpty() {
+        assertEquals("", smash(new String[] {}));
+    }
+
+
+    @Test
+    public void validateOneWord() {
+        assertEquals("Bilal", smash(new String[] {"Bilal"}));
+    }
 }
