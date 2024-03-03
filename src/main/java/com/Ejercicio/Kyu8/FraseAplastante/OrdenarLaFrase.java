189f42c345466cab69300eda0e59a7e9e17c2c45
Roman31X <die.haa65@gmail.com>
2024-03-03 12:23:06 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/FraseAplastante/OrdenarLaFrase.java
+++ src/main/java/com/Ejercicio/Kyu8/FraseAplastante/OrdenarLaFrase.java
@@ -1,4 +1,17 @@
 package com.Ejercicio.Kyu8.FraseAplastante;
 
 public class OrdenarLaFrase {
+    public static String smash(String... words){
+        if (words == null || words.length == 0) {
+            return "";
+        }
+
+        StringBuilder sentence = new StringBuilder(words[0]);
+
+        for (int i = 1; i < words.length; i++) {
+            sentence.append(" ").append(words[i]);
+        }
+
+        return sentence.toString();
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/FraseAplastante/TestOrdenarFrase.java
+++ src/main/java/com/Ejercicio/Kyu8/FraseAplastante/TestOrdenarFrase.java
@@ -1,4 +1,12 @@
 package com.Ejercicio.Kyu8.FraseAplastante;
 
+import static com.Ejercicio.Kyu8.FraseAplastante.OrdenarLaFrase.smash;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestOrdenarFrase {
+    public static void main(String[] args) {
+        System.out.println("Test Frase Aplastante");
+    }
+
 }
