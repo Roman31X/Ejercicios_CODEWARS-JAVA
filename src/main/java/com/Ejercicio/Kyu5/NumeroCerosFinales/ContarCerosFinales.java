ff084870ad70c485309b1500adc8bc0fe4ef8304
Roman31X <die.haa65@gmail.com>
2024-03-06 11:08:38 -0500

    Ejercicio completado planteamiento de test

--- src/main/java/com/Ejercicio/Kyu5/NumeroCerosFinales/ContarCerosFinales.java
+++ src/main/java/com/Ejercicio/Kyu5/NumeroCerosFinales/ContarCerosFinales.java
@@ -1,4 +1,13 @@
 package com.Ejercicio.Kyu5.NumeroCerosFinales;
 
 public class ContarCerosFinales {
+    public static int zeros(int n) {
+        int count = 0;
+        while (n >= 5) {
+            n /= 5;
+            count += n;
+        }
+
+        return count;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu5/NumeroCerosFinales/TestNumerosDeCerosFinalesDeN.java
+++ src/main/java/com/Ejercicio/Kyu5/NumeroCerosFinales/TestNumerosDeCerosFinalesDeN.java
@@ -1,5 +1,7 @@
 package com.Ejercicio.Kyu5.NumeroCerosFinales;
 
 public class TestNumerosDeCerosFinalesDeN {
-
+    public static void main(String[] args) {
+        System.out.println("Test Números de ceros finales de N!");
+    }
 }
