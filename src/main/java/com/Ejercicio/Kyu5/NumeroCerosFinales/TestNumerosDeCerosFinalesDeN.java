cf0435accca0021a0bfd7c8cb9ed35173c5c3e22
Roman31X <die.haa65@gmail.com>
2024-03-06 11:09:19 -0500

    Test aprobados

--- src/main/java/com/Ejercicio/Kyu5/NumeroCerosFinales/TestNumerosDeCerosFinalesDeN.java
+++ src/main/java/com/Ejercicio/Kyu5/NumeroCerosFinales/TestNumerosDeCerosFinalesDeN.java
@@ -1,7 +1,20 @@
 package com.Ejercicio.Kyu5.NumeroCerosFinales;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu5.NumeroCerosFinales.ContarCerosFinales.zeros;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
+
 public class TestNumerosDeCerosFinalesDeN {
     public static void main(String[] args) {
         System.out.println("Test Números de ceros finales de N!");
+    }
+
+    @Test
+    public void testZeros() throws Exception {
+        assertThat(zeros(0), is(0));
+        assertThat(zeros(6), is(1));
+        assertThat(zeros(14), is(2));
     }
 }
