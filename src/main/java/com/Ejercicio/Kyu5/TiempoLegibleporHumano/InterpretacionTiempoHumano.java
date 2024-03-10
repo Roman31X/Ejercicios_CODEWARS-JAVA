415b684b8edbdb3c682f2900866136233bae2c8e
Roman31X <die.haa65@gmail.com>
2024-03-10 10:41:22 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu5/TiempoLegibleporHumano/InterpretacionTiempoHumano.java
+++ src/main/java/com/Ejercicio/Kyu5/TiempoLegibleporHumano/InterpretacionTiempoHumano.java
@@ -1,4 +1,15 @@
 package com.Ejercicio.Kyu5.TiempoLegibleporHumano;
 
 public class InterpretacionTiempoHumano {
+    public static String makeReadable(int seconds) {
+        if (seconds < 0 || seconds > 359999) {
+            throw new IllegalArgumentException("Invalid input. Seconds should be a non-negative integer less than or equal to 359999.");
+        }
+
+        int hours = seconds / 3600;
+        int minutes = (seconds % 3600) / 60;
+        int remainingSeconds = seconds % 60;
+
+        return String.format("%02d:%02d:%02d", hours, minutes, remainingSeconds);
+    }
 }
--- src/main/java/com/Ejercicio/Kyu5/TiempoLegibleporHumano/TestTiempoLegibleHumano.java
+++ src/main/java/com/Ejercicio/Kyu5/TiempoLegibleporHumano/TestTiempoLegibleHumano.java
@@ -1,4 +1,7 @@
 package com.Ejercicio.Kyu5.TiempoLegibleporHumano;
 
 public class TestTiempoLegibleHumano {
+    public static void main(String[] args) {
+        System.out.println("Test Tiempo legible por humanos");
+    }
 }
