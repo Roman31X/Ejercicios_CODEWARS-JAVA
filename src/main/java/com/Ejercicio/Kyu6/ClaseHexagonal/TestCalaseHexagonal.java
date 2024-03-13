d051df7a4cd3b5bd272e1741d4375f063b290db4
Roman31X <die.haa65@gmail.com>
2024-03-13 15:47:00 -0500

    Test aprobados

--- src/main/java/com/Ejercicio/Kyu6/ClaseHexagonal/TestCalaseHexagonal.java
+++ src/main/java/com/Ejercicio/Kyu6/ClaseHexagonal/TestCalaseHexagonal.java
@@ -1,7 +1,24 @@
 package com.Ejercicio.Kyu6.ClaseHexagonal;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class TestCalaseHexagonal {
     public static void main(String[] args) {
+        System.out.println("Test de Convertido a Hexadecimal");
+    }
 
+    @Test
+    public void SampleTests() {
+        Hex FF = new Hex(255);
+        assertEquals("0xFF",FF.toString());
+        assertEquals(256 ,FF.valueOf() + 1);
+        assertEquals( "0xFF",FF.toJSON());
+        assertEquals(FF,new Hex(255));
+        assertEquals("0xFE",FF.minus(1).toString());
+        assertEquals("0x0",FF.minus(FF).toString());
+        assertEquals("0xF",new Hex(10).plus(5).toString());
+        assertEquals(32,Hex.parse("0x20"));
+        assertEquals(512,Hex.parse("200"));
     }
 }
