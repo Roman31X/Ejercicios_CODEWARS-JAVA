09f6eb148666fee47d72f618ab3f57de769e7e05
Roman31X <die.haa65@gmail.com>
2024-03-13 15:22:00 -0500

    Desarrollo completado

--- src/main/java/com/Ejercicio/Kyu6/ContruyeMontonCubos/ConstruyeUnMontonCubos.java
+++ src/main/java/com/Ejercicio/Kyu6/ContruyeMontonCubos/ConstruyeUnMontonCubos.java
@@ -1,4 +1,15 @@
 package com.Ejercicio.Kyu6.ContruyeMontonCubos;
 
 public class ConstruyeUnMontonCubos {
+    public static long findNb(long m) {
+        long volume = 0;
+        long n = 0;
+
+        while (volume < m) {
+            n++;
+            volume += n * n * n;
+        }
+
+        return volume == m ? n : -1;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu6/ContruyeMontonCubos/TestConstruyeMontonCubos.java
+++ src/main/java/com/Ejercicio/Kyu6/ContruyeMontonCubos/TestConstruyeMontonCubos.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu6.ContruyeMontonCubos;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class TestConstruyeMontonCubos {
+    public static void main(String[] args) {
+
+    }
 }
