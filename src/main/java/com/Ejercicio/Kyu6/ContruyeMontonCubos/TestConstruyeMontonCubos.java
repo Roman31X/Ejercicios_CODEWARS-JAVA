1e71f43b4f4a79b55a3bc269525f4e99afb7fd89
Roman31X <die.haa65@gmail.com>
2024-03-13 15:30:00 -0500

    Testverificados y aprobados

--- src/main/java/com/Ejercicio/Kyu6/ContruyeMontonCubos/TestConstruyeMontonCubos.java
+++ src/main/java/com/Ejercicio/Kyu6/ContruyeMontonCubos/TestConstruyeMontonCubos.java
@@ -1,11 +1,29 @@
 package com.Ejercicio.Kyu6.ContruyeMontonCubos;
 
+import static com.Ejercicio.Kyu6.ContruyeMontonCubos.ConstruyeUnMontonCubos.findNb;
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestConstruyeMontonCubos {
     public static void main(String[] args) {
+        System.out.println("Test Construye un montón de cubos");
+    }
 
+    @Test
+    public void test1() {
+        assertEquals(2022, findNb(4183059834009L));
+    }
+    @Test
+    public void test2() {
+        assertEquals(-1, findNb(24723578342962L));
+    }
+    @Test
+    public void test3() {
+        assertEquals(4824, findNb(135440716410000L));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(3568, findNb(40539911473216L));
     }
 }
