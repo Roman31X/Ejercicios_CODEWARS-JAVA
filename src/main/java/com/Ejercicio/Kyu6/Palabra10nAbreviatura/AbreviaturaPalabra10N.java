b29b5d50dcebc82c7dc9f88b179597b8ec51193c
Roman31X <die.haa65@gmail.com>
2024-03-14 22:09:11 -0500

    Desarrollo concluido

--- src/main/java/com/Ejercicio/Kyu6/Palabra10nAbreviatura/AbreviaturaPalabra10N.java
+++ src/main/java/com/Ejercicio/Kyu6/Palabra10nAbreviatura/AbreviaturaPalabra10N.java
@@ -1,5 +1,40 @@
 package com.Ejercicio.Kyu6.Palabra10nAbreviatura;
 
 public class AbreviaturaPalabra10N {
+    public String abbreviate(String string) {
+        StringBuilder result = new StringBuilder();
+        StringBuilder word = new StringBuilder();
+        boolean inWord = false;
+
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+
+            if (Character.isLetter(c)) {
+                word.append(c);
+                inWord = true;
+            } else {
+                if (inWord) {
+                    result.append(abbreviateWord(word.toString()));
+                    word.setLength(0);
+                }
+                result.append(c);
+                inWord = false;
+            }
+        }
+
+        if (inWord) {
+            result.append(abbreviateWord(word.toString()));
+        }
+
+        return result.toString();
+    }
+
+    private static String abbreviateWord(String word) {
+        if (word.length() < 4) {
+            return word;
+        } else {
+            return word.charAt(0) + String.valueOf(word.length() - 2) + word.charAt(word.length() - 1);
+        }
+    }
 
 }
--- src/main/java/com/Ejercicio/Kyu6/Palabra10nAbreviatura/TestPalabra10NAbreviatura.java
+++ src/main/java/com/Ejercicio/Kyu6/Palabra10nAbreviatura/TestPalabra10NAbreviatura.java
@@ -1,5 +1,8 @@
 package com.Ejercicio.Kyu6.Palabra10nAbreviatura;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestPalabra10NAbreviatura {
 
 }
