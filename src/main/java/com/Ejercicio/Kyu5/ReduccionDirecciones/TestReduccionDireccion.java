6ccd2d8ec0f909c3fdc22b84563bd2fa03b864d0
Roman31X <die.haa65@gmail.com>
2024-03-15 15:10:00 -0500

    Test desarrollados

--- src/main/java/com/Ejercicio/Kyu5/ReduccionDirecciones/TestReduccionDireccion.java
+++ src/main/java/com/Ejercicio/Kyu5/ReduccionDirecciones/TestReduccionDireccion.java
@@ -1,4 +1,22 @@
 package com.Ejercicio.Kyu5.ReduccionDirecciones;
 
+import org.junit.Test;
+import static com.Ejercicio.Kyu5.ReduccionDirecciones.DireccionReducida.dirReduc;
+import static org.junit.Assert.*;
+
 public class TestReduccionDireccion {
+
+    public static void main(String[] args) {
+        System.out.println("Test de Reducción de Direcciones");
+    }
+
+    @Test
+    public void testSimpleDirReduc() {
+        assertArrayEquals("\"NORTH\", \"SOUTH\", \"SOUTH\", \"EAST\", \"WEST\", \"NORTH\", \"WEST\"",
+                new String[]{"WEST"},
+                dirReduc(new String[]{"NORTH", "SOUTH", "SOUTH", "EAST", "WEST", "NORTH", "WEST"}));
+        assertArrayEquals("\"NORTH\",\"SOUTH\",\"SOUTH\",\"EAST\",\"WEST\",\"NORTH\"",
+                new String[]{},
+                dirReduc(new String[]{"NORTH","SOUTH","SOUTH","EAST","WEST","NORTH"}));
+    }
 }
