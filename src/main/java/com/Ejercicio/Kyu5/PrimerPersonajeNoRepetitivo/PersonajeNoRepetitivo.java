3846f84eb8c00a4b21c7f65b8cb2f5451da13246
Roman31X <die.haa65@gmail.com>
2024-03-15 15:25:00 -0500

    Desarrollo completado

--- src/main/java/com/Ejercicio/Kyu5/PrimerPersonajeNoRepetitivo/PersonajeNoRepetitivo.java
+++ src/main/java/com/Ejercicio/Kyu5/PrimerPersonajeNoRepetitivo/PersonajeNoRepetitivo.java
@@ -1,5 +1,23 @@
 package com.Ejercicio.Kyu5.PrimerPersonajeNoRepetitivo;
 
-public class PersonajeNoRepetitivo {
+import java.util.*;
 
+public class PersonajeNoRepetitivo {
+    public static String firstNonRepeatingLetter(String s){
+        Map<Character, Integer> charCounts = new HashMap<>();
+
+        String lowercaseS = s.toLowerCase();
+
+        for (char c : lowercaseS.toCharArray()) {
+            charCounts.put(c, charCounts.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : s.toCharArray()) {
+            if (charCounts.get(Character.toLowerCase(c)) == 1) {
+                return String.valueOf(c);
+            }
+        }
+
+        return "";
+    }
 }
--- src/main/java/com/Ejercicio/Kyu5/PrimerPersonajeNoRepetitivo/TestPrimerPersonajeNoRepetitivo.java
+++ src/main/java/com/Ejercicio/Kyu5/PrimerPersonajeNoRepetitivo/TestPrimerPersonajeNoRepetitivo.java
@@ -1,4 +1,5 @@
 package com.Ejercicio.Kyu5.PrimerPersonajeNoRepetitivo;
 
 public class TestPrimerPersonajeNoRepetitivo {
+
 }
