17b05fa10ae458f249590cb1cd0e08eb8db7ce70
Roman31X <die.haa65@gmail.com>
2024-03-16 23:53:06 -0500

    Ejercicio dearrollado

--- src/main/java/com/Ejercicio/Kyu8/EliminandoElementos/EliminarElementoLista.java
+++ src/main/java/com/Ejercicio/Kyu8/EliminandoElementos/EliminarElementoLista.java
@@ -1,5 +1,15 @@
 package com.Ejercicio.Kyu8.EliminandoElementos;
 
 public class EliminarElementoLista {
+    public static Object[] removeEveryOther(Object[] arr) {
+        int newSize = (arr.length + 1) / 2;
 
+        Object[] result = new Object[newSize];
+
+        for (int i = 0, j = 0; i < arr.length; i += 2, j++) {
+            result[j] = arr[i];
+        }
+
+        return result;
+    }
 }
