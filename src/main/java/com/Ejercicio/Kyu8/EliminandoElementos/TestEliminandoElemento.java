f8ffa5ae1bd302ed26142c8b66b3d1fdcdb47fb1
Roman31X <die.haa65@gmail.com>
2024-03-16 23:54:15 -0500

    Test propuestos

--- src/main/java/com/Ejercicio/Kyu8/EliminandoElementos/TestEliminandoElemento.java
+++ src/main/java/com/Ejercicio/Kyu8/EliminandoElementos/TestEliminandoElemento.java
@@ -1,5 +1,10 @@
 package com.Ejercicio.Kyu8.EliminandoElementos;
 
-public class TestEliminandoElemento {
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import org.junit.jupiter.api.Test;
 
+public class TestEliminandoElemento {
+    public static void main(String[] args) {
+        System.out.println("Test Eliminando elemento");
+    }
 }
