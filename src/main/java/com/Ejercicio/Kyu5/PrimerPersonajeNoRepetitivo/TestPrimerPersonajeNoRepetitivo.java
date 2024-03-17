389b9e33ec9c07a0008ec562508bff01c858538f
Roman31X <die.haa65@gmail.com>
2024-03-16 23:47:26 -0500

    Test propuestos y evaluados

--- src/main/java/com/Ejercicio/Kyu5/PrimerPersonajeNoRepetitivo/TestPrimerPersonajeNoRepetitivo.java
+++ src/main/java/com/Ejercicio/Kyu5/PrimerPersonajeNoRepetitivo/TestPrimerPersonajeNoRepetitivo.java
@@ -1,10 +1,20 @@
 package com.Ejercicio.Kyu5.PrimerPersonajeNoRepetitivo;
 
 import org.junit.jupiter.api.*;
+import static com.Ejercicio.Kyu5.PrimerPersonajeNoRepetitivo.PersonajeNoRepetitivo.firstNonRepeatingLetter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestPrimerPersonajeNoRepetitivo {
     public static void main(String[] args) {
         System.out.println("Test Primer personaje no repetitivo");
     }
+
+    @Test @DisplayName("Sample tests")
+    void sampleTests() {
+        assertEquals("a", firstNonRepeatingLetter("a"), "For input \"a\"");
+        assertEquals("t", firstNonRepeatingLetter("streSS"), "For input \"streSS\"");
+        assertEquals("-", firstNonRepeatingLetter("moon-men"), "For input \"moon-men\"");
+        assertEquals("", firstNonRepeatingLetter("moonmoon"), "For input \"moonmoon\"");
+    }
+
 }
