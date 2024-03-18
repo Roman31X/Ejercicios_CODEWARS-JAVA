9c8538593d946865c3c3d3dd4c0f3eeb50ac6070
Roman31X <die.haa65@gmail.com>
2024-03-18 13:03:58 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu5/PrimosEnNumeros/CalcularPrimosEnNumeros.java
+++ src/main/java/com/Ejercicio/Kyu5/PrimosEnNumeros/CalcularPrimosEnNumeros.java
@@ -1,4 +1,26 @@
 package com.Ejercicio.Kyu5.PrimosEnNumeros;
 
+import java.util.*;
+
 public class CalcularPrimosEnNumeros {
+    public static String factors(int n) {
+        List<String> factors = new ArrayList<>();
+
+        for (int i = 2; i <= n; i++) {
+            int count = 0;
+            while (n % i == 0) {
+                n /= i;
+                count++;
+            }
+            if (count > 0) {
+                if (count == 1) {
+                    factors.add("(" + i + ")");
+                } else {
+                    factors.add("(" + i + "**" + count + ")");
+                }
+            }
+        }
+
+        return String.join("", factors);
+    }
 }
