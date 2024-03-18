c2102553443176c3da161112bcfae00e704c689d
Roman31X <die.haa65@gmail.com>
2024-03-18 12:31:14 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/CalcularColaSupermercado/ColaDelSupermercado.java
+++ src/main/java/com/Ejercicio/Kyu6/CalcularColaSupermercado/ColaDelSupermercado.java
@@ -1,4 +1,25 @@
 package com.Ejercicio.Kyu6.CalcularColaSupermercado;
 
+import java.util.*;
+
 public class ColaDelSupermercado {
+    public static int solveSuperMarketQueue(int[] customers, int n){
+        int time = 0;
+        if(n == 1){
+            for (int i = 0; i < customers.length; i++) {
+                time+=customers[i];
+            }
+        }else{
+            int mayor = 0, contador = 0;
+            for (int i = 0; i < customers.length; i++) {
+                if(mayor < customers[i]){
+                    mayor = customers[i];
+                }else{
+                    contador+=customers[i];
+                }
+            }
+            System.out.println(mayor+" - "+contador);
+        }
+        return time;
+    }
 }
