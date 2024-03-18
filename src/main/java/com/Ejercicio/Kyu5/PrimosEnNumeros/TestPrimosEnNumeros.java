efbe0909c76ce293ded55ccfedae58f94617cac0
Roman31X <die.haa65@gmail.com>
2024-03-18 13:04:53 -0500

    Agregar importaciones de test y planteados

--- src/main/java/com/Ejercicio/Kyu5/PrimosEnNumeros/TestPrimosEnNumeros.java
+++ src/main/java/com/Ejercicio/Kyu5/PrimosEnNumeros/TestPrimosEnNumeros.java
@@ -1,4 +1,10 @@
 package com.Ejercicio.Kyu5.PrimosEnNumeros;
 
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class TestPrimosEnNumeros {
+    public static void main(String[] args) {
+        System.out.println("Test Primos en Números");
+    }
 }
