3f068cccd0b6e953d5a4057d2a6cbcbd6e437804
Roman31X <die.haa65@gmail.com>
2024-03-17 19:12:38 -0500

    Propuesta de test

--- src/main/java/com/Ejercicio/Kyu7/DosLuchadoresUnGanador/TestDosLuchadoresUNGanador.java
+++ src/main/java/com/Ejercicio/Kyu7/DosLuchadoresUnGanador/TestDosLuchadoresUNGanador.java
@@ -1,5 +1,12 @@
 package com.Ejercicio.Kyu7.DosLuchadoresUnGanador;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
 public class TestDosLuchadoresUNGanador {
+    public static void main(String[] args) {
+        System.out.println("Test Dos luchadores un Ganador");
+    }
 
 }
