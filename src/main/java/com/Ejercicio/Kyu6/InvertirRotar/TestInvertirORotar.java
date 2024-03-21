6767bab52136fd3d1404d4c0c980c4c2187f1e1f
Roman31X <die.haa65@gmail.com>
2024-03-21 15:10:00 -0500

    Test propuestos para diagnostico

--- src/main/java/com/Ejercicio/Kyu6/InvertirRotar/TestInvertirORotar.java
+++ src/main/java/com/Ejercicio/Kyu6/InvertirRotar/TestInvertirORotar.java
@@ -1,4 +1,22 @@
 package com.Ejercicio.Kyu6.InvertirRotar;
-
+import static com.Ejercicio.Kyu6.InvertirRotar.InvertirORotarOrden.revRot;
+import static org.junit.Assert.*;
+import org.junit.Test;
 public class TestInvertirORotar {
+    public static void main(String[] args) {
+        System.out.println("Test para ¿Invertir o Rotar?");
+    }
+
+    private static void testing(String actual, String expected) {
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void test1() {
+        System.out.println("Fixed Tests: revRot");
+        testing(revRot("1234", 0), "");
+        testing(revRot("", 0), "");
+        testing(revRot("1234", 5), "");
+        String s = "733049910872815764";
+        testing(revRot(s, 5), "330479108928157");
+    }
 }
