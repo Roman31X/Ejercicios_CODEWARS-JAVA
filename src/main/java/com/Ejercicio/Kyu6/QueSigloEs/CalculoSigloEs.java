08a4bbea9e234b0c1b37225abc8c8ef53e1ead6d
Roman31X <die.haa65@gmail.com>
2024-03-21 15:20:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/QueSigloEs/CalculoSigloEs.java
+++ src/main/java/com/Ejercicio/Kyu6/QueSigloEs/CalculoSigloEs.java
@@ -1,4 +1,22 @@
 package com.Ejercicio.Kyu6.QueSigloEs;
 
 public class CalculoSigloEs {
+    public static String whatCentury(int year) {
+        int century = year / 100 + (year % 100 > 0 ? 1 : 0);
+
+        String suffix;
+        if (century % 100 >= 11 && century % 100 <= 13) {
+            suffix = "th";
+        } else if (century % 10 == 1) {
+            suffix = "st";
+        } else if (century % 10 == 2) {
+            suffix = "nd";
+        } else if (century % 10 == 3) {
+            suffix = "rd";
+        } else {
+            suffix = "th";
+        }
+
+        return century + suffix;
+    }
 }
