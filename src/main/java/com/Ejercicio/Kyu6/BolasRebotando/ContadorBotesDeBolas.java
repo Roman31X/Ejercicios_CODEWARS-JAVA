c2bc245cc7a0de587d5e6c3e1a967688b516771c
Roman31X <die.haa65@gmail.com>
2024-03-20 21:53:11 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/BolasRebotando/ContadorBotesDeBolas.java
+++ src/main/java/com/Ejercicio/Kyu6/BolasRebotando/ContadorBotesDeBolas.java
@@ -1,4 +1,20 @@
 package com.Ejercicio.Kyu6.BolasRebotando;
 
 public class ContadorBotesDeBolas {
+    public static int bouncingBall(double h, double bounce, double window) {
+        if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+            return -1;
+        }
+        int count = 0;
+        while (h > window) {
+            count++;
+
+            h *= bounce;
+            if (h > window) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
