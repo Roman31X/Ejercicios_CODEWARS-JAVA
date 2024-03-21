3c50e2404b3327c127935738d823cf8aac11d165
Roman31X <die.haa65@gmail.com>
2024-03-20 21:47:48 -0500

    Tets planteados bajos las recomendaciones del ejercicio

--- src/main/java/com/Ejercicio/Kyu5/ProductoNumeroConsecutivo/TestProductoNumeroFibConsecutivo.java
+++ src/main/java/com/Ejercicio/Kyu5/ProductoNumeroConsecutivo/TestProductoNumeroFibConsecutivo.java
@@ -1,9 +1,22 @@
 package com.Ejercicio.Kyu5.ProductoNumeroConsecutivo;
 
+import static com.Ejercicio.Kyu5.ProductoNumeroConsecutivo.CalcularFibConsecutivo.productFib;
 import static org.junit.Assert.*;
-import java.util.Random;
 import org.junit.Test;
 
 public class TestProductoNumeroFibConsecutivo {
+    public static void main(String[] args) {
+        System.out.println("Test Producto de números Fib consecutivos");
+    }
 
+    @Test
+    public void test1() {
+        long[] r = new long[] {55, 89, 1};
+        assertArrayEquals(r, productFib(4895));
+    }
+    @Test
+    public void test2() {
+        long[] r = new long[] {89, 144, 0};
+        assertArrayEquals(r, productFib(5895));
+    }
 }
