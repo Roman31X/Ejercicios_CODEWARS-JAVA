0ae55711098be8b8045e6cc097f721f4c9c132af
Roman31X <die.haa65@gmail.com>
2024-03-21 15:22:00 -0500

    Importaciones paquetes de test Junit

--- src/main/java/com/Ejercicio/Kyu6/QueSigloEs/TestQueSigloEs.java
+++ src/main/java/com/Ejercicio/Kyu6/QueSigloEs/TestQueSigloEs.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu6.QueSigloEs;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
 public class TestQueSigloEs {
+    public static void main(String[] args) {
+        System.out.println("Test ¿Que siglo es?");
+    }
 }
