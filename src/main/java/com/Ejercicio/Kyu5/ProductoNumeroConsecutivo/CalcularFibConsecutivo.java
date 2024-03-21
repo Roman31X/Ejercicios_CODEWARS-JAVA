ca5bf2cf1026bf8dfad0a76e776c59c5aeb03ce9
Roman31X <die.haa65@gmail.com>
2024-03-20 21:46:19 -0500

    Ejerciico desarrollado

--- src/main/java/com/Ejercicio/Kyu5/ProductoNumeroConsecutivo/CalcularFibConsecutivo.java
+++ src/main/java/com/Ejercicio/Kyu5/ProductoNumeroConsecutivo/CalcularFibConsecutivo.java
@@ -1,4 +1,20 @@
 package com.Ejercicio.Kyu5.ProductoNumeroConsecutivo;
 
 public class CalcularFibConsecutivo {
+    public static long[] productFib(long prod) {
+        long fib1 = 0;
+        long fib2 = 1;
+
+        while (fib1 * fib2 < prod) {
+            long nextFib = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = nextFib;
+        }
+
+        if (fib1 * fib2 == prod) {
+            return new long[]{fib1, fib2, 1};
+        } else {
+            return new long[]{fib1, fib2, 0};
+        }
+    }
 }
--- src/main/java/com/Ejercicio/Kyu5/ProductoNumeroConsecutivo/TestProductoNumeroFibConsecutivo.java
+++ src/main/java/com/Ejercicio/Kyu5/ProductoNumeroConsecutivo/TestProductoNumeroFibConsecutivo.java
@@ -1,4 +1,9 @@
 package com.Ejercicio.Kyu5.ProductoNumeroConsecutivo;
 
+import static org.junit.Assert.*;
+import java.util.Random;
+import org.junit.Test;
+
 public class TestProductoNumeroFibConsecutivo {
+
 }
