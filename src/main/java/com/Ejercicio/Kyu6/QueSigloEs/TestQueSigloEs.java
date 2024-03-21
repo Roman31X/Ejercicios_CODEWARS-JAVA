58e7a74deeb9ce9fdc104225c09a8f499d6aab07
Roman31X <die.haa65@gmail.com>
2024-03-21 15:25:00 -0500

    Test propuestos listos para diagnostico

--- src/main/java/com/Ejercicio/Kyu6/QueSigloEs/TestQueSigloEs.java
+++ src/main/java/com/Ejercicio/Kyu6/QueSigloEs/TestQueSigloEs.java
@@ -1,11 +1,21 @@
 package com.Ejercicio.Kyu6.QueSigloEs;
 
 import org.junit.Test;
+
+import static com.Ejercicio.Kyu6.QueSigloEs.CalculoSigloEs.whatCentury;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
 
 public class TestQueSigloEs {
     public static void main(String[] args) {
         System.out.println("Test ¿Que siglo es?");
+    }
+
+    @Test
+    public void testSomething() {
+        assertEquals("20th", whatCentury(1999));
+        assertEquals("21st", whatCentury(2011));
+        assertEquals("22nd", whatCentury(2154));
+        assertEquals("23rd", whatCentury(2259));
     }
 }
