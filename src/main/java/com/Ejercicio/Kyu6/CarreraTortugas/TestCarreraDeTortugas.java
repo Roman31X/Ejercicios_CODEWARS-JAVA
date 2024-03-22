867e3b1c7c519ccae200ff7ba4bf5a923b97bc96
Roman31X <die.haa65@gmail.com>
2024-03-22 10:31:31 -0500

    Test aprobados y lisnk agregado a REDAME

--- src/main/java/com/Ejercicio/Kyu6/CarreraTortugas/TestCarreraDeTortugas.java
+++ src/main/java/com/Ejercicio/Kyu6/CarreraTortugas/TestCarreraDeTortugas.java
@@ -1,4 +1,19 @@
 package com.Ejercicio.Kyu6.CarreraTortugas;
 
+import static com.Ejercicio.Kyu6.CarreraTortugas.CalcularCarreraTortuga.race;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestCarreraDeTortugas {
+    public static void main(String[] args) {
+        System.out.println("Test carrera de Tortugas");
+    }
+
+    @Test
+    public void test1() {
+        System.out.println("Basic Tests");
+        assertArrayEquals(new int[]{0, 32, 18}, race(720, 850, 70));
+        assertArrayEquals(new int[]{3, 21, 49}, race(80, 91, 37));
+        assertArrayEquals(new int[]{2, 0, 0}, race(80, 100, 40));
+    }
 }
