5f8a2e3d44f7f2bd5c68b293c119665fe9751eaa
Roman31X <die.haa65@gmail.com>
2024-03-22 10:23:19 -0500

    Test aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/SumaDiferenciaMatriz/TestSumaDiferenciasMatriz.java
+++ src/main/java/com/Ejercicio/Kyu8/SumaDiferenciaMatriz/TestSumaDiferenciasMatriz.java
@@ -1,7 +1,24 @@
 package com.Ejercicio.Kyu8.SumaDiferenciaMatriz;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu8.SumaDiferenciaMatriz.CalcularSumaMatrisParesDescendente.sumOfDifferences;
+import static org.junit.Assert.assertEquals;
+
 public class TestSumaDiferenciasMatriz {
     public static void main(String[] args) {
         System.out.println("Test suma de diferencias en matriz");
+    }
+
+    @Test
+    public void basicTests() {
+        assertEquals(9, sumOfDifferences(new int[]{1, 2, 10}));
+        assertEquals(2, sumOfDifferences(new int[]{-3, -2, -1}));
+        assertEquals(0, sumOfDifferences(new int[]{1, 1, 1, 1, 1}));
+        assertEquals(34, sumOfDifferences(new int[]{-17, 17}));
+        assertEquals(0, sumOfDifferences(new int[0]));
+        assertEquals(0, sumOfDifferences(new int[]{0}));
+        assertEquals(0, sumOfDifferences(new int[]{-1}));
+        assertEquals(0, sumOfDifferences(new int[]{1}));
     }
 }
