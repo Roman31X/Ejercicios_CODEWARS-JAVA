7bd8544b250a85e340b3a7d023cd35c2abc64b22
Roman31X <die.haa65@gmail.com>
2024-03-22 10:22:18 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/SumaDiferenciaMatriz/CalcularSumaMatrisParesDescendente.java
+++ src/main/java/com/Ejercicio/Kyu8/SumaDiferenciaMatriz/CalcularSumaMatrisParesDescendente.java
@@ -1,4 +1,18 @@
 package com.Ejercicio.Kyu8.SumaDiferenciaMatriz;
 
+import java.util.Arrays;
+
 public class CalcularSumaMatrisParesDescendente {
+    public static int sumOfDifferences(int[] arr) {
+        if (arr == null || arr.length <= 1) {
+            return 0;
+        }
+        Arrays.sort(arr);
+        int sum = 0;
+        for (int i = arr.length - 1; i > 0; i--) {
+            sum += arr[i] - arr[i - 1];
+        }
+
+        return sum;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/SumaDiferenciaMatriz/TestSumaDiferenciasMatriz.java
+++ src/main/java/com/Ejercicio/Kyu8/SumaDiferenciaMatriz/TestSumaDiferenciasMatriz.java
@@ -1,4 +1,7 @@
 package com.Ejercicio.Kyu8.SumaDiferenciaMatriz;
 
 public class TestSumaDiferenciasMatriz {
+    public static void main(String[] args) {
+        System.out.println("Test suma de diferencias en matriz");
+    }
 }
