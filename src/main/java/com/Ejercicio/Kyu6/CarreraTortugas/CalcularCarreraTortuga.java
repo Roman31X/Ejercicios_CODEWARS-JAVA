9a13f6c2bc95ed20c0e416670ce7613cc5fbfebe
Roman31X <die.haa65@gmail.com>
2024-03-22 10:29:44 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/CarreraTortugas/CalcularCarreraTortuga.java
+++ src/main/java/com/Ejercicio/Kyu6/CarreraTortugas/CalcularCarreraTortuga.java
@@ -1,4 +1,16 @@
 package com.Ejercicio.Kyu6.CarreraTortugas;
 
 public class CalcularCarreraTortuga {
+    public static int[] race(int v1, int v2, int g) {
+        if (v1 >= v2) {
+            return null;
+        }
+
+        int totalSeconds = (g * 3600) / (v2 - v1);
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+
+        return new int[]{hours, minutes, seconds};
+    }
 }
