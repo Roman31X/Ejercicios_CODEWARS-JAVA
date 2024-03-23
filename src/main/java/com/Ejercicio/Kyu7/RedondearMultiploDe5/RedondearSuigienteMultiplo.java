7038216379f884d1e6410bb546594c2d72e21d7f
Roman31X <die.haa65@gmail.com>
2024-03-23 15:25:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/RedondearMultiploDe5/RedondearSuigienteMultiplo.java
+++ src/main/java/com/Ejercicio/Kyu7/RedondearMultiploDe5/RedondearSuigienteMultiplo.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu7.RedondearMultiploDe5;
 
 public class RedondearSuigienteMultiplo {
+    public static int roundToNext5(int number) {
+        if (number >= 0) {
+            return number % 5 == 0 ? number : number + (5 - number % 5);
+        } else {
+            return number % 5 == 0 ? number : number - number % 5;
+        }
+    }
 }
