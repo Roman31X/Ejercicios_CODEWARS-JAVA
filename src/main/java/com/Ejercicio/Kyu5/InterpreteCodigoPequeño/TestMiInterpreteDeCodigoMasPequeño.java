fda4e381a810fc9e84bcc4261118ca5f0031f8de
Roman31X <die.haa65@gmail.com>
2024-03-23 15:06:00 -0500

    Importacion paquetes de Test

--- src/main/java/com/Ejercicio/Kyu5/InterpreteCodigoPequeño/TestMiInterpreteDeCodigoMasPequeño.java
+++ src/main/java/com/Ejercicio/Kyu5/InterpreteCodigoPequeño/TestMiInterpreteDeCodigoMasPequeño.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu5.InterpreteCodigoPequeño;
 
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class TestMiInterpreteDeCodigoMasPequeño {
+    public static void main(String[] args) {
+        System.out.println("Test de Mi intérprete de código m´´as pequeño (tambien conocido como Brainf**k)");
+    }
 }
