2008958cae501ab22fad8397eb53fe2d5e4ba771
Roman31X <die.haa65@gmail.com>
2024-03-23 15:10:00 -0500

    Test aprovados con exito

--- src/main/java/com/Ejercicio/Kyu5/InterpreteCodigoPequeño/TestMiInterpreteDeCodigoMasPequeño.java
+++ src/main/java/com/Ejercicio/Kyu5/InterpreteCodigoPequeño/TestMiInterpreteDeCodigoMasPequeño.java
@@ -8,4 +8,20 @@ public class TestMiInterpreteDeCodigoMasPequeño {
     public static void main(String[] args) {
         System.out.println("Test de Mi intérprete de código m´´as pequeño (tambien conocido como Brainf**k)");
     }
+
+    @Test
+    public void testEchoUntilByte255Encountered() {
+        assertThat(new BrainLuck(",+[-.,+]").process("Codewars" + ((char) 255)), is("Codewars"));
+    }
+
+    @Test
+    public void testEchoUntilByte0Encountered() {
+        assertThat(new BrainLuck(",[.[-],]").process("Codewars" + ((char) 0)), is("Codewars"));
+    }
+
+    @Test
+    public void testTwoNumbersMultiplier() {
+        final char[] input = {8, 9};
+        assertThat(new BrainLuck(",>,<[>[->+>+<<]>>[-<<+>>]<<<-]>>.").process(String.valueOf(input[0]) + String.valueOf(input[1])), is(String.valueOf((char) (input[0] * input[1]))));
+    }
 }
