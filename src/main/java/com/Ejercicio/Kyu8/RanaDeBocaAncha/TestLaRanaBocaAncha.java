ca2e499c41db1d9f20e771550e119cca3cb9e5e2
Roman31X <die.haa65@gmail.com>
2024-03-24 19:58:45 -0500

    Test aprovados con exito

--- src/main/java/com/Ejercicio/Kyu8/RanaDeBocaAncha/TestLaRanaBocaAncha.java
+++ src/main/java/com/Ejercicio/Kyu8/RanaDeBocaAncha/TestLaRanaBocaAncha.java
@@ -1,11 +1,19 @@
 package com.Ejercicio.Kyu8.RanaDeBocaAncha;
 
 import org.junit.Test;
+
+import static com.Ejercicio.Kyu8.RanaDeBocaAncha.RanaBocaAncha.mouthSize;
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class TestLaRanaBocaAncha {
     public static void main(String[] args) {
         System.out.println("Test de !La rana de boca ancha!");
+    }
+
+    @Test
+    public void fixedTests() {
+        assertEquals("wide", mouthSize("toucan"));
+        assertEquals("wide",mouthSize("ant bear"));
+        assertEquals("small", mouthSize("alligator"));
     }
 }
