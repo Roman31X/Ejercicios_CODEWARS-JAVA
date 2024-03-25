ded45b02127c85f3ca84fa1d04c289d8cadf344d
Roman31X <die.haa65@gmail.com>
2024-03-24 19:52:21 -0500

    Test aprovados con exito

--- src/main/java/com/Ejercicio/Kyu7/RedondearMultiploDe5/TestRedondearMultiploDe5.java
+++ src/main/java/com/Ejercicio/Kyu7/RedondearMultiploDe5/TestRedondearMultiploDe5.java
@@ -1,4 +1,31 @@
 package com.Ejercicio.Kyu7.RedondearMultiploDe5;
 
+import static com.Ejercicio.Kyu7.RedondearMultiploDe5.RedondearSuigienteMultiplo.roundToNext5;
+import static org.junit.Assert.*;
+import java.util.Arrays;
+import org.junit.Test;
+
 public class TestRedondearMultiploDe5 {
+    public static void main(String[] args) {
+        System.out.println("Test Redondear al siguiente múltiplo de 5");
+    }
+    @Test
+    public void basicTests() {
+        int[][] arr = {
+                {0, 0},
+                {1, 5},
+                {3, 5},
+                {5, 5},
+                {7, 10},
+                {39, 40}
+        };
+        Arrays.stream(arr)
+                .forEach(
+                        (testCase) -> {
+                            assertEquals(
+                                    "Input: " + testCase[0],
+                                    testCase[1],
+                                    roundToNext5(testCase[0]));
+                        });
+    }
 }
