85d8860d28f0019a4d36924b1e844c049f483f16
Roman31X <die.haa65@gmail.com>
2024-03-24 19:57:39 -0500

    Importaciones de paquetes de test

--- src/main/java/com/Ejercicio/Kyu8/RanaDeBocaAncha/RanaBocaAncha.java
+++ src/main/java/com/Ejercicio/Kyu8/RanaDeBocaAncha/RanaBocaAncha.java
@@ -1,4 +1,7 @@
 package com.Ejercicio.Kyu8.RanaDeBocaAncha;
 
 public class RanaBocaAncha {
+    public static String mouthSize(String animal){
+        return animal.equalsIgnoreCase("alligator") ? "small" : "wide";
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/RanaDeBocaAncha/TestLaRanaBocaAncha.java
+++ src/main/java/com/Ejercicio/Kyu8/RanaDeBocaAncha/TestLaRanaBocaAncha.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu8.RanaDeBocaAncha;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
 public class TestLaRanaBocaAncha {
+    public static void main(String[] args) {
+        System.out.println("Test de !La rana de boca ancha!");
+    }
 }
