4e0e64e19ac9f468278bff89c30a0ffe39e56b6d
Roman31X <die.haa65@gmail.com>
2024-03-25 22:47:42 -0500

    Ejercicio terminado importaciones de paquetes de test

--- src/main/java/com/Ejercicio/Kyu7/DiversionLetraInversa/DiversionSimpleLetraInversa.java
+++ src/main/java/com/Ejercicio/Kyu7/DiversionLetraInversa/DiversionSimpleLetraInversa.java
@@ -1,4 +1,16 @@
 package com.Ejercicio.Kyu7.DiversionLetraInversa;
 
 public class DiversionSimpleLetraInversa {
+    public static String reverseLetter(final String str) {
+        StringBuilder reversed = new StringBuilder();
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char c = str.charAt(i);
+            if (Character.isLetter(c)) {
+                reversed.append(c);
+            }
+        }
+
+        return reversed.toString();
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/DiversionLetraInversa/TestDiversionSimpleLetraInversa.java
+++ src/main/java/com/Ejercicio/Kyu7/DiversionLetraInversa/TestDiversionSimpleLetraInversa.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu7.DiversionLetraInversa;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
 public class TestDiversionSimpleLetraInversa {
+    public static void main(String[] args) {
+        System.out.println();
+    }
 }
