333c6ef741d92f2b6b67eb99fca9f6627afa0a35
Roman31X <die.haa65@gmail.com>
2024-03-25 23:09:09 -0500

    Test propuestos y importacion de paquetes junit

--- src/main/java/com/Ejercicio/Kyu6/EvaluarGrupoParentesis/EvaluarGrupoDeParentesis.java
+++ src/main/java/com/Ejercicio/Kyu6/EvaluarGrupoParentesis/EvaluarGrupoDeParentesis.java
@@ -1,4 +1,22 @@
 package com.Ejercicio.Kyu6.EvaluarGrupoParentesis;
 
+import java.util.*;
+
 public class EvaluarGrupoDeParentesis {
+    public static int evalParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        int score = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(score);
+                score = 0;
+            } else {
+                int prevScore = stack.pop();
+                score = prevScore + Math.max(score * 2, 1);
+            }
+        }
+
+        return score;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu6/EvaluarGrupoParentesis/TestEvaluarElGrupoDeParentesis.java
+++ src/main/java/com/Ejercicio/Kyu6/EvaluarGrupoParentesis/TestEvaluarElGrupoDeParentesis.java
@@ -1,22 +1,12 @@
 package com.Ejercicio.Kyu6.EvaluarGrupoParentesis;
 
-import java.util.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestEvaluarElGrupoDeParentesis {
-    public static int evalParentheses(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int score = 0;
-
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(score);
-                score = 0;
-            } else {
-                int prevScore = stack.pop();
-                score = prevScore + Math.max(score * 2, 1);
-            }
-        }
-
-        return score;
+    public static void main(String[] args) {
+        System.out.println("Test Evaluar el grupo de paréntesis");
     }
+
+
 }
