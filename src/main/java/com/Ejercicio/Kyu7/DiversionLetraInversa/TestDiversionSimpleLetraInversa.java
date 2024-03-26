33e76de8100cb2f909e495edb2609bc7b8c00c92
Roman31X <die.haa65@gmail.com>
2024-03-25 22:48:54 -0500

    Test aprovados con exito

--- src/main/java/com/Ejercicio/Kyu7/DiversionLetraInversa/TestDiversionSimpleLetraInversa.java
+++ src/main/java/com/Ejercicio/Kyu7/DiversionLetraInversa/TestDiversionSimpleLetraInversa.java
@@ -1,11 +1,23 @@
 package com.Ejercicio.Kyu7.DiversionLetraInversa;
 
 import org.junit.Test;
+import static com.Ejercicio.Kyu7.DiversionLetraInversa.DiversionSimpleLetraInversa.reverseLetter;
 import static org.junit.Assert.assertEquals;
-import org.junit.runners.JUnit4;
 
 public class TestDiversionSimpleLetraInversa {
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Test Diversión simple #176: Letra inversa");
+    }
+
+
+    @Test
+    public void testSomething() {
+        doTest("krishan", "nahsirk");
+        doTest("ultr53o?n", "nortlu");
+        doTest("ab23c", "cba");
+        doTest("krish21an", "nahsirk");
+    }
+    private void doTest(final String str, final String expected) {
+        assertEquals(expected, reverseLetter(str));
     }
 }
