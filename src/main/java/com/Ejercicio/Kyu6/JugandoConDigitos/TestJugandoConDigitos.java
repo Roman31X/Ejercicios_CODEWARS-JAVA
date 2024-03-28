0bfae1088dd9e3f05d6e3f36a1ad41ce6d838bff
Roman31X <die.haa65@gmail.com>
2024-03-28 15:15:00 -0500

    Test planteados y aprovados con exito

--- src/main/java/com/Ejercicio/Kyu6/JugandoConDigitos/TestJugandoConDigitos.java
+++ src/main/java/com/Ejercicio/Kyu6/JugandoConDigitos/TestJugandoConDigitos.java
@@ -1,4 +1,25 @@
 package com.Ejercicio.Kyu6.JugandoConDigitos;
 
+import static com.Ejercicio.Kyu6.JugandoConDigitos.CalculoJugandoConDigitos.digPow;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestJugandoConDigitos {
+    public static void main(String[] args) {
+        System.out.println("Test Jugando con dígitos");
+    }
+
+    @Test
+    public void Test1() {
+        assertEquals(1, digPow(89, 1));
+    }
+    @Test
+    public void Test2() {
+        assertEquals(-1, digPow(92, 1));
+    }
+    @Test
+    public void Test3() {
+        assertEquals(51, digPow(46288, 3));
+    }
+
 }
