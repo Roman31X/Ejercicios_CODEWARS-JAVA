a0084df0621a8d106c870c11e27455b0b95232c1
Roman31X <die.haa65@gmail.com>
2024-03-28 15:10:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/JugandoConDigitos/CalculoJugandoConDigitos.java
+++ src/main/java/com/Ejercicio/Kyu6/JugandoConDigitos/CalculoJugandoConDigitos.java
@@ -1,4 +1,16 @@
 package com.Ejercicio.Kyu6.JugandoConDigitos;
 
 public class CalculoJugandoConDigitos {
+    public static long digPow(int n, int p) {
+        String numStr = String.valueOf(n);
+        int sum = 0;
+        for (int i = 0; i < numStr.length(); i++) {
+            sum += Math.pow(Character.getNumericValue(numStr.charAt(i)), p + i);
+        }
+        if (sum % n == 0) {
+            return sum / n;
+        } else {
+            return -1;
+        }
+    }
 }
