7fe3e9e6a4599bff7ad14229b2ea24aa25b9c003
Roman31X <die.haa65@gmail.com>
2024-03-27 20:34:36 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/SecuenciaTribonacci/CalcularSecuenciaTribonacci.java
+++ src/main/java/com/Ejercicio/Kyu6/SecuenciaTribonacci/CalcularSecuenciaTribonacci.java
@@ -1,4 +1,26 @@
 package com.Ejercicio.Kyu6.SecuenciaTribonacci;
 
 public class CalcularSecuenciaTribonacci {
+    public double[] tribonacci(double[] s, int n) {
+        if (n == 0) {
+            return new double[0];
+        }
+
+        double[] result = new double[n];
+        if (n >= 1) {
+            result[0] = s[0];
+        }
+        if (n >= 2) {
+            result[1] = s[1];
+        }
+        if (n >= 3) {
+            result[2] = s[2];
+        }
+
+        for (int i = 3; i < n; i++) {
+            result[i] = result[i - 1] + result[i - 2] + result[i - 3];
+        }
+
+        return result;
+    }
 }
