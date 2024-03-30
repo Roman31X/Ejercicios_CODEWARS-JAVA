935d0bdfc5bec81aa9b71a50781fed2a288b127d
Roman31X <die.haa65@gmail.com>
2024-03-30 15:00:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu5/MejorViaje/CalcularMejorViaje.java
+++ src/main/java/com/Ejercicio/Kyu5/MejorViaje/CalcularMejorViaje.java
@@ -1,4 +1,29 @@
 package com.Ejercicio.Kyu5.MejorViaje;
 
+import java.util.*;
+
 public class CalcularMejorViaje {
+    public static Integer chooseBestSum(int t, int k, List<Integer> ls) {
+        return chooseBestSumHelper(t, k, ls, 0, 0);
+    }
+
+    private static Integer chooseBestSumHelper(int t, int k, List<Integer> ls, int index, int currentSum) {
+        if (k == 0) {
+            return currentSum <= t ? currentSum : null;
+        }
+        if (index == ls.size()) {
+            return null;
+        }
+
+        Integer includeCurrent = chooseBestSumHelper(t, k - 1, ls, index + 1, currentSum + ls.get(index));
+        Integer excludeCurrent = chooseBestSumHelper(t, k, ls, index + 1, currentSum);
+
+        if (includeCurrent == null) {
+            return excludeCurrent;
+        }
+        if (excludeCurrent == null) {
+            return includeCurrent;
+        }
+        return Math.max(includeCurrent, excludeCurrent);
+    }
 }
--- src/main/java/com/Ejercicio/Kyu5/MejorViaje/TestMejorViaje.java
+++ src/main/java/com/Ejercicio/Kyu5/MejorViaje/TestMejorViaje.java
@@ -1,4 +1,9 @@
 package com.Ejercicio.Kyu5.MejorViaje;
 
+
+
 public class TestMejorViaje {
+    public static void main(String[] args) {
+        System.out.println("Test Mejor Viaje");
+    }
 }
