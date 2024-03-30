6c4797449ecc739be5c0f265ed93cdad0b86f805
Roman31X <die.haa65@gmail.com>
2024-03-30 15:10:00 -0500

    Test aprovados con exito

--- src/main/java/com/Ejercicio/Kyu5/MejorViaje/TestMejorViaje.java
+++ src/main/java/com/Ejercicio/Kyu5/MejorViaje/TestMejorViaje.java
@@ -1,9 +1,29 @@
 package com.Ejercicio.Kyu5.MejorViaje;
 
-
+import static com.Ejercicio.Kyu5.MejorViaje.CalcularMejorViaje.chooseBestSum;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class TestMejorViaje {
     public static void main(String[] args) {
         System.out.println("Test Mejor Viaje");
+    }
+
+    @Test
+    public void BasicTests1() {
+        System.out.println("****** Basic Tests small numbers******");
+        List<Integer> ts = new ArrayList<>(Arrays.asList(50, 55, 56, 57, 58));
+        int n = chooseBestSum(163, 3, ts);
+        assertEquals(163, n);
+        ts = new ArrayList<>(Arrays.asList(50));
+        Integer m = chooseBestSum(163, 3, ts);
+        assertEquals(null, m);
+        ts = new ArrayList<>(Arrays.asList(91, 74, 73, 85, 73, 81, 87));
+        n = chooseBestSum(230, 3, ts);
+        assertEquals(228, n);
     }
 }
