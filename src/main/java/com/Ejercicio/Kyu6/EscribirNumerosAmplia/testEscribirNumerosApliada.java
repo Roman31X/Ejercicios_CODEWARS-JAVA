fb36bdafc11dc7e4a016d1ba80b9d5c75d531c1a
Roman31X <die.haa65@gmail.com>
2024-04-06 17:21:08 -0500

    Test propuestos y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu6/EscribirNumerosAmplia/testEscribirNumerosApliada.java
+++ src/main/java/com/Ejercicio/Kyu6/EscribirNumerosAmplia/testEscribirNumerosApliada.java
@@ -1,4 +1,19 @@
 package com.Ejercicio.Kyu6.EscribirNumerosAmplia;
 
+import org.junit.Test;
+import static com.Ejercicio.Kyu6.EscribirNumerosAmplia.EscribirNumerosDeFomaAmplia.expandedForm;
+import static org.junit.Assert.assertEquals;
+
 public class testEscribirNumerosApliada {
+    public static void main(String[] args) {
+        System.out.println("TestEscribir números en forma ampliada ");
+    }
+
+    @Test
+    public void testSomething() {
+        assertEquals("10 + 2", expandedForm(12));
+        assertEquals("40 + 2", expandedForm(42));
+        assertEquals("70000 + 300 + 4", expandedForm(70304));
+    }
+
 }
