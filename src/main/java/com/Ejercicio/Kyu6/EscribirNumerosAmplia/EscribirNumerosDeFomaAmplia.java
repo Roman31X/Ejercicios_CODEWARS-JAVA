bd927328b8ae2a17339719368b44032cdb81680a
Roman31X <die.haa65@gmail.com>
2024-04-06 17:19:48 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu6/EscribirNumerosAmplia/EscribirNumerosDeFomaAmplia.java
+++ src/main/java/com/Ejercicio/Kyu6/EscribirNumerosAmplia/EscribirNumerosDeFomaAmplia.java
@@ -1,4 +1,24 @@
 package com.Ejercicio.Kyu6.EscribirNumerosAmplia;
 
 public class EscribirNumerosDeFomaAmplia {
+    public static String expandedForm(int num)
+    {
+        String numString = String.valueOf(num);
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < numString.length(); i++) {
+            int digit = Character.getNumericValue(numString.charAt(i));
+            if (digit != 0) {
+                if (result.length() != 0) {
+                    result.append(" + ");
+                }
+                result.append(digit);
+                for (int j = i + 1; j < numString.length(); j++) {
+                    result.append("0");
+                }
+            }
+        }
+
+        return result.toString();
+    }
 }
