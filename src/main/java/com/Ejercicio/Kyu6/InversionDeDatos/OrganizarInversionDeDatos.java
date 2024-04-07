6dd7b3bea8ea87438d5f0df1513a6464032e8b0c
Roman31X <die.haa65@gmail.com>
2024-04-07 15:25:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/InversionDeDatos/OrganizarInversionDeDatos.java
+++ src/main/java/com/Ejercicio/Kyu6/InversionDeDatos/OrganizarInversionDeDatos.java
@@ -1,4 +1,18 @@
 package com.Ejercicio.Kyu6.InversionDeDatos;
 
 public class OrganizarInversionDeDatos {
+    public static int[] DataReverse(int[] data) {
+        int[] reversedData = new int[data.length];
+        int segmentSize = 8;
+        int segmentCount = data.length / segmentSize;
+
+        // Reverse the segments
+        for (int i = 0; i < segmentCount; i++) {
+            for (int j = 0; j < segmentSize; j++) {
+                reversedData[(segmentCount - i - 1) * segmentSize + j] = data[i * segmentSize + j];
+            }
+        }
+
+        return reversedData;
+    }
 }
