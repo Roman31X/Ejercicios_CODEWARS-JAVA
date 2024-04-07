4b446d8d6ae5dbd324afff3d752c6ca16291c59f
Roman31X <die.haa65@gmail.com>
2024-04-07 15:10:00 -0500

    Test propuestos y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu5/PepePanadero/TestPepeElPanadero.java
+++ src/main/java/com/Ejercicio/Kyu5/PepePanadero/TestPepeElPanadero.java
@@ -1,4 +1,41 @@
 package com.Ejercicio.Kyu5.PepePanadero;
 
+import org.junit.jupiter.api.Test;
+import static com.Ejercicio.Kyu5.PepePanadero.PanaderoPedidos.cakes;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Map;
+
 public class TestPepeElPanadero {
+    public static void main(String[] args) {
+        System.out.println("Test Pepe el panadero recetas");
+    }
+
+    @Test
+    void basicTest() {
+        Map<String, Integer> recipe = Map.of(
+                "flour", 500,
+                "sugar", 200,
+                "eggs", 1);
+        Map<String, Integer> available = Map.of(
+                "flour", 1200,
+                "sugar", 1200,
+                "eggs", 5,
+                "milk", 200);
+        assertEquals(2, cakes(recipe, available), "For recipe " + recipe + " and ingredients " + available);
+    }
+
+    @Test
+    void missingIngredient() {
+        Map<String, Integer> recipe = Map.of(
+                "flour", 500,
+                "sugar", 200,
+                "eggs", 1,
+                "cinnamon", 300);
+        Map<String, Integer> available = Map.of(
+                "flour", 1200,
+                "sugar", 1200,
+                "eggs", 5,
+                "milk", 200);
+        assertEquals(0, cakes(recipe, available), "For recipe " + recipe + " and ingredients " + available);
+    }
 }
