0684441eff80c8a622f56d5a1bf4785928aa2a2e
Roman31X <die.haa65@gmail.com>
2024-04-07 15:00:00 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu5/PepePanadero/PanaderoPedidos.java
+++ src/main/java/com/Ejercicio/Kyu5/PepePanadero/PanaderoPedidos.java
@@ -1,4 +1,20 @@
 package com.Ejercicio.Kyu5.PepePanadero;
 
+import java.util.*;
+
 public class PanaderoPedidos {
+    public static int cakes(Map<String, Integer> recipe, Map<String, Integer> available) {
+        int maxCakes = Integer.MAX_VALUE;
+        for (Map.Entry<String, Integer> entry : recipe.entrySet()) {
+            String ingredient = entry.getKey();
+            int requiredAmount = entry.getValue();
+
+            if (!available.containsKey(ingredient) || available.get(ingredient) < requiredAmount) {
+                return 0;
+            }
+            maxCakes = Math.min(maxCakes, available.get(ingredient) / requiredAmount);
+        }
+
+        return maxCakes;
+    }
 }
