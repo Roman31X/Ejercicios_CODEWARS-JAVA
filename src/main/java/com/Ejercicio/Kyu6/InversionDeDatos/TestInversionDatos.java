4cc8203d84b63f5fd18331772efbe0437a516ea3
Roman31X <die.haa65@gmail.com>
2024-04-07 15:30:00 -0500

    Test planteados y desarrollados con exito

--- src/main/java/com/Ejercicio/Kyu6/InversionDeDatos/TestInversionDatos.java
+++ src/main/java/com/Ejercicio/Kyu6/InversionDeDatos/TestInversionDatos.java
@@ -1,4 +1,26 @@
 package com.Ejercicio.Kyu6.InversionDeDatos;
 
+import org.junit.Test;
+import static com.Ejercicio.Kyu6.InversionDeDatos.OrganizarInversionDeDatos.DataReverse;
+import static org.junit.Assert.assertArrayEquals;
+
 public class TestInversionDatos {
+    public static void main(String[] args) {
+        System.out.println("Test de Inversión de datos");
+    }
+
+    @Test
+    public void Test1() {
+        int[] data1= {1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0};
+        int[] data2= {1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1};
+        assertArrayEquals(data2, DataReverse(data1));
+    }
+
+    @Test
+    public void Test2() {
+        int[] data1= {0,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1};
+        int[] data2= {0,0,1,0,1,0,0,1,0,0,1,1,0,1,1,0};
+        assertArrayEquals(data2, DataReverse(data1));
+
+    }
 }
