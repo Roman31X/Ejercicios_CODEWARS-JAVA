40e0b964558e9c9b9bd1d00defe1aced35f6da88
Roman31X <die.haa65@gmail.com>
2024-04-09 21:03:19 -0500

    Ejercicio desarollado

--- src/main/java/com/Ejercicio/Kyu8/RegresarElDia/QueDiaEsHoy.java
+++ src/main/java/com/Ejercicio/Kyu8/RegresarElDia/QueDiaEsHoy.java
@@ -1,4 +1,13 @@
 package com.Ejercicio.Kyu8.RegresarElDia;
 
 public class QueDiaEsHoy {
+    public static String getDay(int n) {
+        return (n==1)?"Sunday":
+                (n==2)?"Monday":
+                (n==3)?"Tuesday":
+                (n==4)?"Wednesday":
+                (n==5)?"Thursday":
+                (n==6)?"Friday":
+                (n==7)?"Saturday":"Wrong, please enter a number between 1 and 7";
+    }
 }
