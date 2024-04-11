72c0de7815f2af268815728cc1255ab99de53d6e
Roman31X <die.haa65@gmail.com>
2024-04-11 18:48:12 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/Prueba123/EvaluarTest123.java
+++ src/main/java/com/Ejercicio/Kyu7/Prueba123/EvaluarTest123.java
@@ -1,4 +1,17 @@
 package com.Ejercicio.Kyu7.Prueba123;
 
+import java.util.*;
+
 public class EvaluarTest123 {
+    public static List<String> number(List<String> lines) {
+        List<String> numberedLines = new ArrayList<>();
+
+        for (int i = 0; i < lines.size(); i++) {
+            int lineNumber = i + 1;
+            String numberedLine = lineNumber + ": " + lines.get(i);
+            numberedLines.add(numberedLine);
+        }
+
+        return numberedLines;
+    }
 }
