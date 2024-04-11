b06c27aa1d7310729dac52c4105286febdd5328b
Roman31X <die.haa65@gmail.com>
2024-04-10 22:22:01 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/OrdenarAplaraMtriz/AplanarOrdenarMatriz.java
+++ src/main/java/com/Ejercicio/Kyu7/OrdenarAplaraMtriz/AplanarOrdenarMatriz.java
@@ -1,4 +1,25 @@
 package com.Ejercicio.Kyu7.OrdenarAplaraMtriz;
 
+import java.util.*;
+
 public class AplanarOrdenarMatriz {
+    public static int[] flattenAndSort(int[][] array) {
+        int totalElements = 0;
+        for (int[] subArray : array) {
+            totalElements += subArray.length;
+        }
+
+        int[] flattenedArray = new int[totalElements];
+
+        int index = 0;
+        for (int[] subArray : array) {
+            for (int num : subArray) {
+                flattenedArray[index++] = num;
+            }
+        }
+
+        Arrays.sort(flattenedArray);
+
+        return flattenedArray;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/OrdenarAplaraMtriz/TestAplanarYOrdenarMatriz.java
+++ src/main/java/com/Ejercicio/Kyu7/OrdenarAplaraMtriz/TestAplanarYOrdenarMatriz.java
@@ -1,4 +1,7 @@
 package com.Ejercicio.Kyu7.OrdenarAplaraMtriz;
 
 public class TestAplanarYOrdenarMatriz {
+    public static void main(String[] args) {
+
+    }
 }
