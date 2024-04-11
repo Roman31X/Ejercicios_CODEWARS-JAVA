1358fad0917bddb0da46f6dce2328fc1b5cb4195
Roman31X <die.haa65@gmail.com>
2024-04-10 22:15:32 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/QuitarElMinimo/EliminarElMinimoArrays.java
+++ src/main/java/com/Ejercicio/Kyu7/QuitarElMinimo/EliminarElMinimoArrays.java
@@ -1,4 +1,26 @@
 package com.Ejercicio.Kyu7.QuitarElMinimo;
 
 public class EliminarElMinimoArrays {
+    public static int[] removeSmallest(int[] numbers) {
+        if (numbers.length <= 1) {
+            return new int[0];
+        }
+
+        int minIndex = 0;
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] < numbers[minIndex]) {
+                minIndex = i;
+            }
+        }
+
+        int[] result = new int[numbers.length - 1];
+
+        for (int i = 0, j = 0; i < numbers.length; i++) {
+            if (i != minIndex) {
+                result[j++] = numbers[i];
+            }
+        }
+
+        return result;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/QuitarElMinimo/TestQuitarElMinimo.java
+++ src/main/java/com/Ejercicio/Kyu7/QuitarElMinimo/TestQuitarElMinimo.java
@@ -1,4 +1,7 @@
 package com.Ejercicio.Kyu7.QuitarElMinimo;
 
 public class TestQuitarElMinimo {
+    public static void main(String[] args) {
+
+    }
 }
