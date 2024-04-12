44e488cb693f26fddc5bf3a97ed8f8d9898b40b0
Roman31X <die.haa65@gmail.com>
2024-04-12 15:25:00 -0500

    Ejercicio Terminando con test aprobados

--- src/main/java/com/Ejercicio/Kyu8/EntrenamientoIfElse/EntrenandoConIfElse.java
+++ src/main/java/com/Ejercicio/Kyu8/EntrenamientoIfElse/EntrenandoConIfElse.java
@@ -1,4 +1,13 @@
 package com.Ejercicio.Kyu8.EntrenamientoIfElse;
 
 public class EntrenandoConIfElse {
+    public static int saleHotdogs(final int n){
+        if (n < 5) {
+            return n * 100;
+        } else if (n >= 5 && n < 10) {
+            return n * 95;
+        } else {
+            return n * 90;
+        }
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/EntrenamientoIfElse/TestEntrenandoConIfELSE.java
+++ src/main/java/com/Ejercicio/Kyu8/EntrenamientoIfElse/TestEntrenandoConIfELSE.java
@@ -1,4 +1,23 @@
 package com.Ejercicio.Kyu8.EntrenamientoIfElse;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu8.EntrenamientoIfElse.EntrenandoConIfElse.saleHotdogs;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
 public class TestEntrenandoConIfELSE {
+    public static void main(String[] args) {
+        System.out.println("Test de Entrenamiento JS #7: if...else y operadores ternarios");
+    }
+
+    @Test
+    public void BasicTests() {
+        assertEquals(100,saleHotdogs(1));
+        assertEquals(400,saleHotdogs(4));
+        assertEquals(475,saleHotdogs(5));
+        assertEquals(855,saleHotdogs(9));
+        assertEquals(900,saleHotdogs(10));
+        assertEquals(9000,saleHotdogs(100));
+    }
 }
