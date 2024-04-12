5eba282efd4edc68334333c7262ab60a5273a3f0
Roman31X <die.haa65@gmail.com>
2024-04-12 15:05:00 -0500

    Ejercicio COMPLETADO

--- src/main/java/com/Ejercicio/Kyu8/NombreEnCartelera/CostoNombreCartelera.java
+++ src/main/java/com/Ejercicio/Kyu8/NombreEnCartelera/CostoNombreCartelera.java
@@ -1,4 +1,15 @@
 package com.Ejercicio.Kyu8.NombreEnCartelera;
 
 public class CostoNombreCartelera {
+    public static int billboard(String name, int price) {
+        int length = name.length();
+
+        int totalCost = 0;
+
+        for (int i = 0; i < length; i++) {
+            totalCost += price;
+        }
+
+        return totalCost;
+    }
 }
