bee416f1574f4a5f015230fd4f3a19655337384e
Roman31X <die.haa65@gmail.com>
2024-04-13 20:24:33 -0500

    Ejercicio desarrollado y test aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/DiferenciaVolumenesCuboides/CalcularVolumenCuboides.java
+++ src/main/java/com/Ejercicio/Kyu8/DiferenciaVolumenesCuboides/CalcularVolumenCuboides.java
@@ -1,4 +1,9 @@
 package com.Ejercicio.Kyu8.DiferenciaVolumenesCuboides;
 
 public class CalcularVolumenCuboides {
+    public static int findDifference(final int[] firstCuboid, final int[] secondCuboid) {
+        int volumeFirst = firstCuboid[0] * firstCuboid[1] * firstCuboid[2];
+        int volumeSecond = secondCuboid[0] * secondCuboid[1] * secondCuboid[2];
+        return Math.abs(volumeFirst - volumeSecond);
+    }
 }
--- src/main/java/com/Ejercicio/Kyu8/DiferenciaVolumenesCuboides/TestDiferenciaVolumenCuboides.java
+++ src/main/java/com/Ejercicio/Kyu8/DiferenciaVolumenesCuboides/TestDiferenciaVolumenCuboides.java
@@ -1,4 +1,21 @@
 package com.Ejercicio.Kyu8.DiferenciaVolumenesCuboides;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu8.DiferenciaVolumenesCuboides.CalcularVolumenCuboides.findDifference;
+import static org.junit.Assert.assertEquals;
+
 public class TestDiferenciaVolumenCuboides {
+    public static void main(String[] args) {
+        System.out.println("Test de Diferencias de volumen de cuboides");
+    }
+
+    @Test
+    public void sampleTestCases() {
+        assertEquals(14, findDifference(new int[]{3, 2, 5}, new int[]{1, 4, 4}));
+        assertEquals(106, findDifference(new int[]{9, 7, 2}, new int[]{5, 2, 2}));
+        assertEquals(30, findDifference(new int[]{11, 2, 5}, new int[]{1, 10, 8}));
+        assertEquals(31, findDifference(new int[]{4, 4, 7}, new int[]{3, 9, 3}));
+        assertEquals(0, findDifference(new int[]{15, 20, 25}, new int[]{10, 30, 25}));
+    }
 }
