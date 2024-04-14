e5704d5f8d90f1ac1b47d77d523399ca85180dd3
Roman31X <die.haa65@gmail.com>
2024-04-13 20:18:17 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/CantidadTotalPuntos/ContadorPuntosArrays.java
+++ src/main/java/com/Ejercicio/Kyu8/CantidadTotalPuntos/ContadorPuntosArrays.java
@@ -1,4 +1,23 @@
 package com.Ejercicio.Kyu8.CantidadTotalPuntos;
 
 public class ContadorPuntosArrays {
+    public static int points(String[] games) {
+        int puntos = 0;
+
+        for (String resultado : games) {
+            String[] partes = resultado.split(":");
+            int x = Integer.parseInt(partes[0]);
+            int y = Integer.parseInt(partes[1]);
+
+            if (x > y) {
+                puntos += 3; // Ganar
+            } else if (x < y) {
+                puntos += 0; // Pérdida
+            } else {
+                puntos += 1; // Empate
+            }
+        }
+
+        return puntos;
+    }
 }
