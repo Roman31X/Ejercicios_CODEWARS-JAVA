e7f58d085c878fdfde4df1860e6dad1370af1403
Roman31X <die.haa65@gmail.com>
2024-04-14 20:50:14 -0500

    Test planateados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/CapitalizacionAlternativa/TestCapitalizacionAlternativa.java
+++ src/main/java/com/Ejercicio/Kyu7/CapitalizacionAlternativa/TestCapitalizacionAlternativa.java
@@ -1,4 +1,18 @@
 package com.Ejercicio.Kyu7.CapitalizacionAlternativa;
 
+import org.junit.Test;
+import static com.Ejercicio.Kyu7.CapitalizacionAlternativa.ConvertirMayuscularOrden.capitalize;
+import static org.junit.Assert.assertArrayEquals;
+
 public class TestCapitalizacionAlternativa {
+    public static void main(String[] args) {
+        System.out.println("Test Capitalización alternativa");
+    }
+    @Test
+    public void basicTests(){
+        assertArrayEquals(new String[]{"AbCdEf", "aBcDeF"}, capitalize("abcdef"));
+        assertArrayEquals(new String[]{"CoDeWaRs", "cOdEwArS"}, capitalize("codewars"));
+        assertArrayEquals(new String[]{"AbRaCaDaBrA", "aBrAcAdAbRa"}, capitalize("abracadabra"));
+        assertArrayEquals(new String[]{"CoDeWaRrIoRs", "cOdEwArRiOrS"}, capitalize("codewarriors"));
+    }
 }
