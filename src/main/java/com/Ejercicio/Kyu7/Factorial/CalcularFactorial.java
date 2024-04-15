d2abbc99c1fdfbe5e0a60fd49393e275e323e3ec
Roman31X <die.haa65@gmail.com>
2024-04-15 15:15:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/Factorial/CalcularFactorial.java
+++ src/main/java/com/Ejercicio/Kyu7/Factorial/CalcularFactorial.java
@@ -1,4 +1,20 @@
 package com.Ejercicio.Kyu7.Factorial;
 
 public class CalcularFactorial {
+    public int factorial(int n) {
+        if (n < 0 || n > 12) {
+            throw new IllegalArgumentException("Argument out of range");
+        }
+
+        if (n == 0) {
+            return 1;
+        }
+
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
 }
