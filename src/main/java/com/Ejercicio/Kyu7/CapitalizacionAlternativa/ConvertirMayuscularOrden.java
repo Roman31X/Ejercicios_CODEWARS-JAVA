702f30cf645678be6e8bbe8f49fb285b31564ec9
Roman31X <die.haa65@gmail.com>
2024-04-14 20:48:33 -0500

    ejercicio completado

--- src/main/java/com/Ejercicio/Kyu7/CapitalizacionAlternativa/ConvertirMayuscularOrden.java
+++ src/main/java/com/Ejercicio/Kyu7/CapitalizacionAlternativa/ConvertirMayuscularOrden.java
@@ -1,4 +1,21 @@
 package com.Ejercicio.Kyu7.CapitalizacionAlternativa;
 
 public class ConvertirMayuscularOrden {
+    public static String[] capitalize(String s){
+        StringBuilder evenIndices = new StringBuilder();
+        StringBuilder oddIndices = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (i % 2 == 0) {
+                evenIndices.append(Character.toUpperCase(c));
+                oddIndices.append(c);
+            } else {
+                evenIndices.append(c);
+                oddIndices.append(Character.toUpperCase(c));
+            }
+        }
+
+        return new String[]{evenIndices.toString(), oddIndices.toString()};
+    }
 }
