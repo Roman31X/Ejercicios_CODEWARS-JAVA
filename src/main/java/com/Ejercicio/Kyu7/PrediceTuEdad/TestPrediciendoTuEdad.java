e4056672916bc1ad435e1941c99bf422ae2bb4e6
Roman31X <die.haa65@gmail.com>
2024-04-14 20:56:32 -0500

    Ejercicio completado y test aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/PrediceTuEdad/TestPrediciendoTuEdad.java
+++ src/main/java/com/Ejercicio/Kyu7/PrediceTuEdad/TestPrediciendoTuEdad.java
@@ -5,5 +5,13 @@ import static com.Ejercicio.Kyu7.PrediceTuEdad.CalcularAñosEnjecerias.predictAg
 import static org.junit.Assert.assertEquals;
 
 public class TestPrediciendoTuEdad {
+    public static void main(String[] args) {
+        System.out.println("Test ¡Predice tu edad!");
+    }
 
+    @Test
+    public void FixedTests() {
+        assertEquals(86, predictAge(65,60,75,55,60,63,64,45));
+        assertEquals(79, predictAge(32,54,76,65,34,63,64,45));
+    }
 }
