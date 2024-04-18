375cf8d7024f4e42259d097b731882dbc22756a5
Roman31X <die.haa65@gmail.com>
2024-04-17 20:34:12 -0500

    Ejercicio completado

--- src/main/java/com/Ejercicio/Kyu7/ObstaculosEnCamino/IdentificarObstaculosCamino.java
+++ src/main/java/com/Ejercicio/Kyu7/ObstaculosEnCamino/IdentificarObstaculosCamino.java
@@ -1,4 +1,21 @@
 package com.Ejercicio.Kyu7.ObstaculosEnCamino;
 
 public class IdentificarObstaculosCamino {
+    public static String bumps(final String road) {
+        int bumpCount = 0; // Contador de baches
+
+        for (int i = 0; i < road.length(); i++) {
+            char c = road.charAt(i);
+            if (c == 'n') {
+                bumpCount++; // Incrementa el contador si encuentra un bache
+            }
+        }
+
+        // Retorna el resultado basado en el contador de baches
+        if (bumpCount <= 15) {
+            return "Woohoo!";
+        } else {
+            return "Car Dead";
+        }
+    }
 }
