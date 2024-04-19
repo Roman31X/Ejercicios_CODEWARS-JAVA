9c74dd19b89005a89ad8ac4b09e3280d09ac838d
Roman31X <die.haa65@gmail.com>
2024-04-19 15:35:09 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/SumaEnesimaTerminoSerie/TestCalcularSumaPrimerEnesimo.java
+++ src/main/java/com/Ejercicio/Kyu7/SumaEnesimaTerminoSerie/TestCalcularSumaPrimerEnesimo.java
@@ -1,4 +1,24 @@
 package com.Ejercicio.Kyu7.SumaEnesimaTerminoSerie;
 
+import static com.Ejercicio.Kyu7.SumaEnesimaTerminoSerie.CalcularTerminoEnesimoSerie.seriesSum;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestCalcularSumaPrimerEnesimo {
+    public static void main(String[] args) {
+        System.out.println("Test Suma de primer enésimo termino de la Serie");
+    }
+
+    @Test
+    public void test1() {
+        assertEquals("1.57", seriesSum(5));
+    }
+    @Test
+    public void test2() {
+        assertEquals("1.77", seriesSum(9));
+    }
+    @Test
+    public void test3() {
+        assertEquals("1.94", seriesSum(15));
+    }
 }
