b009fbf3982581afe029162e9a02f1b9d4c3c5a8
Roman31X <die.haa65@gmail.com>
2024-04-19 15:33:03 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/SumaEnesimaTerminoSerie/CalcularTerminoEnesimoSerie.java
+++ src/main/java/com/Ejercicio/Kyu7/SumaEnesimaTerminoSerie/CalcularTerminoEnesimoSerie.java
@@ -1,4 +1,19 @@
 package com.Ejercicio.Kyu7.SumaEnesimaTerminoSerie;
 
 public class CalcularTerminoEnesimoSerie {
+    public static String seriesSum(int n) {
+        if (n == 0) {
+            return "0.00";
+        }
+
+        double result = 1.0;
+        double denominator = 4.0;
+
+        for (int i = 2; i <= n; i++) {
+            result += 1.0 / denominator;
+            denominator += 3.0;
+        }
+
+        return String.format("%.2f", result);
+    }
 }
