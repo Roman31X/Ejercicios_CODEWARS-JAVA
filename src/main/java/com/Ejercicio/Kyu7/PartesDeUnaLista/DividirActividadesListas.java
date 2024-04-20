c420e4efce88721d83aa172ff272fd9655f6724f
Roman31X <die.haa65@gmail.com>
2024-04-20 08:39:08 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/PartesDeUnaLista/DividirActividadesListas.java
+++ src/main/java/com/Ejercicio/Kyu7/PartesDeUnaLista/DividirActividadesListas.java
@@ -1,4 +1,24 @@
 package com.Ejercicio.Kyu7.PartesDeUnaLista;
 
 public class DividirActividadesListas {
+    public static String[][] partlist(String[] arr) {
+        int n = arr.length;
+        String[][] result = new String[n - 1][2];
+        StringBuilder firstPart = new StringBuilder();
+        StringBuilder secondPart = new StringBuilder();
+
+        for (int i = 0; i < n - 1; i++) {
+            firstPart.append(arr[i]);
+            secondPart.setLength(0);
+            for (int j = i + 1; j < n; j++) {
+                secondPart.append(arr[j]);
+                if (j < n - 1) secondPart.append(" ");
+            }
+            result[i][0] = firstPart.toString();
+            result[i][1] = secondPart.toString();
+            firstPart.append(" ");
+        }
+
+        return result;
+    }
 }
