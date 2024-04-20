047b174d6498b6ca09b5583b8886087300687c8d
Roman31X <die.haa65@gmail.com>
2024-04-20 08:46:37 -0500

    Ejercicio desarrollado y test aprpbados con exito

--- src/main/java/com/Ejercicio/Kyu7/MayoriaDigitos/CalcularDigitoMasLargo.java
+++ src/main/java/com/Ejercicio/Kyu7/MayoriaDigitos/CalcularDigitoMasLargo.java
@@ -1,5 +1,22 @@
 package com.Ejercicio.Kyu7.MayoriaDigitos;
 
 public class CalcularDigitoMasLargo {
+    public static int findLongest(int[] numbers) {
+        int longestNumber = numbers[0]; // Inicializamos con el primer número
+        for (int i = 1; i < numbers.length; i++) {
+            if (countDigits(numbers[i]) > countDigits(longestNumber)) {
+                longestNumber = numbers[i];
+            }
+        }
+        return longestNumber;
+    }
 
+    public static int countDigits(int number) {
+        int count = 0;
+        while (number != 0) {
+            number /= 10;
+            count++;
+        }
+        return count;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/MayoriaDigitos/TestLaMayoriaDeLosDigitos.java
+++ src/main/java/com/Ejercicio/Kyu7/MayoriaDigitos/TestLaMayoriaDeLosDigitos.java
@@ -1,4 +1,42 @@
 package com.Ejercicio.Kyu7.MayoriaDigitos;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu7.MayoriaDigitos.CalcularDigitoMasLargo.findLongest;
+import static org.junit.Assert.assertEquals;
+
 public class TestLaMayoriaDeLosDigitos {
+    public static void main(String[] args) {
+        System.out.println("Test de la mayoría de los dígitos");
+    }
+
+    @Test
+    public void exampleTest1() {
+        assertEquals(100, findLongest(new int[] {1, 10, 100}));
+    }
+
+    @Test
+    public void exampleTest2() {
+        assertEquals(9000, findLongest(new int[] {9000, 8, 800}));
+    }
+
+    @Test
+    public void exampleTest3() {
+        assertEquals(900, findLongest(new int[]  {8, 900, 500}));
+    }
+
+    @Test
+    public void exampleTest4() {
+        assertEquals(40000, findLongest(new int[] {3, 40000, 100}));
+    }
+
+    @Test
+    public void exampleTest5() {
+        assertEquals(100000, findLongest(new int[] {1, 200, 100000}));
+    }
+
+    @Test
+    public void exampleTest6() {
+        assertEquals(-10, findLongest(new int[] {-10, 1, 0, 1, 10}));
+    }
 }
