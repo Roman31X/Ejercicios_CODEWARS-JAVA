16fec3f54d09dcbf2bb2f41a444f4c03e5c71d39
Roman31X <die.haa65@gmail.com>
2024-04-21 15:00:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/GiraEjemploKata/InvertirKataEjercicio.java
+++ src/main/java/com/Ejercicio/Kyu8/GiraEjemploKata/InvertirKataEjercicio.java
@@ -1,4 +1,12 @@
 package com.Ejercicio.Kyu8.GiraEjemploKata;
 
 public class InvertirKataEjercicio {
+    public static String[] kataExampleTwist()
+    {
+        String[] websites = new String[1000];
+        for (int i = 0; i < 1000; i++) {
+            websites[i] = "codewars";
+        }
+        return websites;
+    }
 }
