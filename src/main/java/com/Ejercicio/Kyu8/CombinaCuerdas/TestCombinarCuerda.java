b6627c9969e734e3ae5b7b65d25ae6ea694d4e5b
Roman31X <die.haa65@gmail.com>
2024-04-22 15:10:00 -0500

    Testa aprovados y datos agregados a README

--- src/main/java/com/Ejercicio/Kyu8/CombinaCuerdas/TestCombinarCuerda.java
+++ src/main/java/com/Ejercicio/Kyu8/CombinaCuerdas/TestCombinarCuerda.java
@@ -5,5 +5,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class TestCombinarCuerda {
-
+    public static void main(String[] args) {
+        System.out.println("Test ");
+    }
+    @Test
+    void test() {
+        assertEquals("James Stevens",
+                combineNames("James","Stevens"));
+    }
 }
