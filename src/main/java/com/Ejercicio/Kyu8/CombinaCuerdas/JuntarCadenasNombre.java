43c922daaba96ad525bf4a24772dd5ec7c04ae64
Roman31X <die.haa65@gmail.com>
2024-04-22 15:10:00 -0500

    Ejercicio desarrollado

---
+++ src/main/java/com/Ejercicio/Kyu8/CombinaCuerdas/JuntarCadenasNombre.java
@@ -0,0 +1,7 @@
+package com.Ejercicio.Kyu8.CombinaCuerdas;
+
+public class JuntarCadenasNombre {
+    public static String combineNames(String first,String last) {
+        return first+" "+last;
+    }
+}
---
+++ src/main/java/com/Ejercicio/Kyu8/CombinaCuerdas/TestCombinarCuerda.java
@@ -0,0 +1,9 @@
+package com.Ejercicio.Kyu8.CombinaCuerdas;
+
+import static com.Ejercicio.Kyu8.CombinaCuerdas.JuntarCadenasNombre.combineNames;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestCombinarCuerda {
+
+}
