b491b551216a0a19d0564d34ce74e920fd42bad9
Roman31X <die.haa65@gmail.com>
2024-04-23 01:33:24 -0500

    Test planteados y aprovados con exito

--- src/main/java/com/Ejercicio/Kyu8/EnceuntraPrimerNumeroConsecutivo/TestEncuentraPrimerNumeroConsecutivo.java
+++ src/main/java/com/Ejercicio/Kyu8/EnceuntraPrimerNumeroConsecutivo/TestEncuentraPrimerNumeroConsecutivo.java
@@ -1,4 +1,21 @@
 package com.Ejercicio.Kyu8.EnceuntraPrimerNumeroConsecutivo;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu8.EnceuntraPrimerNumeroConsecutivo.CalcularNumeroConsecutivo.find;
+import static org.junit.Assert.assertEquals;
+
 public class TestEncuentraPrimerNumeroConsecutivo {
+    public static void main(String[] args) {
+        System.out.println("Test Encuentra el primer número no consecutivo");
+    }
+    @Test public void basicTests() {
+        assertEquals(Integer.valueOf(6), find(new int[]{1, 2, 3, 4, 6, 7, 8}));
+        assertEquals(null, find(new int[]{1, 2, 3, 4, 5, 6, 7, 8}));
+        assertEquals(Integer.valueOf(6), find(new int[]{4, 6, 7, 8, 9, 11}));
+        assertEquals(Integer.valueOf(11), find(new int[]{4, 5, 6, 7, 8, 9, 11}));
+        assertEquals(null, find(new int[]{31, 32}));
+        assertEquals(Integer.valueOf(0), find(new int[]{-3, -2, 0, 1}));
+        assertEquals(Integer.valueOf(-1), find(new int[]{-5, -4, -3, -1}));
+    }
 }
