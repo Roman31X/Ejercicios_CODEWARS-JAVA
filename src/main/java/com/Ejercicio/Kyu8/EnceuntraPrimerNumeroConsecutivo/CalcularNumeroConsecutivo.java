75feec085e669de2703237f49ce1d201cf6706ac
Roman31X <die.haa65@gmail.com>
2024-04-23 01:31:34 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/EnceuntraPrimerNumeroConsecutivo/CalcularNumeroConsecutivo.java
+++ src/main/java/com/Ejercicio/Kyu8/EnceuntraPrimerNumeroConsecutivo/CalcularNumeroConsecutivo.java
@@ -1,4 +1,17 @@
 package com.Ejercicio.Kyu8.EnceuntraPrimerNumeroConsecutivo;
 
 public class CalcularNumeroConsecutivo {
+    public static Integer find(final int[] array) {
+        if (array.length <= 1) {
+            return null; // Retorna null si el array está vacío o tiene un solo elemento
+        }
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[i - 1] + 1) {
+                return array[i]; // Retorna el primer elemento no consecutivo encontrado
+            }
+        }
+
+        return null;
+    }
 }
