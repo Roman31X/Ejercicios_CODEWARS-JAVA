22482b1b8622876be82747f2a0f64760c1eaaa01
Roman31X <die.haa65@gmail.com>
2024-04-24 15:10:00 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu6/AyudaLibrero/TestAyudaAlLibros.java
+++ src/main/java/com/Ejercicio/Kyu6/AyudaLibrero/TestAyudaAlLibros.java
@@ -1,4 +1,18 @@
 package com.Ejercicio.Kyu6.AyudaLibrero;
 
+import static com.Ejercicio.Kyu6.AyudaLibrero.OrganizarLibros.stockSummary;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestAyudaAlLibros {
+    public static void main(String[] args) {
+        System.out.println("Test ayuda al librero");
+    }
+    @Test
+    public void test1() {
+        String art[] = new String[]{"ABAR 200", "CDXE 500", "BKWR 250", "BTSQ 890", "DRTY 600"};
+        String cd[] = new String[] {"A", "B"};
+        assertEquals("(A : 200) - (B : 1140)",
+                stockSummary(art, cd));
+    }
 }
