2d20307b14644241fe3cc5c2559c3bd273116d6a
Roman31X <die.haa65@gmail.com>
2024-04-26 15:35:21 -0500

    ejercicico completado

--- src/main/java/com/Ejercicio/Kyu8/AnalizarProblemaCaracterEdad/ConvertirStringAIntEdad.java
+++ src/main/java/com/Ejercicio/Kyu8/AnalizarProblemaCaracterEdad/ConvertirStringAIntEdad.java
@@ -1,4 +1,9 @@
 package com.Ejercicio.Kyu8.AnalizarProblemaCaracterEdad;
 
 public class ConvertirStringAIntEdad {
+    public static int howOld(final String herOld) {
+        char firstChar = herOld.charAt(0);
+        int edad = Character.getNumericValue(firstChar);
+        return edad;
+    }
 }
