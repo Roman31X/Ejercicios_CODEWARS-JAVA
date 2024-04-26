029c47a0a0d3121f36ea8235edf2ec4ad46e9df8
Roman31X <die.haa65@gmail.com>
2024-04-26 15:37:08 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/AnalizarProblemaCaracterEdad/TestAnalizarBuenintDelProblemaDeCaracter.java
+++ src/main/java/com/Ejercicio/Kyu8/AnalizarProblemaCaracterEdad/TestAnalizarBuenintDelProblemaDeCaracter.java
@@ -1,4 +1,24 @@
 package com.Ejercicio.Kyu8.AnalizarProblemaCaracterEdad;
 
+import static com.Ejercicio.Kyu8.AnalizarProblemaCaracterEdad.ConvertirStringAIntEdad.howOld;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestAnalizarBuenintDelProblemaDeCaracter {
+    public static void main(String[] args) {
+        System.out.println("Analizar buen ont del problema de caracteres");
+
+    }
+    @Test
+    public void test1() {
+        assertEquals(5, howOld("5 years old"));
+    }
+    @Test
+    public void test2() {
+        assertEquals(9, howOld("9 years old"));
+    }
+    @Test
+    public void test3() {
+        assertEquals(1, howOld("1 year old"));
+    }
 }
