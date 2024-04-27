b84dfcef634b0c86df953ffa3bbcbd37711b30df
Roman31X <die.haa65@gmail.com>
2024-04-27 15:51:36 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/CalculadoraPropinas/TestCalcularPropinas.java
+++ src/main/java/com/Ejercicio/Kyu8/CalculadoraPropinas/TestCalcularPropinas.java
@@ -1,4 +1,30 @@
 package com.Ejercicio.Kyu8.CalculadoraPropinas;
 
+import org.junit.Test;
+import static com.Ejercicio.Kyu8.CalculadoraPropinas.CalcularPropinas.calculateTip;
+import static org.junit.Assert.*;
+
 public class TestCalcularPropinas {
+    public static void main(String[] args) {
+        System.out.println("Test calculadora de propinas");
+    }
+    @Test
+    public void test1() {
+        assertEquals(new Integer(2), calculateTip(30d, "poor"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(new Integer(4), calculateTip(20d, "Excellent"));
+    }
+
+    @Test
+    public void test3() {
+        assertNull(calculateTip(20d, "hi"));
+    }
+
+    @Test
+    public void test4() {
+        assertEquals(new Integer(17), calculateTip(107.65, "GReat"));
+    }
 }
