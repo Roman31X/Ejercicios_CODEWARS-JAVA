a27fb1053d3af08179986c81463c5a601bea801f
Roman31X <die.haa65@gmail.com>
2024-04-28 15:25:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/GolesMessi/CalcularGolesMessi.java
+++ src/main/java/com/Ejercicio/Kyu8/GolesMessi/CalcularGolesMessi.java
@@ -1,4 +1,10 @@
 package com.Ejercicio.Kyu8.GolesMessi;
 
 public class CalcularGolesMessi {
+    public static int laLigaGoals = 43;
+    public static int championsLeagueGoals = 10;
+    public static int copaDelReyGoals = 5;
+
+    public static int totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals;
+
 }
