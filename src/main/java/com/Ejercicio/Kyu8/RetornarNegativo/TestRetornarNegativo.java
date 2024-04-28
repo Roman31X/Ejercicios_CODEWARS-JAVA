e3b97ce016b11c1bc5b8e8c5c530faecff4e2f2f
Roman31X <die.haa65@gmail.com>
2024-04-28 15:10:00 -0500

    Planteamiento test aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/RetornarNegativo/TestRetornarNegativo.java
+++ src/main/java/com/Ejercicio/Kyu8/RetornarNegativo/TestRetornarNegativo.java
@@ -1,4 +1,16 @@
 package com.Ejercicio.Kyu8.RetornarNegativo;
 
+import org.junit.Test;
+import static com.Ejercicio.Kyu8.RetornarNegativo.RetornarNegativo.makeNegative;
+import static org.junit.Assert.assertEquals;
+
 public class TestRetornarNegativo {
+    public static void main(String[] args) {
+        System.out.println("Test retornando números negativos");
+    }
+
+    @Test
+    public void test1() {
+        assertEquals(-42, makeNegative(42));
+    }
 }
