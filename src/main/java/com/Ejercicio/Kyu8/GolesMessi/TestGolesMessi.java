e92d8e516dfcdaa7a8fb874eb6014c4e0955c5dc
Roman31X <die.haa65@gmail.com>
2024-04-28 15:30:00 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/GolesMessi/TestGolesMessi.java
+++ src/main/java/com/Ejercicio/Kyu8/GolesMessi/TestGolesMessi.java
@@ -1,4 +1,16 @@
 package com.Ejercicio.Kyu8.GolesMessi;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu8.GolesMessi.CalcularGolesMessi.totalGoals;
+import static org.junit.Assert.assertEquals;
 public class TestGolesMessi {
+    public static void main(String[] args) {
+        System.out.println("Test Saltamontes - Goles de Messi");
+    }
+
+    @Test
+    public void BasicTests() {
+        assertEquals(58, totalGoals);
+    }
 }
