41d3ac5b5b2528abe00717e12157c91cc60f2c8b
Roman31X <die.haa65@gmail.com>
2024-04-29 15:10:00 -0500

    Ejercicio desarrollado y test aprobados con exito

--- src/main/java/com/Ejercicio/Kyu4/MezclaDeCuerdas/MezclarCadenas.java
+++ src/main/java/com/Ejercicio/Kyu4/MezclaDeCuerdas/MezclarCadenas.java
@@ -1,4 +1,49 @@
 package com.Ejercicio.Kyu4.MezclaDeCuerdas;
 
+import java.util.*;
+
 public class MezclarCadenas {
+    public static String mix(String s1, String s2) {
+        Map<Character, Integer> s1Frequency = getCharacterFrequency(s1);
+        Map<Character, Integer> s2Frequency = getCharacterFrequency(s2);
+
+        List<String> result = new ArrayList<>();
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            int s1Count = s1Frequency.getOrDefault(c, 0);
+            int s2Count = s2Frequency.getOrDefault(c, 0);
+
+            if (s1Count > 1 || s2Count > 1) {
+                if (s1Count > s2Count) {
+                    result.add("1:" + String.join("", Collections.nCopies(s1Count, String.valueOf(c))));
+                } else if (s2Count > s1Count) {
+                    result.add("2:" + String.join("", Collections.nCopies(s2Count, String.valueOf(c))));
+                } else {
+                    result.add("=:" + String.join("", Collections.nCopies(s1Count, String.valueOf(c))));
+                }
+            }
+        }
+
+        Collections.sort(result, (s, t) -> {
+            if (s.length() != t.length()) {
+                return t.length() - s.length();
+            } else {
+                return s.compareTo(t);
+            }
+        });
+
+        return String.join("/", result);
+    }
+
+    private static Map<Character, Integer> getCharacterFrequency(String s) {
+        Map<Character, Integer> frequency = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                frequency.put(c, frequency.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        return frequency;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu4/MezclaDeCuerdas/TestMezclaCadenas.java
+++ src/main/java/com/Ejercicio/Kyu4/MezclaDeCuerdas/TestMezclaCadenas.java
@@ -1,4 +1,27 @@
 package com.Ejercicio.Kyu4.MezclaDeCuerdas;
 
+import static com.Ejercicio.Kyu4.MezclaDeCuerdas.MezclarCadenas.mix;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestMezclaCadenas {
+    public static void main(String[] args) {
+        System.out.println("Test Mezcla de cadenas String");
+    }
+    @Test
+    public void test() {
+        System.out.println("Mix Fixed Tests");
+        assertEquals("2:eeeee/2:yy/=:hh/=:rr",
+                mix("Are they here", "yes, they are here"));
+        assertEquals("1:ooo/1:uuu/2:sss/=:nnn/1:ii/2:aa/2:dd/2:ee/=:gg",
+                mix("looping is fun but dangerous", "less dangerous than coding"));
+        assertEquals("1:aaa/1:nnn/1:gg/2:ee/2:ff/2:ii/2:oo/2:rr/2:ss/2:tt",
+                mix(" In many languages", " there's a pair of functions"));
+        assertEquals("1:ee/1:ll/1:oo",
+                mix("Lords of the Fallen", "gamekult"));
+        assertEquals("",
+                mix("codewars", "codewars"));
+        assertEquals("1:nnnnn/1:ooooo/1:tttt/1:eee/1:gg/1:ii/1:mm/=:rr",
+                mix("A generation must confront the looming ", "codewarrs"));
+    }
 }
