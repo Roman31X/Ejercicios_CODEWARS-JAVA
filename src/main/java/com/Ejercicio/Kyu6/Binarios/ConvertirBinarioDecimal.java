bcc97781ff5fb0d2d9171ba6d0ed3ec838cab482
Roman31X <die.haa65@gmail.com>
2024-04-30 15:05:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/Binarios/ConvertirBinarioDecimal.java
+++ src/main/java/com/Ejercicio/Kyu6/Binarios/ConvertirBinarioDecimal.java
@@ -1,5 +1,40 @@
 package com.Ejercicio.Kyu6.Binarios;
 
 public class ConvertirBinarioDecimal {
+    public static String code(String strng) {
+        StringBuilder result = new StringBuilder();
 
+        for (char c : strng.toCharArray()) {
+            String binary = Integer.toBinaryString(Character.getNumericValue(c));
+            int numBits = binary.length();
+
+            result.append("0".repeat(numBits - 1)).append("1").append(binary);
+        }
+
+        return result.toString();
+    }
+
+    public static String decode(String strng) {
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+
+        while (i < strng.length()) {
+            int numZeroes = 0;
+
+            while (strng.charAt(i) == '0') {
+                numZeroes++;
+                i++;
+            }
+
+            i++;
+
+            String binary = strng.substring(i, i + numZeroes + 1);
+            int decimal = Integer.parseInt(binary, 2);
+            result.append(decimal);
+
+            i += numZeroes + 1;
+        }
+
+        return result.toString();
+    }
 }
