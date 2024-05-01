ccb1bced496bfe9f56b1b0fd9de1eddaceaadaf4
Roman31X <die.haa65@gmail.com>
2024-05-01 15:20:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/EncontrarMultiplosNumero/CalcularMultiplosNumero.java
+++ src/main/java/com/Ejercicio/Kyu8/EncontrarMultiplosNumero/CalcularMultiplosNumero.java
@@ -1,4 +1,20 @@
 package com.Ejercicio.Kyu8.EncontrarMultiplosNumero;
 
+import java.util.*;
+
 public class CalcularMultiplosNumero {
+    public static int[] find(int base, int limit) {
+        List<Integer> multiples = new ArrayList<>();
+
+        for (int i = base; i <= limit; i += base) {
+            multiples.add(i);
+        }
+
+        int[] result = new int[multiples.size()];
+        for (int i = 0; i < multiples.size(); i++) {
+            result[i] = multiples.get(i);
+        }
+
+        return result;
+    }
 }
