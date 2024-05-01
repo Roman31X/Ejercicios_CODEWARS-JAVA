044dbdfcde5ab2b1010e64983c7136219299927c
Roman31X <die.haa65@gmail.com>
2024-05-01 15:05:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/MasCerosQueUnos/CalculeCerosCadena.java
+++ src/main/java/com/Ejercicio/Kyu6/MasCerosQueUnos/CalculeCerosCadena.java
@@ -1,4 +1,32 @@
 package com.Ejercicio.Kyu6.MasCerosQueUnos;
 
+import java.util.*;
+
 public class CalculeCerosCadena {
+    public static char[] moreZeros(String s) {
+        List<Character> result = new ArrayList<>();
+        Set<Character> seen = new HashSet<>();
+
+        for (char c : s.toCharArray()) {
+            if (seen.contains(c)) {
+                continue;
+            }
+
+            String binary = Integer.toBinaryString(c);
+            int countZeros = binary.length() - binary.replace("0", "").length();
+            int countOnes = binary.length() - countZeros;
+
+            if (countZeros > countOnes) {
+                result.add(c);
+                seen.add(c);
+            }
+        }
+
+        char[] output = new char[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            output[i] = result.get(i);
+        }
+
+        return output;
+    }
 }
