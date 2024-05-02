7bc1e9e8eb1f80731470480a580a79f0c656f273
Roman31X <die.haa65@gmail.com>
2024-05-02 15:05:00 -0500

    Ejercicio DESARROLLADO

--- src/main/java/com/Ejercicio/Kyu8/BienvenidoACiudad/BienvenidaAlaCiudad.java
+++ src/main/java/com/Ejercicio/Kyu8/BienvenidoACiudad/BienvenidaAlaCiudad.java
@@ -1,4 +1,8 @@
 package com.Ejercicio.Kyu8.BienvenidoACiudad;
 
 public class BienvenidaAlaCiudad {
+    public String sayHello(String [] name, String city, String state){
+        String fullName = String.join(" ", name);
+        return "Hello, " + fullName + "! Welcome to " + city + ", " + state + "!";
+    }
 }
