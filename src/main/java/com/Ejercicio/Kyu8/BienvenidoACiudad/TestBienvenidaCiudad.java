a970b9697e409fcc17648630851044ea5222d362
Roman31X <die.haa65@gmail.com>
2024-05-02 15:15:00 -0500

    Test planteado y aprobado con exito

--- src/main/java/com/Ejercicio/Kyu8/BienvenidoACiudad/TestBienvenidaCiudad.java
+++ src/main/java/com/Ejercicio/Kyu8/BienvenidoACiudad/TestBienvenidaCiudad.java
@@ -1,4 +1,18 @@
 package com.Ejercicio.Kyu8.BienvenidoACiudad;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class TestBienvenidaCiudad {
+    public static void main(String[] args) {
+        System.out.println("Test Bienvennido a la ciudad");
+    }
+
+    @Test
+    public void test1() throws Exception {
+        BienvenidaAlaCiudad h = new BienvenidaAlaCiudad();
+        String[] name = {"John", "Smith"};
+        assertEquals("Hello, John Smith! Welcome to Phoenix, Arizona!",
+                h.sayHello(name, "Phoenix", "Arizona"));
+    }
 }
