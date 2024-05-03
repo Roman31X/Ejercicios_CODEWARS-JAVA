6e9e79aa7b7660f3f994053d1576d41f0d1f2968
Roman31X <die.haa65@gmail.com>
2024-05-03 12:02:10 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/PoderDeDos/TestPoderDeDos.java
+++ src/main/java/com/Ejercicio/Kyu7/PoderDeDos/TestPoderDeDos.java
@@ -1,5 +1,22 @@
 package com.Ejercicio.Kyu7.PoderDeDos;
 
-public class TestPoderDeDos {
+import org.junit.jupiter.api.Test;
+import static com.Ejercicio.Kyu7.PoderDeDos.CalcularPoder.isPowerOfTwo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+public class TestPoderDeDos {
+    public static void main(String[] args) {
+        System.out.println("Test poder de dos");
+    }
+    @Test
+    void testPowerOfTwo() {
+        assertEquals(true, isPowerOfTwo(   2));
+        assertEquals(true, isPowerOfTwo(4096));
+    }
+
+    @Test
+    void testNotPowersOfTwo() {
+        assertEquals(false, isPowerOfTwo(333));
+        assertEquals(false, isPowerOfTwo(  0));
+    }
 }
