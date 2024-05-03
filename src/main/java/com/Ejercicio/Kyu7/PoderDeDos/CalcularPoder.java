b4e54d634be340101efc60c5f3ca1e4b50bf73ee
Roman31X <die.haa65@gmail.com>
2024-05-03 12:01:05 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/PoderDeDos/CalcularPoder.java
+++ src/main/java/com/Ejercicio/Kyu7/PoderDeDos/CalcularPoder.java
@@ -1,5 +1,11 @@
 package com.Ejercicio.Kyu7.PoderDeDos;
 
 public class CalcularPoder {
+    public static boolean isPowerOfTwo(long n) {
+        if (n <= 0) {
+            return false;
+        }
 
+        return (n & (n - 1)) == 0;
+    }
 }
