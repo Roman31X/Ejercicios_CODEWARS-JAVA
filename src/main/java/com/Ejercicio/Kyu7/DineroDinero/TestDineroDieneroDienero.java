045b3a49db622aff0cfc15d1bb741c1b32b6e254
Roman31X <die.haa65@gmail.com>
2024-05-03 12:08:09 -0500

    test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/DineroDinero/TestDineroDieneroDienero.java
+++ src/main/java/com/Ejercicio/Kyu7/DineroDinero/TestDineroDieneroDienero.java
@@ -1,4 +1,18 @@
 package com.Ejercicio.Kyu7.DineroDinero;
 
+import static com.Ejercicio.Kyu7.DineroDinero.CalcularDinero.calculateYears;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestDineroDieneroDienero {
+    public static void main(String[] args) {
+        System.out.println("Test Dinero dinero y mas dinero");
+    }
+    @Test
+    public void test() {
+        System.out.println("Fixed Tests calculateYears");
+        assertEquals(3, calculateYears(1000,0.05,0.18,1100));
+        assertEquals(14 , calculateYears(1000,0.01625,0.18,1200));
+        assertEquals(0, calculateYears(1000,0.05,0.18,1000));
+    }
 }
