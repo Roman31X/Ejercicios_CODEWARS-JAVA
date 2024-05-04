f1e1f3536a817ddbf7e8f3249d0ba2d1aec413f2
Roman31X <die.haa65@gmail.com>
2024-05-04 15:20:00 -0500

    Ejercicio desarrollado y test aprovados

--- src/main/java/com/Ejercicio/Kyu7/EvaporadorDesodorante/CalcularGatoDesodorante.java
+++ src/main/java/com/Ejercicio/Kyu7/EvaporadorDesodorante/CalcularGatoDesodorante.java
@@ -1,4 +1,16 @@
 package com.Ejercicio.Kyu7.EvaporadorDesodorante;
 
 public class CalcularGatoDesodorante {
+    public static int evaporator(double content, double evap_per_day, double threshold) {
+        int days = 0;
+        double remainingPercentage = 100.0;
+        double thresholdContent = content * (threshold / 100);
+
+        while (content > thresholdContent) {
+            content -= content * (evap_per_day / 100);
+            days++;
+        }
+
+        return days;
+    }
 }
--- src/main/java/com/Ejercicio/Kyu7/EvaporadorDesodorante/TestEvaporadorDeDesodorante.java
+++ src/main/java/com/Ejercicio/Kyu7/EvaporadorDesodorante/TestEvaporadorDeDesodorante.java
@@ -1,4 +1,15 @@
 package com.Ejercicio.Kyu7.EvaporadorDesodorante;
 
+import static com.Ejercicio.Kyu7.EvaporadorDesodorante.CalcularGatoDesodorante.evaporator;
+import static org.junit.Assert.*;
+import org.junit.*;
+
 public class TestEvaporadorDeDesodorante {
+    public static void main(String[] args) {
+        System.out.println("Test para calcular el evaporador de desodorante");
+    }
+    @Test
+    public void testEvaporatorOne() {
+        assertEquals(22 , evaporator(10, 10, 10));
+    }
 }
