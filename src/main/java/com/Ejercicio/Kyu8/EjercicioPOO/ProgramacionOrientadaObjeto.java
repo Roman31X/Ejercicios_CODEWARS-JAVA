f1fdb1f0f6112a2aeb3f93a46d0e5dac8da328e6
Roman31X <die.haa65@gmail.com>
2024-05-04 15:05:00 -0500

    Ejecicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/EjercicioPOO/ProgramacionOrientadaObjeto.java
+++ src/main/java/com/Ejercicio/Kyu8/EjercicioPOO/ProgramacionOrientadaObjeto.java
@@ -1,4 +1,17 @@
 package com.Ejercicio.Kyu8.EjercicioPOO;
 
 public class ProgramacionOrientadaObjeto {
+
+    private final double draft;
+    private final int crew;
+
+    public ProgramacionOrientadaObjeto(double draft, int crew) {
+        this.draft = draft;
+        this.crew = crew;
+    }
+
+    public boolean isWorthIt() {
+        double totalWeight = draft - (crew * 1.5);
+        return totalWeight > 20;
+    }
 }
