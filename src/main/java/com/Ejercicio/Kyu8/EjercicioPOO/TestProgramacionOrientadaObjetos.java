91ae9a46a1f4fe22d739a17dfb21b30ab38ec2f3
Roman31X <die.haa65@gmail.com>
2024-05-04 15:05:00 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/EjercicioPOO/TestProgramacionOrientadaObjetos.java
+++ src/main/java/com/Ejercicio/Kyu8/EjercicioPOO/TestProgramacionOrientadaObjetos.java
@@ -1,4 +1,22 @@
 package com.Ejercicio.Kyu8.EjercicioPOO;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestProgramacionOrientadaObjetos {
+    public static void main(String[] args) {
+        System.out.println("Test de Programación Orientado a Objetos");
+    }
+
+    @Test
+    void emptyShipShouldNotWorthToAboard() {
+        ProgramacionOrientadaObjeto ship = new ProgramacionOrientadaObjeto(0 , 0);
+        assertEquals(false, ship.isWorthIt(), "<draft: 0, crew: 0>");
+    }
+
+    @Test
+    void shipWithLotOfBootyWorthToAboard() {
+        ProgramacionOrientadaObjeto ship = new ProgramacionOrientadaObjeto(40 , 5);
+        assertEquals(true, ship.isWorthIt(), "<draft: 40, crew: 5>");
+    }
 }
