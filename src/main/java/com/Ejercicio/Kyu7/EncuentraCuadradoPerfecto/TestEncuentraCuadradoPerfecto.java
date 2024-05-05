e1a527b7493cc5e0b6298e0ceff1765afab92c57
Roman31X <die.haa65@gmail.com>
2024-05-05 15:00:00 -0500

    Ejercicio inicalizado

---
+++ src/main/java/com/Ejercicio/Kyu7/EncuentraCuadradoPerfecto/TestEncuentraCuadradoPerfecto.java
@@ -0,0 +1,9 @@
+package com.Ejercicio.Kyu7.EncuentraCuadradoPerfecto;
+
+
+
+public class TestEncuentraCuadradoPerfecto {
+    public static void main(String[] args) {
+        System.out.println("Test ¡Encuentra el siguiente cuadrado perfecto!");
+    }
+}
