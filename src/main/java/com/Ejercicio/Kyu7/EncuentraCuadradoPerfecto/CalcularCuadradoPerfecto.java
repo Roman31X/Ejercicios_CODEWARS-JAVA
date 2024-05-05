08839e19391854f40beda48a2d365806e62f23a5
Roman31X <die.haa65@gmail.com>
2024-05-05 15:00:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/EncuentraCuadradoPerfecto/CalcularCuadradoPerfecto.java
+++ src/main/java/com/Ejercicio/Kyu7/EncuentraCuadradoPerfecto/CalcularCuadradoPerfecto.java
@@ -1,4 +1,12 @@
 package com.Ejercicio.Kyu7.EncuentraCuadradoPerfecto;
 
 public class CalcularCuadradoPerfecto {
+    public static long findNextSquare(long sq) {
+        long root = (long) Math.sqrt(sq);
+        if (root * root != sq) {
+            return -1;
+        }
+        long nextRoot = root + 1;
+        return nextRoot * nextRoot;
+    }
 }
