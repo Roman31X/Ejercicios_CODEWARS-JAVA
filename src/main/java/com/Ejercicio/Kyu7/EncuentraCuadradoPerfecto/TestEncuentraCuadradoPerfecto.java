9bea390cb347e711177bc73feb3fd8cc4621d4a1
Roman31X <die.haa65@gmail.com>
2024-05-05 15:10:00 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/EncuentraCuadradoPerfecto/TestEncuentraCuadradoPerfecto.java
+++ src/main/java/com/Ejercicio/Kyu7/EncuentraCuadradoPerfecto/TestEncuentraCuadradoPerfecto.java
@@ -1,9 +1,25 @@
 package com.Ejercicio.Kyu7.EncuentraCuadradoPerfecto;
 
-
+import org.junit.Test;
+import static com.Ejercicio.Kyu7.EncuentraCuadradoPerfecto.CalcularCuadradoPerfecto.findNextSquare;
+import static org.junit.Assert.assertEquals;
 
 public class TestEncuentraCuadradoPerfecto {
     public static void main(String[] args) {
         System.out.println("Test ¡Encuentra el siguiente cuadrado perfecto!");
+    }
+    @Test
+    public void test1() {
+        assertEquals(144, findNextSquare(121));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(676, findNextSquare(625));
+    }
+
+    @Test
+    public void test3() {
+        assertEquals(-1, findNextSquare(114));
     }
 }
