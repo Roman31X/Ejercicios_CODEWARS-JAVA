ed7af16c11cbc0ecac0bf7e33dadd3f2bf224c73
Roman31X <die.haa65@gmail.com>
2024-05-06 15:10:00 -0500

    Test plaanteado y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/OrdenarYDestacar/TestOrdenaryDestacar.java
+++ src/main/java/com/Ejercicio/Kyu8/OrdenarYDestacar/TestOrdenaryDestacar.java
@@ -1,4 +1,17 @@
 package com.Ejercicio.Kyu8.OrdenarYDestacar;
 
+import org.junit.Test;
+import static com.Ejercicio.Kyu8.OrdenarYDestacar.OrdenarCadenaDestacarOrden.twoSort;
+import static org.junit.Assert.assertEquals;
+
 public class TestOrdenaryDestacar {
+    public static void main(String[] args) {
+        System.out.println("Test ordenar y destacar");
+    }
+
+    @Test
+    public void testFixed() {
+        assertEquals("b***i***t***c***o***i***n", twoSort(new String[] {"bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"}));
+        assertEquals("a***r***e", twoSort(new String[] {"turns", "out", "random", "test", "cases", "are", "easier", "than", "writing", "out", "basic", "ones"}));
+    }
 }
