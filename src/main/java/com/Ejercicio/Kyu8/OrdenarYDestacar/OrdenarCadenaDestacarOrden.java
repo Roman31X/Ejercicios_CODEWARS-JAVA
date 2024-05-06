cdca0e537395824635c4d19f5ffcf510b5eb82bc
Roman31X <die.haa65@gmail.com>
2024-05-06 15:00:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/OrdenarYDestacar/OrdenarCadenaDestacarOrden.java
+++ src/main/java/com/Ejercicio/Kyu8/OrdenarYDestacar/OrdenarCadenaDestacarOrden.java
@@ -1,4 +1,19 @@
 package com.Ejercicio.Kyu8.OrdenarYDestacar;
 
+import java.util.*;
+
 public class OrdenarCadenaDestacarOrden {
+    public static String twoSort(String[] s) {
+        Arrays.sort(s);
+        String firstSortedString = s[0];
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < firstSortedString.length(); i++) {
+            result.append(firstSortedString.charAt(i));
+            if (i != firstSortedString.length() - 1) {
+                result.append("***");
+            }
+        }
+
+        return result.toString();
+    }
 }
