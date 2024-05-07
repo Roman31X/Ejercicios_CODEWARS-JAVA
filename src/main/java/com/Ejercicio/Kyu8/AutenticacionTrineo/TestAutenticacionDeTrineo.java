82494f235b1616dd571d9a8bd4bd78bbab9d1aac
Roman31X <die.haa65@gmail.com>
2024-05-07 15:10:00 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu8/AutenticacionTrineo/TestAutenticacionDeTrineo.java
+++ src/main/java/com/Ejercicio/Kyu8/AutenticacionTrineo/TestAutenticacionDeTrineo.java
@@ -1,4 +1,19 @@
 package com.Ejercicio.Kyu8.AutenticacionTrineo;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu8.AutenticacionTrineo.EncontrarSantaClaus.authenticate;
+import static org.junit.Assert.assertEquals;
+
 public class TestAutenticacionDeTrineo {
+    public static void main(String[] args) {
+        System.out.println("Test Autenticación de trineo");
+    }
+
+    @Test
+    public void test_authentication() {
+        assertEquals(true, authenticate("Santa Claus", "Ho Ho Ho!"));
+        assertEquals(false, authenticate("Santa", "Ho Ho Ho!"));
+        assertEquals(false, authenticate("Santa Claus", "Ho Ho Ho"));
+    }
 }
