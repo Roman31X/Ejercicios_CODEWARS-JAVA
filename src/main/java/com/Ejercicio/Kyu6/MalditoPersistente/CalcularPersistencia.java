6fd25819990434e50ae06b9e5aa5b494acba8be8
Roman31X <die.haa65@gmail.com>
2024-05-09 15:05:00 -0500

    Ejericicio desarrollado

--- src/main/java/com/Ejercicio/Kyu6/MalditoPersistente/CalcularPersistencia.java
+++ src/main/java/com/Ejercicio/Kyu6/MalditoPersistente/CalcularPersistencia.java
@@ -1,4 +1,19 @@
 package com.Ejercicio.Kyu6.MalditoPersistente;
 
 public class CalcularPersistencia {
+    public static int persistence(long n) {
+        int count = 0;
+
+        while (n >= 10) {
+            long product = 1;
+            while (n > 0) {
+                product *= n % 10;
+                n /= 10;
+            }
+            n = product;
+            count++;
+        }
+
+        return count;
+    }
 }
