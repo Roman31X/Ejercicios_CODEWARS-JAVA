cbbe123d260e75da291d0f78a9457baa77b0b93c
Roman31X <die.haa65@gmail.com>
2024-05-10 15:10:00 -0500

    Test planteados y aprobado con exito

--- src/main/java/com/Ejercicio/Kyu8/EsUnNumero/TestEsUnNumero.java
+++ src/main/java/com/Ejercicio/Kyu8/EsUnNumero/TestEsUnNumero.java
@@ -1,4 +1,23 @@
 package com.Ejercicio.Kyu8.EsUnNumero;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class TestEsUnNumero {
+    public static void main(String[] args) {
+        System.out.println("Test ¿Es un Número?");
+    }
+
+    IdentificarSiEsNumero myUtil = new IdentificarSiEsNumero();
+
+    @Test
+    public void test0() throws Exception
+    {
+        assertEquals(false,myUtil.isDigit("s2324"));
+    }
+    @Test
+    public void test1() throws Exception
+    {
+        assertEquals(true,myUtil.isDigit("-234.4"));
+    }
 }
