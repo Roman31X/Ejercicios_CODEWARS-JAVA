902d34d52f7e90ed198dc3bb43f6072c90e2e2d2
Roman31X <die.haa65@gmail.com>
2024-05-10 15:10:00 -0500

    Ejericio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/EsUnNumero/IdentificarSiEsNumero.java
+++ src/main/java/com/Ejercicio/Kyu8/EsUnNumero/IdentificarSiEsNumero.java
@@ -1,4 +1,13 @@
 package com.Ejercicio.Kyu8.EsUnNumero;
 
 public class IdentificarSiEsNumero {
+    public boolean isDigit(String s)
+    {
+        try {
+            Double.parseDouble(s.trim());
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
