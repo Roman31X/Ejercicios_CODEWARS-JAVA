58f06c18c8aa7f53a453b356ae3c5a453feb0c44
Roman31X <die.haa65@gmail.com>
2024-05-11 15:05:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/ADNComplementario/CompletarADN.java
+++ src/main/java/com/Ejercicio/Kyu7/ADNComplementario/CompletarADN.java
@@ -1,4 +1,30 @@
 package com.Ejercicio.Kyu7.ADNComplementario;
 
 public class CompletarADN {
+    public static String makeComplement(String dna) {
+        StringBuilder complement = new StringBuilder();
+
+        for (int i = 0; i < dna.length(); i++) {
+            char nucleotide = dna.charAt(i);
+            char complementNucleotide = getComplement(nucleotide);
+            complement.append(complementNucleotide);
+        }
+
+        return complement.toString();
+    }
+
+    private static char getComplement(char nucleotide) {
+        switch (nucleotide) {
+            case 'A':
+                return 'T';
+            case 'T':
+                return 'A';
+            case 'C':
+                return 'G';
+            case 'G':
+                return 'C';
+            default:
+                throw new IllegalArgumentException("Invalid nucleotide: " + nucleotide);
+        }
+    }
 }
