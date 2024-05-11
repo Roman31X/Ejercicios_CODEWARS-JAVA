dfd6a8a125c00fdbfc142a506972b87063775499
Roman31X <die.haa65@gmail.com>
2024-05-11 15:05:00 -0500

    Test propuestos y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/ADNComplementario/TestCompletandoADN.java
+++ src/main/java/com/Ejercicio/Kyu7/ADNComplementario/TestCompletandoADN.java
@@ -1,4 +1,25 @@
 package com.Ejercicio.Kyu7.ADNComplementario;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu7.ADNComplementario.CompletarADN.makeComplement;
+import static org.junit.Assert.assertEquals;
+
 public class TestCompletandoADN {
+    public static void main(String[] args) {
+        System.out.println("Test completando cadenas ADN");
+    }
+
+    @Test
+    public void test01() {
+        assertEquals("TTTT", makeComplement("AAAA"));
+    }
+    @Test
+    public void test02() {
+        assertEquals("TAACG", makeComplement("ATTGC"));
+    }
+    @Test
+    public void test03() {
+        assertEquals("CATA", makeComplement("GTAT"));
+    }
 }
