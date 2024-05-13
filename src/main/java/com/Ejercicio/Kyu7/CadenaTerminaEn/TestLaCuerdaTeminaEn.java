1cc33dcdf02f34930c63a2e0ca454d5299e42140
Roman31X <die.haa65@gmail.com>
2024-05-13 15:10:00 -0500

    Test propuestos y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/CadenaTerminaEn/TestLaCuerdaTeminaEn.java
+++ src/main/java/com/Ejercicio/Kyu7/CadenaTerminaEn/TestLaCuerdaTeminaEn.java
@@ -1,5 +1,34 @@
 package com.Ejercicio.Kyu7.CadenaTerminaEn;
 
-public class TestLaCuerdaTeminaEn {
+import org.junit.Test;
+import static com.Ejercicio.Kyu7.CadenaTerminaEn.IdentificarFinalCadena.solution;
+import static org.junit.Assert.assertEquals;
 
+public class TestLaCuerdaTeminaEn {
+    public static void main(String[] args) {
+        System.out.println("Test de La cadena termina con");
+    }
+    @Test
+    public void staticTests() {
+        check("samurai", "ai", true);
+        check("sumo", "omo", false);
+        check("ninja", "ja", true);
+        check("sensei", "i", true);
+        check("samurai", "ra", false);
+        check("abc", "abcd", false);
+        check("abc", "abc", true);
+        check("abcabc", "bc", true);
+        check("ails", "fails", false);
+        check("fails", "ails", true);
+        check("this", "fails", false);
+        check("this", "", true);
+        check(":-)", ":-(", false);
+        check("!@#$%^&*() :-)", ":-)", true);
+        check("abc\n", "abc", false);
+    }
+
+    private static void check(String str, String ending, boolean expected) {
+        boolean result = solution(str, ending);
+        assertEquals("Expected solution(\"" + str + "\", \"" + ending + "\") to return " + expected, expected, result);
+    }
 }
