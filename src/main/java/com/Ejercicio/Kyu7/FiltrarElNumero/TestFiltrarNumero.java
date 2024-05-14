39b5eee68ba1a4ed9b4e48731655eb81b2ceeede
Roman31X <die.haa65@gmail.com>
2024-05-14 15:15:00 -0500

    Test propuestos y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/FiltrarElNumero/TestFiltrarNumero.java
+++ src/main/java/com/Ejercicio/Kyu7/FiltrarElNumero/TestFiltrarNumero.java
@@ -1,4 +1,17 @@
 package com.Ejercicio.Kyu7.FiltrarElNumero;
 
+import org.junit.Test;
+import static com.Ejercicio.Kyu7.FiltrarElNumero.FiltrarNumeroCadena.filterString;
+import static junit.framework.TestCase.assertEquals;
+
 public class TestFiltrarNumero {
+    public static void main(String[] args) {
+        System.out.println("Test filtrar el número");
+    }
+    @Test
+    public void exampleTests() {
+        assertEquals(123,filterString("123"));
+        assertEquals(123,filterString("a1b2c3"));
+        assertEquals(123,filterString("aa1bb2cc3dd"));
+    }
 }
