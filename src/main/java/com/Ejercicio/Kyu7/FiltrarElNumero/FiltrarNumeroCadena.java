0a23f2141fd0ec1547ef5046c89480afd5ad60a7
Roman31X <die.haa65@gmail.com>
2024-05-14 15:05:00 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/FiltrarElNumero/FiltrarNumeroCadena.java
+++ src/main/java/com/Ejercicio/Kyu7/FiltrarElNumero/FiltrarNumeroCadena.java
@@ -1,4 +1,15 @@
 package com.Ejercicio.Kyu7.FiltrarElNumero;
 
 public class FiltrarNumeroCadena {
+    public static long filterString(final String value) {
+        StringBuilder number = new StringBuilder();
+
+        for (char c : value.toCharArray()) {
+            if (Character.isDigit(c)) {
+                number.append(c);
+            }
+        }
+
+        return Long.parseLong(number.toString());
+    }
 }
