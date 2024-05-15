b2f23bfc35da8088898b060e44f3a97dbaa61715
Roman31X <die.haa65@gmail.com>
2024-05-15 15:05:00 -0500

    ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu8/SaltamontesSuma/CalcularSuma.java
+++ src/main/java/com/Ejercicio/Kyu8/SaltamontesSuma/CalcularSuma.java
@@ -1,4 +1,11 @@
 package com.Ejercicio.Kyu8.SaltamontesSuma;
 
 public class CalcularSuma {
+    public static int summation(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 }
