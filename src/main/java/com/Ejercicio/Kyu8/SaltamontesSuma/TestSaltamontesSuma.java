f6573f47bd5c128bfe53a449346b3ad28ea8926b
Roman31X <die.haa65@gmail.com>
2024-05-15 15:10:00 -0500

    Test propuestos y aprobados

--- src/main/java/com/Ejercicio/Kyu8/SaltamontesSuma/TestSaltamontesSuma.java
+++ src/main/java/com/Ejercicio/Kyu8/SaltamontesSuma/TestSaltamontesSuma.java
@@ -1,4 +1,23 @@
 package com.Ejercicio.Kyu8.SaltamontesSuma;
 
+import java.util.Random;
+
+import static com.Ejercicio.Kyu8.SaltamontesSuma.CalcularSuma.summation;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class TestSaltamontesSuma {
+    public static void main(String[] args) {
+        System.out.println("Test de Satamontes suma");
+    }
+    @Test
+    public void test1() {
+        assertEquals(1,
+                summation(1));
+    }
+    @Test
+    public void test2() {
+        assertEquals(36,
+                summation(8));
+    }
 }
