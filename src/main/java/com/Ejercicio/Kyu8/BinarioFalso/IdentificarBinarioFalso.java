25a8f1c912adff3a3baf0fabb87f77ca6ed41377
Roman31X <die.haa65@gmail.com>
2024-05-16 15:05:00 -0500

    Kyu8 desarrollado

--- src/main/java/com/Ejercicio/Kyu8/BinarioFalso/IdentificarBinarioFalso.java
+++ src/main/java/com/Ejercicio/Kyu8/BinarioFalso/IdentificarBinarioFalso.java
@@ -1,4 +1,17 @@
 package com.Ejercicio.Kyu8.BinarioFalso;
 
 public class IdentificarBinarioFalso {
+    public static String fakeBin(String numberString) {
+        StringBuilder result = new StringBuilder();
+
+        for (char c : numberString.toCharArray()) {
+            if (c < '5') {
+                result.append('0');
+            } else {
+                result.append('1');
+            }
+        }
+
+        return result.toString();
+    }
 }
