eeb3fae16b929fa7a696a44d126003d65a355ed7
Roman31X <die.haa65@gmail.com>
2024-05-17 17:33:31 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/ArreglarCasoDeCuerda/ArreglarCdaenaDeString.java
+++ src/main/java/com/Ejercicio/Kyu7/ArreglarCasoDeCuerda/ArreglarCdaenaDeString.java
@@ -1,4 +1,24 @@
 package com.Ejercicio.Kyu7.ArreglarCasoDeCuerda;
 
 public class ArreglarCdaenaDeString {
+    public static String solve(final String str) {
+        int lowerCount = 0;
+        int upperCount = 0;
+
+        // Contamos las letras mayúsculas y minúsculas
+        for (char c : str.toCharArray()) {
+            if (Character.isLowerCase(c)) {
+                lowerCount++;
+            } else if (Character.isUpperCase(c)) {
+                upperCount++;
+            }
+        }
+
+        // Decidimos si convertimos a minúsculas o mayúsculas
+        if (lowerCount >= upperCount) {
+            return str.toLowerCase();
+        } else {
+            return str.toUpperCase();
+        }
+    }
 }
