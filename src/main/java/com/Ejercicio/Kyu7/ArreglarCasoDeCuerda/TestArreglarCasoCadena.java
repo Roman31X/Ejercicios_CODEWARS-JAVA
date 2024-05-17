2a7ad71522b2fe4b0126e948059fbae85b5a16f5
Roman31X <die.haa65@gmail.com>
2024-05-17 17:34:30 -0500

    Test planteados y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/ArreglarCasoDeCuerda/TestArreglarCasoCadena.java
+++ src/main/java/com/Ejercicio/Kyu7/ArreglarCasoDeCuerda/TestArreglarCasoCadena.java
@@ -1,4 +1,20 @@
 package com.Ejercicio.Kyu7.ArreglarCasoDeCuerda;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu7.ArreglarCasoDeCuerda.ArreglarCdaenaDeString.solve;
+import static org.junit.Assert.assertEquals;
+
 public class TestArreglarCasoCadena {
+    public static void main(String[] args) {
+        System.out.println("test Arreglar caso de cuerda");
+    }
+    @Test
+    public void BasicTests() {
+        assertEquals("code", solve("code"));
+        assertEquals("CODE", solve("CODe"));
+        assertEquals("code", solve("COde"));
+        assertEquals("code", solve("Code"));
+        assertEquals("", solve(""));
+    }
 }
