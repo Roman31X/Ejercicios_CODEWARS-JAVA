d9094432a70010643c0b4aaa98d472a13053c7cf
Roman31X <die.haa65@gmail.com>
2024-05-18 23:37:39 -0500

    Ejercicio desarrollado

--- src/main/java/com/Ejercicio/Kyu7/SumaNumerosde0aN/CalcularSumade0aN.java
+++ src/main/java/com/Ejercicio/Kyu7/SumaNumerosde0aN/CalcularSumade0aN.java
@@ -1,4 +1,29 @@
 package com.Ejercicio.Kyu7.SumaNumerosde0aN;
 
 public class CalcularSumade0aN {
+    public static String showSequence(int value){
+        if (value < 0) {
+            return value + "<0";
+        }
+
+        // Caso cuando el número es 0
+        if (value == 0) {
+            return "0=0";
+        }
+
+        // Para los valores positivos
+        StringBuilder sequence = new StringBuilder();
+        int sum = 0;
+
+        for (int i = 0; i <= value; i++) {
+            if (i > 0) {
+                sequence.append("+");
+            }
+            sequence.append(i);
+            sum += i;
+        }
+
+        sequence.append(" = ").append(sum);
+        return sequence.toString();
+    }
 }
