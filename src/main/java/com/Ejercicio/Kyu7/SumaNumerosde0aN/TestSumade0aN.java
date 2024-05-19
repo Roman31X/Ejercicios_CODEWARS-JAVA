bd0fb5902ab8fad2ff57d5056b9d6c9780c1d7bc
Roman31X <die.haa65@gmail.com>
2024-05-18 23:41:37 -0500

    Test propuestos y aprobados con exito

--- src/main/java/com/Ejercicio/Kyu7/SumaNumerosde0aN/TestSumade0aN.java
+++ src/main/java/com/Ejercicio/Kyu7/SumaNumerosde0aN/TestSumade0aN.java
@@ -1,4 +1,17 @@
 package com.Ejercicio.Kyu7.SumaNumerosde0aN;
 
+import org.junit.Test;
+
+import static com.Ejercicio.Kyu7.SumaNumerosde0aN.CalcularSumade0aN.showSequence;
+import static org.junit.Assert.assertEquals;
+
 public class TestSumade0aN {
+    public static void main(String[] args) {
+        System.out.println("Test de suma desde 0 a N numero");
+    }
+    @Test
+    public void testBasic() {
+        assertEquals("0+1+2+3+4+5+6 = 21",showSequence(6));
+    }
+
 }
